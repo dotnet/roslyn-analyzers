cfc75e06ec03e95ac11f2bf838e4ed60eefc14db
Josh Varty <joshvarty@gmail.com>
2015-09-21 23:48:35 -0400

    Initial attempt at building the analyzer

--- src/CodeAnalysis/CSharp/MetaAnalyzers/CSharpImmutableObjectMethodAnalyzer.cs
+++ src/CodeAnalysis/CSharp/MetaAnalyzers/CSharpImmutableObjectMethodAnalyzer.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Analyzers;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Microsoft.CodeAnalysis.CSharp.Analyzers.MetaAnalyzers
 {
@@ -34,9 +35,69 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.MetaAnalyzers
             }
         }
 
+        private static readonly string s_solutionFullName = @"Microsoft.CodeAnalysis.Solution";
+        private static readonly string s_projectFullName = @"Microsoft.CodeAnalysis.Project";
+        private static readonly string s_documentFullName = @"Microsoft.CodeAnalysis.Document";
+        private static readonly string s_syntaxNodeFullName = @"Microsoft.CodeAnalysis.SyntaxNode";
+
+        // A list of known immutable object names
+        private static readonly List<string> s_immutableObjectNames = new List<string>()
+        {
+            s_solutionFullName,
+            s_projectFullName,
+            s_documentFullName,
+            s_syntaxNodeFullName,
+        };
+
+        private static readonly string s_Add = "Add";
+        private static readonly string s_Remove = "Remove";
+        private static readonly string s_Replace = "Replace";
+        private static readonly string s_With = "With";
+
+        private static readonly List<string> s_immutableMethodNames = new List<string>()
+        {
+            s_Add,
+            s_Remove,
+            s_Replace,
+            s_With,
+        };
+
         public override void Initialize(AnalysisContext context)
         {
+            context.RegisterCodeBlockAction(AnalyzeCodeBlockForIgnoredReturnValues);
+        }
 
+        public void AnalyzeCodeBlockForIgnoredReturnValues(CodeBlockAnalysisContext context)
+        {
+            var model = context.SemanticModel;
+            var syntaxNode = context.CodeBlock;
+
+            //Find invocations that ignore the return value
+            var candidateInvocations = syntaxNode.DescendantNodes()
+                .OfType<InvocationExpressionSyntax>()
+                .Where(n => n.Parent is EqualsValueClauseSyntax);
+
+            foreach (var candidateInvocation in candidateInvocations)
+            {
+                //If we can't find the method symbol, quit
+                var methodSymbol = model.GetSymbolInfo(candidateInvocation).Symbol as IMethodSymbol;
+                if (methodSymbol == null)
+                    continue;
+
+                //If the method doesn't start with something like "With" or "Replace", quit
+                string methodName = methodSymbol.Name;
+                if (!s_immutableMethodNames.Any(n => methodName.StartsWith(n)))
+                    continue;
+
+                //If we're not in one of the known immutable types, quit
+                var parentSymbol = methodSymbol.ContainingType;
+                if (!s_immutableObjectNames.Contains(parentSymbol.Name.ToString()))
+                    continue;
+
+                var location = candidateInvocation.GetLocation();
+                var diagnostic = Diagnostic.Create(DoNotIgnoreReturnValueDiagnosticRule, location);
+                context.ReportDiagnostic(diagnostic);
+            }
         }
     }
 }

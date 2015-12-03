79b42ecbd01d70bbfabab044238427610a680c9a
Srivatsn Narayanan <srivatsn@microsoft.com>
2015-12-03 12:30:54 -0800

    Update script to generate markdown for the new set of analyzers

--- src/Tools/Scripts/BuildAnalyzerMarkdown.bat
+++ src/Tools/Scripts/BuildAnalyzerMarkdown.bat
@@ -6,12 +6,16 @@ set a2md=%binaries%\a2md.exe
 
 pushd %binaries%
 
-%a2md% Desktop.Analyzers.dll Desktop.CSharp.Analyzers.dll Desktop.VisualBasic.Analyzers.dll > %root%\src\FxCop\Desktop.Analyzers\Desktop.Analyzers.md
-%a2md% System.Runtime.InteropServices.Analyzers.dll System.Runtime.InteropServices.CSharp.Analyzers.dll System.Runtime.InteropServices.VisualBasic.Analyzers.pdb > %root%\src\Fxcop\System.Runtime.InteropServices.Analyzers\System.Runtime.InteropServices.Analyzers.md
-%a2md% System.Runtime.Analyzers.dll System.Runtime.CSharp.Analyzers.dll System.Runtime.VisualBasic.Analyzers.dll > %root%\src\fxcop\System.Runtime.Analyzers\System.Runtime.Analyzers.md
-%a2md% Roslyn.Diagnostics.Analyzers.CSharp.dll Roslyn.Diagnostics.Analyzers.dll Roslyn.Diagnostics.Analyzers.VisualBasic.dll > %root%\src\Roslyn\Microsoft.Net.RoslynDiagnostics.md
+%a2md% ApiReview.Analyzers.dll ApiReview.CSharp.Analyzers.dll ApiReview.VisualBasic.Analyzers.dll > %root%\src\ApiReview.Analyzers\ApiReview.Analyzers.md
+%a2md% Desktop.Analyzers.dll Desktop.CSharp.Analyzers.dll Desktop.VisualBasic.Analyzers.dll > %root%\src\Desktop.Analyzers\Desktop.Analyzers.md
+%a2md% Microsoft.ApiDesignGuidelines.Analyzers.dll Microsoft.ApiDesignGuidelines.CSharp.Analyzers.dll Microsoft.ApiDesignGuidelines.VisualBasic.Analyzers.dll > %root%\src\Microsoft.ApiDesignGuidelines.Analyzers\Microsoft.ApiDesignGuidelines.Analyzers.md
 %a2md% Microsoft.CodeAnalysis.Analyzers.dll Microsoft.CodeAnalysis.CSharp.Analyzers.dll Microsoft.CodeAnalysis.VisualBasic.Analyzers.dll > %root%\src\CodeAnalysis\Microsoft.CodeAnalysis.Analyzers.md
-%a2md% Microsoft.AnalyzerPowerPack.Common.dll Microsoft.AnalyzerPowerPack.CSharp.dll Microsoft.AnalyzerPowerPack.VisualBasic.dll > %root%\src\AnalyzerPowerPack\AnalyzerPowerPack.md
-%a2md% AsyncPackage.dll > %root%\src\AsyncPackage\AsyncPackage.md
+%a2md% Roslyn.Diagnostics.Analyzers.CSharp.dll Roslyn.Diagnostics.Analyzers.dll Roslyn.Diagnostics.Analyzers.VisualBasic.dll > %root%\src\Roslyn\Microsoft.Net.RoslynDiagnostics.md
+%a2md% System.Collections.Immutable.Analyzers.dll System.Collections.Immutable.CSharp.Analyzers.dll System.Collections.Immutable.VisualBasic.Analyzers.dll > %root%\src\System.Collections.Immutable.Analyzers\System.Collections.Immutable.Analyzers.md
+%a2md% System.Runtime.Analyzers.dll System.Runtime.CSharp.Analyzers.dll System.Runtime.VisualBasic.Analyzers.dll > %root%\src\System.Runtime.Analyzers\System.Runtime.Analyzers.md
+%a2md% System.Runtime.InteropServices.Analyzers.dll System.Runtime.InteropServices.CSharp.Analyzers.dll System.Runtime.InteropServices.VisualBasic.Analyzers.pdb > %root%\src\System.Runtime.InteropServices.Analyzers\System.Runtime.InteropServices.Analyzers.md
+%a2md% System.Security.Cryptography.Hashing.Algorithms.Analyzers.dll System.Security.Cryptography.Hashing.Algorithms.CSharp.Analyzers.dll System.Security.Cryptography.Hashing.Algorithms.VisualBasic.Analyzers.dll > %root%\src\System.Security.Cryptography.Hashing.Algorithms.Analyzers\System.Runtime.Analyzers.md
+%a2md% System.Threading.Tasks.Analyzers.dll System.Threading.Tasks.CSharp.Analyzers.dll System.Threading.Tasks.VisualBasic.Analyzers.dll > %root%\src\System.Threading.Tasks.Analyzers\System.Threading.Tasks.Analyzers.md
+%a2md% XmlDocumentationComments.Analyzers.dll XmlDocumentationComments.CSharp.Analyzers.dll XmlDocumentationComments.VisualBasic.Analyzers.dll > %root%\src\XmlDocumentationComments.Analyzers\XmlDocumentationComments.Analyzers.md
 
 popd

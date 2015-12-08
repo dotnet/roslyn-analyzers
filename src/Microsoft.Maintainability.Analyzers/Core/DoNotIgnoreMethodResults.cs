98c43dde76dda2071b01e371071c3f1a6aca5269
Manish Vasani <mavasani@microsoft.com>
2015-12-08 07:24:33 -0800

    Port CA1806 ("Do not ignore method results") as an IOperation analyzer.

--- src/Microsoft.Maintainability.Analyzers/Core/DoNotIgnoreMethodResults.cs
+++ src/Microsoft.Maintainability.Analyzers/Core/DoNotIgnoreMethodResults.cs
@@ -3,66 +3,144 @@
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Semantics;
+using System;
 
 namespace Microsoft.Maintainability.Analyzers
-{                   
+{
     /// <summary>
     /// CA1806: Do not ignore method results
     /// </summary>
-    public abstract class DoNotIgnoreMethodResultsAnalyzer : DiagnosticAnalyzer
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotIgnoreMethodResultsAnalyzer : DiagnosticAnalyzer
     {
         internal const string RuleId = "CA1806";
 
+        private static readonly ImmutableHashSet<string> s_stringMethodNames = ImmutableHashSet.CreateRange(
+            new string[] {
+                "ToUpper",
+                "ToLower",
+                "Trim",
+                "TrimEnd",
+                "TrimStart",
+                "ToUpperInvariant",
+                "ToLowerInvariant",
+                "Clone",
+                "Format",
+                "Concat",
+                "Copy",
+                "Insert",
+                "Join",
+                "Normalize",
+                "Remove",
+                "Replace",
+                "Split",
+                "PadLeft",
+                "PadRight",
+                "Substring",
+            });
+
         private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsTitle), MicrosoftMaintainabilityAnalyzersResources.ResourceManager, typeof(MicrosoftMaintainabilityAnalyzersResources));
-        
+
         private static readonly LocalizableString s_localizableMessageObjectCreation = new LocalizableResourceString(nameof(MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageObjectCreation), MicrosoftMaintainabilityAnalyzersResources.ResourceManager, typeof(MicrosoftMaintainabilityAnalyzersResources));
         private static readonly LocalizableString s_localizableMessageStringCreation = new LocalizableResourceString(nameof(MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageStringCreation), MicrosoftMaintainabilityAnalyzersResources.ResourceManager, typeof(MicrosoftMaintainabilityAnalyzersResources));
         private static readonly LocalizableString s_localizableMessageHResultOrErrorCode = new LocalizableResourceString(nameof(MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageHResultOrErrorCode), MicrosoftMaintainabilityAnalyzersResources.ResourceManager, typeof(MicrosoftMaintainabilityAnalyzersResources));
         private static readonly LocalizableString s_localizableMessageTryParse = new LocalizableResourceString(nameof(MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageTryParse), MicrosoftMaintainabilityAnalyzersResources.ResourceManager, typeof(MicrosoftMaintainabilityAnalyzersResources));
         private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsDescription), MicrosoftMaintainabilityAnalyzersResources.ResourceManager, typeof(MicrosoftMaintainabilityAnalyzersResources));
-        
-        internal static DiagnosticDescriptor ObjectCreationRule = new DiagnosticDescriptor(RuleId,
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(RuleId,
                                                                              s_localizableTitle,
-                                                                             s_localizableMessageObjectCreation,
+                                                                             "{0}",     // Use a placeholder message format as we need to display different messages based on the violation.
                                                                              DiagnosticCategory.Performance,
                                                                              DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
+                                                                             isEnabledByDefault: true,
                                                                              description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
-                                                                             customTags: WellKnownDiagnosticTags.Telemetry);
-        internal static DiagnosticDescriptor StringCreationRule = new DiagnosticDescriptor(RuleId,
-                                                                             s_localizableTitle,
-                                                                             s_localizableMessageStringCreation,
-                                                                             DiagnosticCategory.Performance,
-                                                                             DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
-                                                                             description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
-                                                                             customTags: WellKnownDiagnosticTags.Telemetry);
-        internal static DiagnosticDescriptor HResultOrErrorCodeRule = new DiagnosticDescriptor(RuleId,
-                                                                             s_localizableTitle,
-                                                                             s_localizableMessageHResultOrErrorCode,
-                                                                             DiagnosticCategory.Performance,
-                                                                             DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
-                                                                             description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
-                                                                             customTags: WellKnownDiagnosticTags.Telemetry);
-        internal static DiagnosticDescriptor TryParseRule = new DiagnosticDescriptor(RuleId,
-                                                                             s_localizableTitle,
-                                                                             s_localizableMessageTryParse,
-                                                                             DiagnosticCategory.Performance,
-                                                                             DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
-                                                                             description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
+                                                                             helpLinkUri: "https://msdn.microsoft.com/en-us/library/ms182273.aspx",
                                                                              customTags: WellKnownDiagnosticTags.Telemetry);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(ObjectCreationRule, StringCreationRule, HResultOrErrorCodeRule, TryParseRule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
         public override void Initialize(AnalysisContext analysisContext)
         {
-            
+            analysisContext.RegisterOperationBlockStartAction(osContext =>
+            {
+                var method = osContext.OwningSymbol as IMethodSymbol;
+                if (method == null)
+                {
+                    return;
+                }
+
+                osContext.RegisterOperationAction(opContext =>
+                {
+                    var expression = ((IExpressionStatement)opContext.Operation).Expression;
+                    string messageFormat = null;
+                    string targetMethodName = null;
+                    switch (expression.Kind)
+                    {
+                        case OperationKind.ObjectCreationExpression:
+                            var ctor = ((IObjectCreationExpression)expression).Constructor;
+                            if (ctor != null)
+                            {
+                                messageFormat = MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageObjectCreation;
+                                targetMethodName = ctor.ContainingType.Name;
+                            }
+                            break;
+
+                        case OperationKind.InvocationExpression:
+                            var invocationExpression = ((IInvocationExpression)expression);
+                            var targetMethod = invocationExpression.TargetMethod;
+                            if (targetMethod == null)
+                            {
+                                break;
+                            }
+
+                            if (IsStringCreatingMethod(targetMethod))
+                            {
+                                messageFormat = MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageStringCreation;
+                            }
+                            else if (IsTryParseMethod(targetMethod))
+                            {
+                                messageFormat = MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageTryParse;
+                            }
+                            else if (IsHResultOrErrorCodeReturningMethod(targetMethod))
+                            {
+                                messageFormat = MicrosoftMaintainabilityAnalyzersResources.DoNotIgnoreMethodResultsMessageHResultOrErrorCode;
+                            }
+
+                            targetMethodName = targetMethod.Name;
+                            break;
+                    }
+
+                    if (messageFormat != null)
+                    {
+                        var message = string.Format(messageFormat, method.Name, targetMethodName);
+                        var diagnostic = Diagnostic.Create(Rule, expression.Syntax.GetLocation(), message);
+                        opContext.ReportDiagnostic(diagnostic);
+                    }
+                }, OperationKind.ExpressionStatement);
+            });
+        }
+
+        private static bool IsStringCreatingMethod(IMethodSymbol method)
+        {
+            return method.ContainingType.SpecialType == SpecialType.System_String &&
+                s_stringMethodNames.Contains(method.Name);
+        }
+
+        private static bool IsTryParseMethod(IMethodSymbol method)
+        {
+            return method.Name.StartsWith("TryParse", StringComparison.OrdinalIgnoreCase) &&
+                method.ReturnType.SpecialType == SpecialType.System_Boolean &&
+                method.Parameters.Length >= 2 &&
+                method.Parameters[1].RefKind != RefKind.None;
+        }
+
+        private static bool IsHResultOrErrorCodeReturningMethod(IMethodSymbol method)
+        {
+            // Tune this method to match the FxCop behavior once https://github.com/dotnet/roslyn/issues/7282 is addressed.
+            return method.GetDllImportData() != null &&
+                (method.ReturnType.SpecialType == SpecialType.System_Int32 ||
+                method.ReturnType.SpecialType == SpecialType.System_UInt32);
         }
     }
 }

a88de63008e7e939df9e6c97bd0164201032aafe
duracellko <rasto@duracellko.net>
2016-02-06 22:53:56 +0100

    Add CA1009 unit tests for Visual Basic

--- src/Microsoft.ApiDesignGuidelines.Analyzers/UnitTests/DeclareEventHandlersCorrectlyTests.cs
+++ src/Microsoft.ApiDesignGuidelines.Analyzers/UnitTests/DeclareEventHandlersCorrectlyTests.cs
@@ -211,6 +211,166 @@ public delegate void D();
             VerifyCSharp(test);
         }
 
+        [Fact]
+        public void TestVBEventWithEventArgs()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E As EventHandler
+End Class";
+
+            VerifyBasic(test);
+        }
+
+        [Fact]
+        public void TestVBEventWithParameterInheritedFromEventArgs()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E As EventHandler(Of EA)
+End Class
+
+Public Class EA
+    Inherits EventArgs
+End Class";
+
+            VerifyBasic(test);
+        }
+
+        [Fact]
+        public void TestVBEventWithCustomDelegate()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(sender As Object, e As EA)
+End Class
+
+Public Class EA
+    Inherits EventArgs
+End Class";
+
+            VerifyBasic(test);
+        }
+
+        [Fact]
+        public void TestVBEventWithCustomDelegateWithExplicitByVal()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(ByVal sender As Object, ByVal e As EA)
+End Class
+
+Public Class EA
+    Inherits EventArgs
+End Class";
+
+            VerifyBasic(test);
+        }
+
+        [Fact]
+        public void TestVBEventWith3Parameters()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(ByVal sender As Object, ByVal e As EventArgs, ByVal i As Integer)
+End Class";
+
+            VerifyBasic(test, GetCA1009BasicResultAt(5, 18, "E"));
+        }
+
+        [Fact]
+        public void TestVBEventWithFirstParameterNotObjecType()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(ByVal sender As String, ByVal e As EventArgs)
+End Class";
+
+            VerifyBasic(test, GetCA1009BasicResultAt(5, 18, "E"));
+        }
+
+        [Fact]
+        public void TestVBEventWithSecondParameterNotInheritedFromEventArgs()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(ByVal sender As Object, ByVal e As ArgumentNullException)
+End Class";
+
+            VerifyBasic(test, GetCA1009BasicResultAt(5, 18, "E"));
+        }
+
+        [Fact]
+        public void TestVBEventWithNoParameters()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E()
+End Class";
+
+            VerifyBasic(test, GetCA1009BasicResultAt(5, 18, "E"));
+        }
+
+        [Fact]
+        public void TestVBEventWithFirstParameterByReference()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(ByRef sender As Object, ByVal e As EventArgs)
+End Class";
+
+            VerifyBasic(test, GetCA1009BasicResultAt(5, 18, "E"));
+        }
+
+        [Fact]
+        public void TestVBEventWithSecondParameterByReference()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E(ByVal sender As Object, ByRef e As EventArgs)
+End Class";
+
+            VerifyBasic(test, GetCA1009BasicResultAt(5, 18, "E"));
+        }
+
+        [Fact]
+        public void TestVBEventWithNoParametersWithScope()
+        {
+            var test = @"
+Imports System
+
+Public Class C
+    Public Event E()
+End Class
+
+[|Public Class D
+    Public Event E As EventHandler
+End Class|]
+";
+
+            VerifyBasic(test);
+        }
+
         internal static string CA1009Name = "CA1009";
         internal static string CA1009Message = MicrosoftApiDesignGuidelinesAnalyzersResources.DeclareEventHandlersCorrectlyMessage;
 

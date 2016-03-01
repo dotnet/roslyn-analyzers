8b68243287bb195c5c2b953201e18533e5cd7997
Matt Mitchell <mmitche@microsoft.com>
2016-03-01 15:33:25 -0800

    Move to netci configuration of jobs

---
+++ netci.groovy
@@ -0,0 +1,34 @@
+// Import the utility functionality.
+import jobs.generation.Utilities;
+
+// Defines a the new of the repo, used elsewhere in the file
+def project = GithubProject
+def branch = GithubBranchName
+
+// Generate the builds for debug and release, commit and PRJob
+[true, false].each { isPR -> // Defines a closure over true and false, value assigned to isPR
+    ['Debug', 'Release'].each { configuration ->
+        
+        def newJobName = Utilities.getFullJobName(project, "windows_${configuration.toLowerCase()}", isPR)
+
+        def newJob = job(newJobName) {
+            // This opens the set of build steps that will be run.
+            steps {
+                // Indicates that a batch script should be run with the build string (see above)
+                // Also available is:
+                // shell (for unix scripting)
+                batchFile("cibuild.cmd /${configuration.toLowerCase()}")
+            }
+        }
+        
+        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
+        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+        Utilities.addXUnitDotNETResults(newJob, "**/*TestResults.xml")
+        if (isPR) {
+            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows ${configuration}")
+        }
+        else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
+    }
+}

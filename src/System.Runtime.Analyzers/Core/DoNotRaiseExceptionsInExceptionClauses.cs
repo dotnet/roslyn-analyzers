635c6bf241c3a27af5836136995707bd524ba880
Srivatsn Narayanan <srivatsn@microsoft.com>
2016-03-30 10:35:46 -0700

    Implement CA2219 and add a simple test.

--- src/System.Runtime.Analyzers/Core/DoNotRaiseExceptionsInExceptionClauses.cs
+++ src/System.Runtime.Analyzers/Core/DoNotRaiseExceptionsInExceptionClauses.cs
@@ -4,13 +4,17 @@ using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Analyzer.Utilities;
+using Microsoft.CodeAnalysis.Semantics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace System.Runtime.Analyzers
 {
     /// <summary>
     /// CA2219: Do not raise exceptions in exception clauses
     /// </summary>
-    public abstract class DoNotRaiseExceptionsInExceptionClausesAnalyzer : DiagnosticAnalyzer
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotRaiseExceptionsInExceptionClausesAnalyzer : DiagnosticAnalyzer
     {
         internal const string RuleId = "CA2219";
 
@@ -20,39 +24,43 @@ namespace System.Runtime.Analyzers
         private static readonly LocalizableString s_localizableMessageFilter = new LocalizableResourceString(nameof(SystemRuntimeAnalyzersResources.DoNotRaiseExceptionsInExceptionClausesMessageFilter), SystemRuntimeAnalyzersResources.ResourceManager, typeof(SystemRuntimeAnalyzersResources));
         private static readonly LocalizableString s_localizableMessageFault = new LocalizableResourceString(nameof(SystemRuntimeAnalyzersResources.DoNotRaiseExceptionsInExceptionClausesMessageFault), SystemRuntimeAnalyzersResources.ResourceManager, typeof(SystemRuntimeAnalyzersResources));
         private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(SystemRuntimeAnalyzersResources.DoNotRaiseExceptionsInExceptionClausesDescription), SystemRuntimeAnalyzersResources.ResourceManager, typeof(SystemRuntimeAnalyzersResources));
+        private const string helpLinkUrl = "https://msdn.microsoft.com/en-us/library/bb386041.aspx";
 
         internal static DiagnosticDescriptor FinallyRule = new DiagnosticDescriptor(RuleId,
                                                                              s_localizableTitle,
                                                                              s_localizableMessageFinally,
                                                                              DiagnosticCategory.Usage,
                                                                              DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
+                                                                             isEnabledByDefault: true,
                                                                              description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
-                                                                             customTags: WellKnownDiagnosticTags.Telemetry);
-        internal static DiagnosticDescriptor FilterRule = new DiagnosticDescriptor(RuleId,
-                                                                             s_localizableTitle,
-                                                                             s_localizableMessageFilter,
-                                                                             DiagnosticCategory.Usage,
-                                                                             DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
-                                                                             description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
-                                                                             customTags: WellKnownDiagnosticTags.Telemetry);
-        internal static DiagnosticDescriptor FaultRule = new DiagnosticDescriptor(RuleId,
-                                                                             s_localizableTitle,
-                                                                             s_localizableMessageFault,
-                                                                             DiagnosticCategory.Usage,
-                                                                             DiagnosticSeverity.Warning,
-                                                                             isEnabledByDefault: false,
-                                                                             description: s_localizableDescription,
-                                                                             helpLinkUri: null,     // TODO: add MSDN url
+                                                                             helpLinkUri: helpLinkUrl,
                                                                              customTags: WellKnownDiagnosticTags.Telemetry);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(FinallyRule, FilterRule, FaultRule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(FinallyRule);
 
         public override void Initialize(AnalysisContext analysisContext)
         {
+            analysisContext.RegisterCompilationStartAction(compilationStartContext =>
+            {
+                Compilation compilation = compilationStartContext.Compilation;
+                INamedTypeSymbol exceptionType = WellKnownTypes.Exception(compilation);
+                if (exceptionType == null)
+                {
+                    return;
+                }
+
+                compilationStartContext.RegisterOperationAction(operationContext =>
+                {
+                    var tryStatement = operationContext.Operation as ITryStatement;
+
+                    var throwStatements = tryStatement.FinallyHandler.Statements.OfType<IThrowStatement>();
+
+                    foreach (var throwStatement in throwStatements)
+                    {
+                        operationContext.ReportDiagnostic(throwStatement.Syntax.CreateDiagnostic(FinallyRule));
+                    }
+                }, OperationKind.TryStatement);
+            });
         }
     }
 }
--- src/System.Runtime.Analyzers/UnitTests/DoNotRaiseExceptionsInExceptionClausesTests.cs
+++ src/System.Runtime.Analyzers/UnitTests/DoNotRaiseExceptionsInExceptionClausesTests.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.UnitTests;
+using Xunit;
 
 namespace System.Runtime.Analyzers.UnitTests
 {
@@ -9,12 +10,70 @@ namespace System.Runtime.Analyzers.UnitTests
     {
         protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
         {
-            return new BasicDoNotRaiseExceptionsInExceptionClausesAnalyzer();
+            return new DoNotRaiseExceptionsInExceptionClausesAnalyzer();
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
-            return new CSharpDoNotRaiseExceptionsInExceptionClausesAnalyzer();
+            return new DoNotRaiseExceptionsInExceptionClausesAnalyzer();
+        }
+
+        [Fact]
+        public void CSharpSimpleCase()
+        {
+            var code = @"
+using System;
+
+public class Test
+{
+    public void Method()
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch (ArgumentException e)
+        {
+            throw new Exception();
+        }
+        catch
+        {
+            throw new Exception();
+        }
+        finally
+        {
+            throw new Exception();
+        }
+    
+    }
+}
+";
+            VerifyCSharp(code,
+                GetCSharpResultAt(20, 13, DoNotRaiseExceptionsInExceptionClausesAnalyzer.FinallyRule));
+        }
+
+        [Fact]
+        public void BasicSimpleCase()
+        {
+            var code = @"
+Imports System
+
+Public Class Test
+    Public Sub Method()
+        Try
+            Throw New Exception()
+        Catch e As ArgumentException
+            Throw New Exception()
+        Catch
+            Throw New Exception()
+        Finally
+            Throw New Exception()
+        End Try    
+    End Sub
+End Class
+";
+            VerifyBasic(code,
+                GetBasicResultAt(20, 13, DoNotRaiseExceptionsInExceptionClausesAnalyzer.FinallyRule));
         }
     }
 }

7f363f8ad422b4c20444efa6c89d4aa00e8b2310
Nat Ayewah <nayewah@microsoft.com>
2016-04-04 19:18:16 -0700

    Add basic skipped TDD tests

--- src/Microsoft.ApiDesignGuidelines.Analyzers/UnitTests/ConsiderPassingBaseTypesAsParametersTests.cs
+++ src/Microsoft.ApiDesignGuidelines.Analyzers/UnitTests/ConsiderPassingBaseTypesAsParametersTests.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.UnitTests;
+using Xunit;
 
 namespace Microsoft.ApiDesignGuidelines.Analyzers.UnitTests
 {
@@ -15,6 +16,50 @@ namespace Microsoft.ApiDesignGuidelines.Analyzers.UnitTests
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new ConsiderPassingBaseTypesAsParametersAnalyzer();
+        }
+
+        [Fact(Skip = "TDD Todo")]
+        public void TestCSCheckForMemberReferences()
+        {
+            var code = @"
+class MyBase { public string Potato; public int Farm = 42; public void Chicken(){} }
+class MyDerived: MyBase {  }
+class Driver {
+    int foo(MyDerived d) {
+        return d.Farm;
+    }
+}
+";
+            VerifyCSharp(code, GetCA1011CSharpResultAt(2, 23, "C"));
+        }
+
+        [Fact(Skip = "TDD Todo")]
+        public void TestCSCheckForMemberInvocations()
+        {
+            var code = @"
+class MyBase { public string Potato; public int Farm = 42; public void Chicken(){} }
+class MyDerived: MyBase {  }
+class Driver {
+    int foo(MyDerived d) {
+        return d.Farm;
+    }
+}
+";
+            VerifyCSharp(code, GetCA1011CSharpResultAt(2, 23, "C"));
+        }
+
+        internal static readonly string CA1011Name = "CA1011";
+        internal static readonly string CA1011Message = MicrosoftApiDesignGuidelinesAnalyzersResources.ConsiderPassingBaseTypesAsParametersMessage;
+
+        // TODO replace objectName with full paramters
+        private static DiagnosticResult GetCA1011CSharpResultAt(int line, int column, string objectName)
+        {
+            return GetCSharpResultAt(line, column, CA1011Name, string.Format(CA1011Message, objectName));
+        }
+
+        private static DiagnosticResult GetCA1011BasicResultAt(int line, int column, string objectName)
+        {
+            return GetBasicResultAt(line, column, CA1011Name, string.Format(CA1011Message, objectName));
         }
     }
 }

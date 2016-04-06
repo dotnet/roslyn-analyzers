20780d61d7fef45910be0b2f25776011dbcd3820
Nat Ayewah <nayewah@microsoft.com>
2016-04-05 18:05:10 -0700

    Port analyzer Implementation and Setup basic tests

--- src/Microsoft.ApiDesignGuidelines.Analyzers/Core/ConsiderPassingBaseTypesAsParameters.cs
+++ src/Microsoft.ApiDesignGuidelines.Analyzers/Core/ConsiderPassingBaseTypesAsParameters.cs
@@ -1,12 +1,18 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Immutable;
+using Analyzer.Utilities;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Analyzer.Utilities;
+using Microsoft.CodeAnalysis.Semantics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace Microsoft.ApiDesignGuidelines.Analyzers
 {
+    using TMapParameterToType = Dictionary<IParameterSymbol, ITypeSymbol>;
+    using TMapParameterToTypeSet = Dictionary<IParameterSymbol, HashSet<ITypeSymbol>>;
+
     /// <summary>
     /// CA1011: Consider passing base types as parameters
     /// </summary>
@@ -32,8 +38,257 @@ namespace Microsoft.ApiDesignGuidelines.Analyzers
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-        public override void Initialize(AnalysisContext analysisContext)
+        public override void Initialize(AnalysisContext context)
         {
+            // perform analysis on each method block
+            context.RegisterOperationBlockStartAction(
+                (blockStartContext) =>
+                {
+                    IMethodSymbol containingMethod = blockStartContext.OwningSymbol as IMethodSymbol;
+
+                    if (containingMethod == null || containingMethod.IsOverride || containingMethod.IsVirtual) // TODO method.IsSpecialName
+                    {
+                        return;
+                    }
+
+                    // data structures for tracking derived type
+                    var mostDerivedTypeFor = new Dictionary<IParameterSymbol, ITypeSymbol>();
+                    var allDerivedTypesFor = new Dictionary<IParameterSymbol, HashSet<ITypeSymbol>>();
+
+                    // store type of each parameter
+                    //containingMethod.Parameters
+
+                    // 1. check the arguments of all method calls for a parameter
+                    //    E.g.  string Read1(FileStream s) => Read2(s);
+                    //          string Read2(Stream s) => s.Read();
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var argOper = operationContext.Operation as IArgument;
+                            IParameterSymbol parameter;
+                            if (argOper != null && (parameter = GetParameterSymbol(argOper.Value)) != null)
+                            {
+                                RecordParameterUse(parameter, argOper.Parameter.Type,
+                                    mostDerivedTypeFor, allDerivedTypesFor);
+                            }
+                        }, OperationKind.Argument);
+
+                    // 2. check for member references (fields, properties, etc)
+                    //    E.g.  bool CanRead(FileStream s) => s.CanRead; // no diagnostic because this is abstract
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IMemberReferenceExpression;
+                            IParameterSymbol parameter;
+                            if (oper != null && (parameter = GetParameterSymbol(oper.Instance)) != null)
+                            {
+                                RecordParameterUse(parameter, oper.Member.ContainingType,
+                                    mostDerivedTypeFor, allDerivedTypesFor);
+                            }
+                        },
+                        OperationKind.FieldReferenceExpression,
+                        OperationKind.MethodBindingExpression,
+                        OperationKind.PropertyReferenceExpression,
+                        OperationKind.EventReferenceExpression);
+
+                    // 3. check for member invocations
+                    //    E.g.  string ReadLine(FileStream s) => s.ReadLine();
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IInvocationExpression;
+                            IParameterSymbol parameter;
+                            if (oper != null && (parameter = GetParameterSymbol(oper.Instance)) != null)
+                            {
+                                RecordParameterUse(parameter, oper.TargetMethod.ContainingType,
+                                    mostDerivedTypeFor, allDerivedTypesFor);
+                            }
+                        },
+                        OperationKind.InvocationExpression);
+
+                    // 4a. no warning
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IIsTypeExpression;
+                            IParameterSymbol parameter;
+                            if (oper != null && (parameter = GetParameterSymbol(oper.Operand)) != null)
+                            {
+                                // prevent diagnostic for this parameter
+                                RecordParameterUse(parameter, parameter.Type,
+                                    mostDerivedTypeFor, allDerivedTypesFor);
+                            }
+                        },
+                        OperationKind.IsTypeExpression);
+
+                    // 4b. shut down analysis for conversions
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IConversionExpression;
+                            IParameterSymbol parameter;
+                            if (oper != null && (parameter = GetParameterSymbol(oper.Operand)) != null)
+                            {
+                                // prevent diagnostic for this parameter
+                                RecordParameterUse(parameter, parameter.Type,
+                                    mostDerivedTypeFor, allDerivedTypesFor);
+                            }
+                        },
+                        OperationKind.ConversionExpression);
+
+                    // 5a. shut down analysis for variable declarations
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IVariableDeclarationStatement;
+                            IParameterSymbol parameter;
+                            foreach (var var in oper.Variables)
+                            {
+                                if ((parameter = GetParameterSymbol(var.InitialValue)) != null)
+                                {
+                                    // prevent diagnostic for this parameter
+                                    RecordParameterUse(parameter, parameter.Type,
+                                        mostDerivedTypeFor, allDerivedTypesFor);
+                                }
+                            }
+                        },
+                        OperationKind.VariableDeclarationStatement);
+
+                    // 5a. shut down analysis for assignments
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IAssignmentExpression;
+                            IParameterSymbol parameter;
+                            if (oper != null && (parameter = GetParameterSymbol(oper.Value)) != null)
+                            {
+                                // prevent diagnostic for this parameter
+                                RecordParameterUse(parameter, parameter.Type,
+                                    mostDerivedTypeFor, allDerivedTypesFor); // TODO iterate through child values
+                            }
+                        },
+                        OperationKind.AssignmentExpression,
+                        OperationKind.CompoundAssignmentExpression);
+
+                    // tester
+                    blockStartContext.RegisterOperationAction(
+                        (operationContext) =>
+                        {
+                            var oper = operationContext.Operation as IBlockStatement;
+                            var desc = oper?.Descendants();
+                            var names = desc?.Select(v => v.Syntax.ToString());
+                            var kinds = desc?.Select(v => v.Kind);
+                            if (oper != null) { }
+                        },
+                        OperationKind.BlockStatement);
+
+                    // check parameter maps and fire diagnostics as appropriate
+                    blockStartContext.RegisterOperationBlockEndAction(
+                        (blockEndContext) =>
+                        {
+                            CheckParameters(blockEndContext, mostDerivedTypeFor, allDerivedTypesFor);
+                        });
+                });
+        }
+
+
+        private void CheckParameters(
+            OperationBlockAnalysisContext context,
+            TMapParameterToType mostDerivedTypeFor,
+            TMapParameterToTypeSet allDerivedTypesFor)
+        {
+            foreach (var pair in mostDerivedTypeFor)
+            {
+                IParameterSymbol parameter = pair.Key;
+                ITypeSymbol paramType = parameter.Type;
+                ITypeSymbol mostDerived = pair.Value;
+
+                if (mostDerived != null && !mostDerived.Equals(paramType))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, parameter.Locations.FirstOrDefault(), 
+                        parameter.Name,
+                        parameter.ContainingSymbol?.Name,
+                        paramType,
+                        mostDerived));
+                }
+            }
+        }
+
+        private void RecordParameterUse(
+            IParameterSymbol parameter,
+            ITypeSymbol usageType,
+            TMapParameterToType mostDerivedTypeFor,
+            TMapParameterToTypeSet allDerivedTypesFor)
+        {
+            var paramType = parameter.Type;
+
+            if (parameter.IsThis
+                || parameter.IsImplicitlyDeclared // TODO add is Out parameter
+                || paramType.IsValueType
+                || paramType.SpecialType != SpecialType.None // ignore all the special types
+                || paramType.IsValueType
+                )
+            {
+                return;
+            }
+
+            if (!IsAssignableTo(paramType, usageType))
+            {
+                usageType = paramType;
+            }
+
+            ITypeSymbol mostDerivedTypeSoFar;
+            if (!mostDerivedTypeFor.TryGetValue(parameter, out mostDerivedTypeSoFar)
+                || IsAssignableTo(usageType, mostDerivedTypeSoFar))
+            {
+                mostDerivedTypeFor[parameter] = usageType;
+            }
+
+            // TODO All Derived Types
+        }
+
+        static IParameterSymbol GetParameterSymbol(IOperation value)
+        {
+            if (value?.Kind == OperationKind.ConversionExpression)
+            {
+                value = ((IConversionExpression)value).Operand;
+            }
+
+            return (value as IParameterReferenceExpression)?.Parameter;
+        }
+
+        static bool IsAssignableTo(ITypeSymbol derivedType, ITypeSymbol baseType)
+        {
+            if (derivedType.Equals(baseType))
+            {
+                return true;
+            }
+
+            if (derivedType.TypeKind == TypeKind.Class || derivedType.TypeKind == TypeKind.Structure)
+            {
+                INamedTypeSymbol derivedBaseType = derivedType.BaseType;
+                return derivedBaseType != null && (derivedBaseType.Equals(baseType) || IsAssignableTo(derivedBaseType, baseType));
+            }
+
+            else if (derivedType.TypeKind == TypeKind.Interface)
+            {
+                if (derivedType.Interfaces.Contains(baseType))
+                {
+                    return true;
+                }
+
+                foreach (INamedTypeSymbol baseInterface in derivedType.Interfaces)
+                {
+                    if (IsAssignableTo(baseInterface, baseType))
+                    {
+                        return true;
+                    }
+                }
+
+                return baseType.TypeKind == TypeKind.Class && baseType.SpecialType == SpecialType.System_Object;
+            }
+
+            return false;
         }
     }
 }
--- src/Microsoft.ApiDesignGuidelines.Analyzers/UnitTests/ConsiderPassingBaseTypesAsParametersTests.cs
+++ src/Microsoft.ApiDesignGuidelines.Analyzers/UnitTests/ConsiderPassingBaseTypesAsParametersTests.cs
@@ -2,10 +2,51 @@
 
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.UnitTests;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Microsoft.ApiDesignGuidelines.Analyzers.UnitTests
 {
+#if false
+    class SomethingBase
+    {
+        public int Value = 42;
+        public void SomeAction() { }
+        public int PropVal { get; set; }
+        public string PropGetVal { get; set; }
+        public event System.EventHandler OnNothingGood;
+        public virtual void SomeVirtualAction() { }
+        public void SomeHiddenAction() { }
+    }
+    class Something: SomethingBase {
+        public int SubValue = 32;
+        public override void SomeVirtualAction() { }
+        public new void SomeHiddenAction() { }
+    }
+    class Driver
+    {
+        int FieldRef(Something p1) { return p1.Value; }
+        void MethodBind(Something p2) { p2.SomeAction(); }
+        int PropertyRef(Something p3) { return p3.PropVal; }
+        string ROPropertyRef(Something p4) { return p4.PropGetVal ?? p4.Value.ToString(); }
+        void EventRef(Something p5)
+        {
+            p5.OnNothingGood += (object sender, System.EventArgs e) => { p5.SubValue = 2; };
+        }
+
+        string AccessInLambda(Something p5) {
+            Func<string> getV = () => p5.SubValue.ToString();
+            return p5.PropGetVal ?? getV();
+        }
+
+        async Task<string> AccessInAsyncTask(Something p6)
+        {
+            return p6.PropGetVal ?? await Task.Run(() => p6.SubValue.ToString());
+        }
+    }
+#endif
+
     public class ConsiderPassingBaseTypesAsParametersTests : DiagnosticAnalyzerTestBase
     {
         protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
@@ -18,43 +59,82 @@ namespace Microsoft.ApiDesignGuidelines.Analyzers.UnitTests
             return new ConsiderPassingBaseTypesAsParametersAnalyzer();
         }
 
-        [Fact(Skip = "TDD Todo")]
-        public void TestCSCheckForMemberReferences()
-        {
-            var code = @"
-class MyBase { public string Potato; public int Farm = 42; public void Chicken(){} }
-class MyDerived: MyBase {  }
-class Driver {
-    int foo(MyDerived d) {
-        return d.Farm;
+        const string CommonSomething = @"
+    using System;
+    using System.Threading.Tasks;
+
+    class SomethingBase
+    {
+        public int Value = 42;
+        public void SomeAction() { }
+        public int PropVal { get; set; }
+        public string PropGetVal { get; set; }
+        public event EventHandler OnNothingGood;
+        public virtual void SomeVirtualAction() { }
     }
-}
+    class Something: SomethingBase {
+        public int SubValue = 32;
+        public override void SomeVirtualAction() { }
+    }
 ";
-            VerifyCSharp(code, GetCA1011CSharpResultAt(2, 23, "C"));
+
+        [Fact]
+        public void TestCSCheckForBaseMemberReferences()
+        {
+            // Test methods which only access base members (i.e. should report diagnostic)
+            var code = CommonSomething + @"
+    class BaseMemberReferences
+    {
+        int FieldRef(Something p1) { return p1.Value; }
+        void MethodBind(Something p2) { p2.SomeAction(); }
+        int PropertyRef(Something p3) { return p3.PropVal; }
+        string ROPropertyRef(Something p4) { return p4.PropGetVal ?? ""Nop""; }
+        string MultiMemberRef(Something p5) { return p5.PropGetVal ?? p5.Value.ToString(); }
+    }
+";
+            var derivedClass = "Something";
+            var baseClass = "SomethingBase";
+
+            VerifyCSharp(code, 
+                GetCA1011CSharpResultAt(21, 32, "p1", "FieldRef", derivedClass, baseClass),
+                GetCA1011CSharpResultAt(22, 35, "p2", "MethodBind", derivedClass, baseClass),
+                GetCA1011CSharpResultAt(23, 35, "p3", "PropertyRef", derivedClass, baseClass),
+                GetCA1011CSharpResultAt(24, 40, "p4", "ROPropertyRef", derivedClass, baseClass),
+                GetCA1011CSharpResultAt(25, 41, "p5", "MultiMemberRef", derivedClass, baseClass) );
         }
 
-        [Fact(Skip = "TDD Todo")]
-        public void TestCSCheckForMemberInvocations()
+        [Fact]
+        public void TestCSCheckForDerivedMemberReferences()
         {
-            var code = @"
-class MyBase { public string Potato; public int Farm = 42; public void Chicken(){} }
-class MyDerived: MyBase {  }
-class Driver {
-    int foo(MyDerived d) {
-        return d.Farm;
+            // Test methods which only access base AND derived members (i.e. should NOT report diagnostic)
+            var code = CommonSomething + @"
+    class DerivedMemberReferences
+    {
+        int FieldRef(Something p1) { return p1.SubValue; }
+        void MethodBind(Something p2) { p2.SomeVirtualAction(); }
+        int PropertyAndField(Something p3) { return p3.PropVal + p3.SubValue; }
+
+        string AccessInLambda(Something p5) {
+            Func<string> getV = () => p5.SubValue.ToString();
+            return p5.PropGetVal ?? getV();
+        }
+
+        async Task<string> AccessInAsyncTask(Something p6)
+        {
+            return p6.PropGetVal ?? await Task.Run(() => p6.SubValue.ToString());
+        }
     }
-}
 ";
-            VerifyCSharp(code, GetCA1011CSharpResultAt(2, 23, "C"));
+            VerifyCSharp(code);
         }
 
         internal static readonly string CA1011Name = "CA1011";
         internal static readonly string CA1011Message = MicrosoftApiDesignGuidelinesAnalyzersResources.ConsiderPassingBaseTypesAsParametersMessage;
 
-        // TODO replace objectName with full paramters
-        private static DiagnosticResult GetCA1011CSharpResultAt(int line, int column, string objectName)
+        // Message Args: paramName, methodName, paramType, baseType
+        private static DiagnosticResult GetCA1011CSharpResultAt(int line, int column, params string [] messageArgs)
         {
-            return GetCSharpResultAt(line, column, CA1011Name, string.Format(CA1011Message, objectName));
+            return GetCSharpResultAt(line, column, CA1011Name, string.Format(CA1011Message, messageArgs));
         }
 
         private static DiagnosticResult GetCA1011BasicResultAt(int line, int column, string objectName)

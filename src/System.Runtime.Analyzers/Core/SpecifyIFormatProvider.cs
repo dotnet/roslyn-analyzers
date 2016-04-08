2adedd3938195a2f5f39bc824242830bec32f6e3
Balaji Soundrarajan <basoundr@microsoft.com>
2016-04-07 17:08:58 -0700

    Compute correct Overload passing info about parameter instead of string comparison

--- src/System.Runtime.Analyzers/Core/SpecifyIFormatProvider.cs
+++ src/System.Runtime.Analyzers/Core/SpecifyIFormatProvider.cs
@@ -80,17 +80,34 @@ namespace System.Runtime.Analyzers
                     return;
                 }
 
+                var objectType = csaContext.Compilation.GetSpecialType(SpecialType.System_Object);
                 var stringType = csaContext.Compilation.GetSpecialType(SpecialType.System_String);
                 var stringFormatMembers = stringType?.GetMembers("Format").OfType<IMethodSymbol>();
-                var stringFormatMemberWithStringAndObjectParameter = GetSingleOrDefaultMemberWithName(stringFormatMembers, "string.Format(string, object)");
-                var stringFormatMemberWithStringObjectAndObjectParameter = GetSingleOrDefaultMemberWithName(stringFormatMembers, "string.Format(string, object, object)");
-                var stringFormatMemberWithStringObjectObjectAndObjectParameter = GetSingleOrDefaultMemberWithName(stringFormatMembers, "string.Format(string, object, object, object)");
 
-                var paramsObjectArraySymbolDisplay = csaContext.Compilation.Language == LanguageNames.CSharp ?
-                                                     "params object[]" :
-                                                     "ParamArray Object()";
-                var stringFormatMemberWithStringAndParamsObjectParameter = GetSingleOrDefaultMemberWithName(stringFormatMembers, $"string.Format(string, {paramsObjectArraySymbolDisplay})");
-                var stringFormatMemberWithIFormatProviderStringAndParamsObjectParameter = GetSingleOrDefaultMemberWithName(stringFormatMembers, $"string.Format(IFormatProvider, string, {paramsObjectArraySymbolDisplay})");
+                var stringFormatMemberWithStringAndObjectParameter = GetSingleOrDefaultMemberWithParameterInfos(
+                                                                         stringFormatMembers, 
+                                                                         GetParameterInfo(stringType), 
+                                                                         GetParameterInfo(objectType));
+                var stringFormatMemberWithStringObjectAndObjectParameter = GetSingleOrDefaultMemberWithParameterInfos(
+                                                                               stringFormatMembers,
+                                                                               GetParameterInfo(stringType),
+                                                                               GetParameterInfo(objectType),
+                                                                               GetParameterInfo(objectType));
+                var stringFormatMemberWithStringObjectObjectAndObjectParameter = GetSingleOrDefaultMemberWithParameterInfos(
+                                                                                     stringFormatMembers,
+                                                                                     GetParameterInfo(stringType),
+                                                                                     GetParameterInfo(objectType),
+                                                                                     GetParameterInfo(objectType),
+                                                                                     GetParameterInfo(objectType));
+                var stringFormatMemberWithStringAndParamsObjectParameter = GetSingleOrDefaultMemberWithParameterInfos(
+                                                                               stringFormatMembers, 
+                                                                               GetParameterInfo(stringType),
+                                                                               GetParameterInfo(objectType, isArray: true, arrayRank: 1, isParams: true));
+                var stringFormatMemberWithIFormatProviderStringAndParamsObjectParameter = GetSingleOrDefaultMemberWithParameterInfos(
+                                                                                              stringFormatMembers,
+                                                                                              GetParameterInfo(iformatProviderType),
+                                                                                              GetParameterInfo(stringType),
+                                                                                              GetParameterInfo(objectType, isArray: true, arrayRank: 1, isParams: true));
 
                 var currentCultureProperty = cultureInfoType?.GetMembers("CurrentCulture").OfType<IPropertySymbol>().SingleOrDefault();
                 var invariantCultureProperty = cultureInfoType?.GetMembers("InvariantCulture").OfType<IPropertySymbol>().SingleOrDefault();
@@ -218,9 +235,72 @@ namespace System.Runtime.Analyzers
                 .Select(x => x.Index);
         }
 
-        private static IMethodSymbol GetSingleOrDefaultMemberWithName(IEnumerable<IMethodSymbol> stringFormatMembers, string displayName)
+        private ParameterInfo GetParameterInfo(INamedTypeSymbol type, bool isArray = false, int arrayRank = 0, bool isParams = false)
         {
-            return stringFormatMembers?.Where(member => string.Equals(member.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat), displayName, StringComparison.OrdinalIgnoreCase)).SingleOrDefault();
+            return ParameterInfo.GetParameterInfo(type, isArray, arrayRank, isParams);
+        }
+
+        private IMethodSymbol GetSingleOrDefaultMemberWithParameterInfos(IEnumerable<IMethodSymbol> members, params ParameterInfo[] expectedParameterTypesInOrder)
+        {
+            var expectedParameterCount = expectedParameterTypesInOrder.Count();
+            return members?.Where(member =>
+            {
+                if (member.Parameters.Count() != expectedParameterCount)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < expectedParameterCount; i++)
+                {
+                    // check IsParams only on the last parameter
+                    if (i == expectedParameterCount - 1 && 
+                        member.Parameters[i].IsParams != expectedParameterTypesInOrder[i].IsParams)
+                    {
+                        return false;
+                    }
+
+                    var parameterType = member.Parameters[i].Type;
+                    if (expectedParameterTypesInOrder[i].IsArray)
+                    {
+                        var arrayParameterSymbol = parameterType as IArrayTypeSymbol;
+                        if(arrayParameterSymbol?.Rank != expectedParameterTypesInOrder[i].ArrayRank)
+                        {
+                            return false;
+                        }
+
+                        parameterType = arrayParameterSymbol.ElementType;
+                    }
+
+                    if (!expectedParameterTypesInOrder[i].Type.Equals(parameterType))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }).SingleOrDefault();
+        }
+
+        // Contains the expected properties of a parameter
+        private class ParameterInfo
+        {
+            public int ArrayRank { get; private set; }
+            public bool IsArray { get; private set; }
+            public bool IsParams { get; private set; }
+            public INamedTypeSymbol Type { get; private set; }
+
+            private ParameterInfo(INamedTypeSymbol type, bool isArray, int arrayRank, bool isParams)
+            {
+                Type = type;
+                IsArray = isArray;
+                ArrayRank = arrayRank;
+                IsParams = isParams;
+            }
+
+            public static ParameterInfo GetParameterInfo(INamedTypeSymbol type, bool isArray, int arrayRank, bool isParams)
+            {
+                return new ParameterInfo(type, isArray, arrayRank, isParams);
+            }
         }
     }
 }

624d3cc7c1d97ba23806b509d9ffb4e43288942d
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-05-20 11:37:42 +0200

    Fixed compiler errors in System.Resources.Analyzers.UnitTests

--- src/System.Resources.Analyzers/UnitTests/MarkAssembliesWithNeutralResourcesLanguageTests.cs
+++ src/System.Resources.Analyzers/UnitTests/MarkAssembliesWithNeutralResourcesLanguageTests.cs
@@ -30,8 +30,7 @@ Namespace My.Resources
     <Global.System.CodeDom.Compiler.GeneratedCodeAttribute(""System.Resources.Tools.StronglyTypedResourceBuilder"", ""4.0.0.0"")> _
     Friend Class Resource1
     End Class
-End Namespace
-}";
+End Namespace";
 
         [Fact]
         public void TestCSharpNoResourceFile()

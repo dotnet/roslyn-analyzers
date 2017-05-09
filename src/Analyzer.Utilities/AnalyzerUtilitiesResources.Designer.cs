adbcd397d54cc75259b0449f84afc7c745a3d177
Manish Vasani <mavasani@microsoft.com>
2017-05-09 16:08:22 -0700

    Fix typo in message

--- src/Analyzer.Utilities/AnalyzerUtilitiesResources.Designer.cs
+++ src/Analyzer.Utilities/AnalyzerUtilitiesResources.Designer.cs
@@ -161,7 +161,7 @@ namespace Analyzer.Utilities {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Analyzers in this package are preview version and are tied to a specific API version of Microsoft.CodeAnalysis. You have a mistatch between the analyzer and Microsoft.CodeAnalysis version and should switch your analyzer NuGet package/VSIX to a matching version of the Microsoft.CodeAnalysis..
+        ///   Looks up a localized string similar to Analyzers in this package are preview version and are tied to a specific API version of Microsoft.CodeAnalysis. You have a mismatch between the analyzer and Microsoft.CodeAnalysis version and should switch your analyzer NuGet package/VSIX to a matching version of the Microsoft.CodeAnalysis..
         /// </summary>
         internal static string VersionCheckDescription {
             get {

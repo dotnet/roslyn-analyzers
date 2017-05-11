3bf1876e6f3ba025d6c98f934603495fb4ef4bdd
Fredric Silberberg <frsilb@microsoft.com>
2017-05-11 13:59:13 -0700

    Added dummy package for detection in VS

---
+++ src/FxCopAnalyzersSetup/FxCopAnalyzersSetup/FxCopAnalyzersPackage.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace FxCopAnalyzersSetup
+{
+    [Guid(PackageGuid)]
+    class FxCopAnalyzersPackage : Package
+    {
+        private const string PackageGuid = "4A41D270-A97F-4639-A352-28732FC410E4";
+    }
+}

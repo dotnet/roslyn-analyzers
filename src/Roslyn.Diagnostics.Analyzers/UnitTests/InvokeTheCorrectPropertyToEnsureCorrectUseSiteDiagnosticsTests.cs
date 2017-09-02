96334b1107245477bee48f2b13c52fd6b6e4593b
Julien Couvreur <jcouv@microsoft.com>
2017-09-01 18:11:23 -0700

    Add C# Roslyn analyzer for TypeArguments and other properties to avoid

--- src/Roslyn.Diagnostics.Analyzers/UnitTests/InvokeTheCorrectPropertyToEnsureCorrectUseSiteDiagnosticsTests.cs
+++ src/Roslyn.Diagnostics.Analyzers/UnitTests/InvokeTheCorrectPropertyToEnsureCorrectUseSiteDiagnosticsTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Diagnostics.CSharp.Analyzers;
 using Roslyn.Diagnostics.VisualBasic.Analyzers;
 using Test.Utilities;
 using Xunit;
@@ -16,11 +17,11 @@ namespace Roslyn.Diagnostics.Analyzers.UnitTests
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
-            return null;
+            return new CSharpInvokeTheCorrectPropertyToEnsureCorrectUseSiteDiagnosticsAnalyzer();
         }
 
         [Fact]
-        public void TestTypeArgumentsInVB()
+        public void TestTypeArgumentsInBasic()
         {
             var source = @"
 Namespace Microsoft.CodeAnalysis.VisualBasic.Symbols
@@ -42,6 +43,38 @@ End Class
 ";
 
             VerifyBasic(source, GetRS0004BasicResultAt(14, 19));
+        }
+
+        [Fact]
+        public void TestTypeArgumentsInCSharp()
+        {
+            var source = @"
+namespace Microsoft.CodeAnalysis.CSharp.Symbols
+{
+    public class NamedTypeSymbol
+    {
+        public int TypeArguments
+        {
+            get => 1;
+        }
+    }
+}
+class C
+{
+    void M(Microsoft.CodeAnalysis.CSharp.Symbols.NamedTypeSymbol c)
+    {
+        var x = c.TypeArguments;
+        System.Console.Write(x);
+    }
+}
+";
+
+            VerifyCSharp(source, GetRS0004CSharpResultAt(16, 19));
+        }
+
+        private static DiagnosticResult GetRS0004CSharpResultAt(int line, int column, params string[] args)
+        {
+            return GetCSharpResultAt(line, column, CSharpInvokeTheCorrectPropertyToEnsureCorrectUseSiteDiagnosticsAnalyzer.Rule, args);
         }
 
         private static DiagnosticResult GetRS0004BasicResultAt(int line, int column, params string[] args)

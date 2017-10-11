dadde2163a543e5b997defa17c0e49ec2cf04e44
Manish Vasani <mavasani@microsoft.com>
2017-10-11 01:40:40 -0700

    Revert "Disable DoNotCatchCorruptedStateExceptionsAnalyzer CA2153"
    This reverts commit 1ac4bae56bc41c93a514bd9a82cdfaa7a50f50b5.

--- src/Microsoft.NetFramework.Analyzers/Core/DoNotCatchCorruptedStateExceptions.cs
+++ src/Microsoft.NetFramework.Analyzers/Core/DoNotCatchCorruptedStateExceptions.cs
@@ -12,10 +12,8 @@ using Microsoft.CodeAnalysis.Semantics;
 
 namespace Microsoft.NetFramework.Analyzers
 {
-    //[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
-#pragma warning disable RS1001 // Missing diagnostic analyzer attribute.
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
     public sealed class DoNotCatchCorruptedStateExceptionsAnalyzer : DiagnosticAnalyzer
-#pragma warning restore RS1001 // Missing diagnostic analyzer attribute.
     {
         internal const string RuleId = "CA2153";
 
@@ -40,44 +38,43 @@ namespace Microsoft.NetFramework.Analyzers
             analysisContext.EnableConcurrentExecution();
             analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
 
-            //analysisContext.RegisterCompilationStartAction(compilationStartAnalysisContext =>
-            //{
-            //    var compilationTypes = new CompilationSecurityTypes(compilationStartAnalysisContext.Compilation);
-            //    if (compilationTypes.HandleProcessCorruptedStateExceptionsAttribute == null)
-            //    {
-            //        return;
-            //    }
+            analysisContext.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+            {
+                var compilationTypes = new CompilationSecurityTypes(compilationStartAnalysisContext.Compilation);
+                if (compilationTypes.HandleProcessCorruptedStateExceptionsAttribute == null)
+                {
+                    return;
+                }
 
-            //    compilationStartAnalysisContext.RegisterOperationBlockActionInternal(operationBlockAnalysisContext =>
-            //    {
-            //        if (operationBlockAnalysisContext.OwningSymbol.Kind != SymbolKind.Method)
-            //        {
-            //            return;
-            //        }
+                compilationStartAnalysisContext.RegisterOperationBlockActionInternal(operationBlockAnalysisContext =>
+                {
+                    if (operationBlockAnalysisContext.OwningSymbol.Kind != SymbolKind.Method)
+                    {
+                        return;
+                    }
 
-            //        var method = (IMethodSymbol)operationBlockAnalysisContext.OwningSymbol;
+                    var method = (IMethodSymbol)operationBlockAnalysisContext.OwningSymbol;
 
-            //        if (!ContainsHandleProcessCorruptedStateExceptionsAttribute(method, compilationTypes))
-            //        {
-            //            return;
-            //        }
+                    if (!ContainsHandleProcessCorruptedStateExceptionsAttribute(method, compilationTypes))
+                    {
+                        return;
+                    }
 
-            //        foreach (var operation in operationBlockAnalysisContext.OperationBlocks)
-            //        {
-            //            var walker = new EmptyThrowInsideCatchAllWalker(compilationTypes);
-            //            walker.Visit(operation);
+                    foreach (var operation in operationBlockAnalysisContext.OperationBlocks)
+                    {
+                        var walker = new EmptyThrowInsideCatchAllWalker(compilationTypes);
+                        walker.Visit(operation);
 
-            //            foreach (var catchClause in walker.CatchAllCatchClausesWithoutEmptyThrow)
-            //            {
-            //                operationBlockAnalysisContext.ReportDiagnostic(catchClause.Syntax.CreateDiagnostic(Rule,
-            //                    method.ToDisplayString()));
-            //            }
-            //        }
-            //    });
-            //});
+                        foreach (var catchClause in walker.CatchAllCatchClausesWithoutEmptyThrow)
+                        {
+                            operationBlockAnalysisContext.ReportDiagnostic(catchClause.Syntax.CreateDiagnostic(Rule,
+                                method.ToDisplayString()));
+                        }
+                    }
+                });
+            });
         }
 
-    /*
         private bool ContainsHandleProcessCorruptedStateExceptionsAttribute(IMethodSymbol method, CompilationSecurityTypes compilationTypes)
         {
             ImmutableArray<AttributeData> attributes = method.GetAttributes();
@@ -139,6 +136,5 @@ namespace Microsoft.NetFramework.Analyzers
                        caughtType == _compilationTypes.SystemObject;
             }
         }
-    */
-}
+    }
 }

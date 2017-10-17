080ecb6fdaed5957de2d984ba799a5d03738fd9d
Tomas Matousek <tomat@microsoft.com>
2017-10-16 19:08:15 -0700

    Update to RepoToolset 43

--- build/build.ps1
+++ build/build.ps1
@@ -14,12 +14,49 @@ Param(
   [switch] $pack,
   [switch] $ci,
   [switch] $clearCaches,
+  [switch] $log,
+  [switch] $help,
   [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
 )
 
 set-strictmode -version 2.0
 $ErrorActionPreference = "Stop"
 
+function Print-Usage() {
+    Write-Host "Common settings:"
+    Write-Host "  -configuration <value>  Build configuration Debug, Release"
+    Write-Host "  -verbosity <value>      Msbuild verbosity (q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic])"
+    Write-Host "  -help                   Print help and exit"
+    Write-Host ""
+
+    Write-Host "Actions:"
+    Write-Host "  -restore                Restore dependencies"
+    Write-Host "  -build                  Build solution"
+    Write-Host "  -rebuild                Rebuild solution"
+    Write-Host "  -deploy                 Deploy built VSIXes"
+    Write-Host "  -deployDeps             Deploy dependencies (Roslyn VSIXes for integration tests)"
+    Write-Host "  -test                   Run all unit tests in the solution"
+    Write-Host "  -integrationTest        Run all integration tests in the solution"
+    Write-Host "  -sign                   Sign build outputs"
+    Write-Host "  -pack                   Package build outputs into NuGet packages and Willow components"
+    Write-Host ""
+
+    Write-Host "Advanced settings:"
+    Write-Host "  -solution <value>       Path to solution to build"
+    Write-Host "  -ci                     Set when running on CI server"
+    Write-Host "  -log                    Enable logging (by default on CI)"
+    Write-Host "  -clearCaches            Prepare machine for CI run"
+    Write-Host ""
+    Write-Host "Command line arguments not listed above are passed thru to msbuild."
+    Write-Host "The above arguments can be shortened as much as to be unambiguous (e.g. -co for configuration, -t for test, etc.)."
+}
+
+if ($help -or (($properties -ne $null) -and ($properties.Contains("/help") -or $properties.Contains("/?")))) {
+  Print-Usage
+  exit 0
+}
+
+$InVSEnvironment = !($env:VS150COMNTOOLS -eq $null) -and (Test-Path $env:VS150COMNTOOLS)
 $RepoRoot = Join-Path $PSScriptRoot "..\"
 $ToolsRoot = Join-Path $RepoRoot ".tools"
 $BuildProj = Join-Path $PSScriptRoot "build.proj"
@@ -40,47 +77,54 @@ function GetVSWhereVersion {
   return $xml.Project.PropertyGroup.VSWhereVersion
 }
 
-function LocateMsbuild {
-  
+function LocateVisualStudio {
+  if ($InVSEnvironment) {
+    return Join-Path $env:VS150COMNTOOLS "..\.."
+  }
+
   $vswhereVersion = GetVSWhereVersion
   $vsWhereDir = Join-Path $ToolsRoot "vswhere\$vswhereVersion"
   $vsWhereExe = Join-Path $vsWhereDir "vswhere.exe"
-    
+  
   if (!(Test-Path $vsWhereExe)) {
     Create-Directory $vsWhereDir   
+    Write-Host "Downloading vswhere"
     Invoke-WebRequest "http://github.com/Microsoft/vswhere/releases/download/$vswhereVersion/vswhere.exe" -OutFile $vswhereExe
   }
   
   $vsInstallDir = & $vsWhereExe -latest -property installationPath -requires Microsoft.Component.MSBuild -requires Microsoft.VisualStudio.Component.VSSDK -requires Microsoft.Net.Component.4.6.TargetingPack -requires Microsoft.VisualStudio.Component.Roslyn.Compiler -requires Microsoft.VisualStudio.Component.VSSDK
-  $msbuildExe = Join-Path $vsInstallDir "MSBuild\15.0\Bin\msbuild.exe"
   
-  if (!(Test-Path $msbuildExe)) {
-    throw "Failed to locate msbuild (exit code '$lastExitCode')."
+  if (!(Test-Path $vsInstallDir)) {
+    throw "Failed to locate Visual Studio (exit code '$lastExitCode')."
   }
 
-  return $msbuildExe
+  return $vsInstallDir
 }
 
 function Build {
-  $msbuildExe = LocateMsbuild
-  
-  if ($ci) {
-  Create-Directory($logDir)
-    # Microbuild is on 15.1 which doesn't support binary log
-    if ($env:BUILD_BUILDNUMBER -eq "") {
-      $log = "/bl:" + (Join-Path $LogDir "Build.binlog")
-    } else {
-      $log = "/flp1:Summary;Verbosity=diagnostic;Encoding=UTF-8;LogFile=" + (Join-Path $LogDir "Build.log")
-    }
+  $vsInstallDir = LocateVisualStudio
+  $msbuildExe = Join-Path $vsInstallDir "MSBuild\15.0\Bin\msbuild.exe"
+
+  if (!$InVSEnvironment) {
+    $env:VS150COMNTOOLS = Join-Path $vsInstallDir "Common7\Tools\"
+    $env:VSSDK150Install = Join-Path $vsInstallDir "VSSDK\"
+    $env:VSSDKInstall = Join-Path $vsInstallDir "VSSDK\"
+  }
+
+  if ($ci -or $log) {
+    Create-Directory($logDir)
+    $logCmd = "/bl:" + (Join-Path $LogDir "Build.binlog")
   } else {
-    $log = ""
+    $logCmd = ""
   }
 
-  & $msbuildExe $BuildProj /m /v:$verbosity $log /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:DeployDeps=$deployDeps /p:Build=$build /p:Rebuild=$rebuild /p:Deploy=$deploy /p:Test=$test /p:IntegrationTest=$integrationTest /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci  /p:RestoreConfigFile=$NugetConfig $properties
-
-  if ($lastExitCode -ne 0) {
-    throw "Build failed (exit code '$lastExitCode')."
+  if ($ci) {
+    Write-Host "Using $msbuildExe"
   }
+
+  $nodeReuse = !$ci
+
+  & $msbuildExe $BuildProj /m /nologo /clp:Summary /nodeReuse:$nodeReuse /warnaserror /v:$verbosity $logCmd /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:DeployDeps=$deployDeps /p:Build=$build /p:Rebuild=$rebuild /p:Deploy=$deploy /p:Test=$test /p:IntegrationTest=$integrationTest /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci /p:RestoreConfigFile=$NugetConfig $properties
 }
 
 function Stop-Processes() {
@@ -104,12 +148,13 @@ try {
     $env:TMP = $TempDir
   }
 
+  # Preparation of a CI machine
   if ($clearCaches) {
     Clear-NuGetCache
   }
 
   Build
-  exit 0
+  exit $lastExitCode
 }
 catch {
   Write-Host $_
@@ -123,3 +168,4 @@ finally {
     Stop-Processes
   }
 }
+

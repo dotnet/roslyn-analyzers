7b686d8ac75b9f555a2e87417be45c37a3ad79f3
James Ko <jamesqko@gmail.com>
2017-12-09 22:33:47 -0500

    Port Async006 unit tests from Unfactored

--- src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/DoNotMixBlockingAndAsyncTests.Fixer.cs
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/DoNotMixBlockingAndAsyncTests.Fixer.cs
@@ -5,6 +5,7 @@ using Microsoft.CodeQuality.VisualBasic.Analyzers.ApiDesignGuidelines;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Test.Utilities;
+using Xunit;
 
 namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
 {
@@ -28,6 +29,390 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
             return new CSharpDoNotMixBlockingAndAsyncFixer();
+        }
+
+        private DiagnosticResult GetBasicResultAt(int line, int column, params object[] messageArguments)
+        {
+            return GetBasicResultAt(line, column, DoNotMixBlockingAndAsyncAnalyzer.Rule, messageArguments);
+        }
+
+        private DiagnosticResult GetCSharpResultAt(int line, int column, params object[] messageArguments)
+        {
+            return GetCSharpResultAt(line, column, DoNotMixBlockingAndAsyncAnalyzer.Rule, messageArguments);
+        }
+
+        private static string GetFullSource(string body)
+        {
+            return $@"
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{{
+    class Program
+    {{   {body}
+    }}
+}}
+";
+        }
+
+        // Wait block on async code - 1 diagnostic - code fix
+        [Fact]
+        public void WaitBlockAsyncCode()
+        {
+            var body = @"
+        async Task<int> fooAsync(int somenumber)
+        {
+            foo1Async(somenumber).Wait();
+            return somenumber;
+        }
+        async Task<int> foo1Async(int value)
+        {
+            await Task.Yield();
+            return value;
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(16, 35));
+
+            var fixbody = @"
+        async Task<int> fooAsync(int somenumber)
+        {
+            await foo1Async(somenumber);
+            return somenumber;
+        }
+        async Task<int> foo1Async(int value)
+        {
+            await Task.Yield();
+            return value;
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        // Result block on async code - 1 diagnostic - code fix
+        [Fact]
+        public void ResultBlockAsyncCode()
+        {
+            var body = @"
+        async Task foo2Async(int somenumber)
+        {
+            var temp = foo3Async(somenumber).Result;
+        }
+        async Task<int> foo3Async(int value)
+        {
+            await Task.Yield();
+            return value;
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(16, 46));
+
+            var fixbody = @"
+        async Task foo2Async(int somenumber)
+        {
+            var temp = await foo3Async(somenumber);
+        }
+        async Task<int> foo3Async(int value)
+        {
+            await Task.Yield();
+            return value;
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        // Invocation expression assigned to variable - 1 diagnostic
+        [Fact]
+        public void InvocationExpressionVariable()
+        {
+            var body = @"
+        async Task<Task<int>> SomeMethodAsync()
+        {
+            var t = addAsync(25, 50);
+            t.Wait();
+            return t;
+        }
+        async Task<int> addAsync(int num1, int num2)
+        {
+            await Task.Delay(29292);
+            return (num1 + num2);
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(17, 15));
+
+            var fixbody = @"
+        async Task<Task<int>> SomeMethodAsync()
+        {
+            var t = addAsync(25, 50);
+            await t;
+            return t;
+        }
+        async Task<int> addAsync(int num1, int num2)
+        {
+            await Task.Delay(29292);
+            return (num1 + num2);
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        // Async method using waitall on task - 1 diagnostic - code fix
+        [Fact]
+        public void WaitAllBlockAsyncCode()
+        {
+            var body = @"
+        async Task WaitAllAsync()
+        //Source: http://msdn.microsoft.com/en-us/library/dd270695(v=vs.110).aspx
+        {
+            Func<object, int> action = (object obj) =>
+                {
+                    int i = (int)obj;
+                    // The tasks that receive an argument between 2 and 5 throw exceptions 
+                    if (2 <= i && i <= 5)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    int tickCount = Environment.TickCount;
+                    return tickCount;
+                };
+            const int n = 10;
+            // Construct started tasks
+            Task<int>[] tasks = new Task<int>[n];
+            for (int i = 0; i<n; i++)
+            {
+                tasks[i] = Task<int>.Factory.StartNew(action, i);
+            }
+            // Exceptions thrown by tasks will be propagated to the main thread 
+            // while it waits for the tasks. The actual exceptions will be wrapped in AggregateException. 
+            try
+            {
+                // Wait for all the tasks to finish.
+                Task.WaitAll(tasks);
+                // We should never get to this point
+            }
+            catch (AggregateException e)
+            {
+                for (int j = 0; j<e.InnerExceptions.Count; j++)
+                {
+                    Console.WriteLine(e.InnerExceptions[j].ToString());
+                }
+            }
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(46, 22));
+
+            var fixbody = @"
+        async Task WaitAllAsync()
+        //Source: http://msdn.microsoft.com/en-us/library/dd270695(v=vs.110).aspx
+        {
+            Func<object, int> action = (object obj) =>
+                {
+                    int i = (int)obj;
+                    // The tasks that receive an argument between 2 and 5 throw exceptions 
+                    if (2 <= i && i <= 5)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    int tickCount = Environment.TickCount;
+                    return tickCount;
+                };
+            const int n = 10;
+            // Construct started tasks
+            Task<int>[] tasks = new Task<int>[n];
+            for (int i = 0; i<n; i++)
+            {
+                tasks[i] = Task<int>.Factory.StartNew(action, i);
+            }
+            // Exceptions thrown by tasks will be propagated to the main thread 
+            // while it waits for the tasks. The actual exceptions will be wrapped in AggregateException. 
+            try
+            {
+                // Wait for all the tasks to finish.
+                await Task.WhenAll(tasks);
+                // We should never get to this point
+            }
+            catch (AggregateException e)
+            {
+                for (int j = 0; j<e.InnerExceptions.Count; j++)
+                {
+                    Console.WriteLine(e.InnerExceptions[j].ToString());
+                }
+            }
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest, null, true);
+        }
+
+        // Async with thread.sleep - 1 diagnostic - code fix
+        [Fact]
+        public void AsyncWithThreadSleep()
+        {
+            var body = @"
+        async Task<string> SleepHeadAsync(string phrase)
+        {
+            Thread.Sleep(9999);
+            return phrase;
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(16, 20));
+
+            var fixbody = @"
+        async Task<string> SleepHeadAsync(string phrase)
+        {
+            await Task.Delay(9999);
+            return phrase;
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest, null, true);
+        }
+
+        // Thread.sleep within loop - 1 diagnostic - code fix
+        [Fact]
+        public void ThreadSleepInLoop()
+        {
+            var body = @"
+        async Task SleepAsync()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(3000);
+            }
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(18, 24));
+
+            var fixbody = @"
+        async Task SleepAsync()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                await Task.Delay(3000);
+            }
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest, null, true);
+        }
+
+        // Thread.sleep(timespan) - 1 diagnostic - code fix
+        [Fact]
+        public void AsyncWithThreadSleepTimeSpan()
+        {
+            var body = @"
+        async Task TimeSpanAsync()
+        {
+            TimeSpan time = new TimeSpan(1000);
+            Thread.Sleep(time);
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(17, 20));
+
+            var fixbody = @"
+        async Task TimeSpanAsync()
+        {
+            TimeSpan time = new TimeSpan(1000);
+            await Task.Delay(time);
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest, null, true);
+        }
+
+        // Thread.sleep(TimeSpan) within loop - 1 diagnostic - code fix
+        [Fact]
+        public void ThreadSleepTimeSpanInForLoop()
+        {
+            var body = @"
+        async Task TimespaninForloopAsync()
+        {
+            TimeSpan interval = new TimeSpan(2, 20, 55, 33, 0);
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(interval);
+            }
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(20, 24));
+
+            var fixbody = @"
+        async Task TimespaninForloopAsync()
+        {
+            TimeSpan interval = new TimeSpan(2, 20, 55, 33, 0);
+            for (int i = 0; i < 5; i++)
+            {
+                await Task.Delay(interval);
+            }
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest, null, true);
+        }
+
+        // Thread.sleep(timespan) within while loop - 1 diagnostic - code fix
+        [Fact]
+        public void ThreadSleepTimeSpanInWhileLoop()
+        {
+            var body = @"
+        async Task TimeSpanWhileLoop()
+        {
+            bool trueorfalse = true;
+            TimeSpan interval = new TimeSpan(0, 0, 2);
+            while (trueorfalse)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(interval);
+                }
+                trueorfalse = false;
+            }
+        }";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test, GetCSharpResultAt(22, 28));
+
+            var fixbody = @"
+        async Task TimeSpanWhileLoop()
+        {
+            bool trueorfalse = true;
+            TimeSpan interval = new TimeSpan(0, 0, 2);
+            while (trueorfalse)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    await Task.Delay(interval);
+                }
+                trueorfalse = false;
+            }
+        }";
+
+            var fixtest = GetFullSource(fixbody);
+            VerifyCSharpFix(test, fixtest, null, true);
         }
     }
 }
--- src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/DoNotMixBlockingAndAsyncTests.cs
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/DoNotMixBlockingAndAsyncTests.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeQuality.CSharp.Analyzers.ApiDesignGuidelines;
 using Microsoft.CodeQuality.VisualBasic.Analyzers.ApiDesignGuidelines;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Test.Utilities;
+using Xunit;
 
 namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
 {
@@ -17,6 +18,258 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new CSharpDoNotMixBlockingAndAsyncAnalyzer();
+        }
+
+        private DiagnosticResult GetBasicResultAt(int line, int column, params object[] messageArguments)
+        {
+            return GetBasicResultAt(line, column, DoNotMixBlockingAndAsyncAnalyzer.Rule, messageArguments);
+        }
+
+        private DiagnosticResult GetCSharpResultAt(int line, int column, params object[] messageArguments)
+        {
+            return GetCSharpResultAt(line, column, DoNotMixBlockingAndAsyncAnalyzer.Rule, messageArguments);
+        }
+
+        private static string GetFullSource(string body)
+        {
+            return $@"
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{{
+    class Program
+    {{   {body}
+    }}
+}}
+";
+        }
+
+        // No methods - no diagnostic should show
+        [Fact]
+        public void NoMethodNoDiagnostics()
+        {
+            var body = @"";
+
+            var test = GetFullSource(body);
+
+            VerifyCSharp(test);
+        }
+
+        // Async with task.delay - no diagnostic
+        [Fact]
+        public void AsyncWithAsync()
+        {
+            var body = @"
+        async Task SleepNowAsync()
+        {
+            await Task.Delay(1000);
+        }";
+            var test = GetFullSource(body);
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a function with thread.sleep in it - out of scope, no diagnostic
+        [Fact]
+        public void HiddenSyncCode()
+        {
+            var body = @"
+        async Task CallAnotherMethodAsync()
+        {
+            SleepAlittle(5888);
+        }
+        public void SleepAlittle(int value)
+        {
+            Thread.Sleep(value);
+        }";
+            var test = GetFullSource(body);
+            VerifyCSharp(test);
+        }
+
+        // Non async method calls thread.wait - no diagnostic
+        [Fact]
+        public void NonAsyncWait()
+        {
+            var body = @"
+        Task Example()
+        {
+            Task.Wait();
+        }";
+            var test = GetFullSource(body);
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a different Task method - no diagnostic
+        [Fact]
+        public void AsyncWithOtherMethod()
+        {
+            var body = @"
+        async Task Example()
+        {
+            await Task.Delay(100);
+        }";
+            var test = GetFullSource(body);
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a Wait method on a Task, but not the same Wait - no diagnostic
+        [Fact]
+        public void AsyncCallsOtherWait()
+        {
+            var test = @"
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        async Task Example(Action a, string s)
+        {
+            await Task.Delay(100);
+            var t = new Task(a);
+            t.Wait(s);
+        }
+    }
+    public static class MyTaskExtensions
+    {
+        public static void Wait(this Task t, string s)
+        {
+            s = s + 1;
+        }
+    }
+}";
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a WaitAll method on a Task, but not the same WaitAll - no diagnostic
+        [Fact]
+        public void AsyncCallsOtherWaitAll()
+        {
+            var test = @"
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        async Task Example(Action a, string s)
+        {
+            await Task.Delay(100);
+            var t = new Task(a);
+            t.WaitAll(s);
+        }
+    }
+    public static class MyTaskExtensions
+    {
+        public static void WaitAll(this Task t, string s)
+        {
+            s = s + 1;
+        }
+    }
+}";
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a WaitAny method on a Task, but not the same WaitAny - no diagnostic
+        [Fact]
+        public void AsyncCallsOtherWaitAny()
+        {
+            var test = @"
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        async Task Example(Action a, string s)
+        {
+            await Task.Delay(100);
+            var t = new Task(a);
+            t.WaitAny(s);
+        }
+    }
+    public static class MyTaskExtensions
+    {
+        public static void WaitAny(this Task t, string s)
+        {
+            s = s + 1;
+        }
+    }
+}";
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a Sleep method on a Task, but not the same Sleep - no diagnostic
+        [Fact]
+        public void AsyncCallsOtherSleep()
+        {
+            var test = @"
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        async Task Example(Action a, string s)
+        {
+            await Task.Delay(100);
+            var t = new Task(a);
+            t.Sleep(s);
+        }
+    }
+    public static class MyTaskExtensions
+    {
+        public static void Sleep(this Task t, string s)
+        {
+            s = s + 1;
+        }
+    }
+}";
+            VerifyCSharp(test);
+        }
+
+        // Async method calls a GetResult method on a Task, but not the same GetResult - no diagnostic
+        [Fact]
+        public void AsyncCallsOtherGetResult()
+        {
+            var test = @"
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        async Task Example(Action a, string s)
+        {
+            await Task.Delay(100);
+            var t = new Task(a);
+            t.GetResult(s);
+        }
+    }
+    public static class MyTaskExtensions
+    {
+        public static void GetResult(this Task t, string s)
+        {
+            s = s + 1;
+        }
+    }
+}";
+            VerifyCSharp(test);
         }
     }
 }

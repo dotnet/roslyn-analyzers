cb0b761df1f8092999cd4536b9dd318ab08953e4
Tomas Matousek <tomat@microsoft.com>
2018-05-22 12:13:27 -0700

    Update to RT 1.0.0-beta2-62922-02

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile %~dp0build\Build.ps1 -restore -build -deploy %*
+powershell -ExecutionPolicy ByPass -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
 exit /b %ErrorLevel%
--- build/CIBuild.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy ByPass %~dp0Build.ps1 -restore -build -test -sign -pack -ci %*
-exit /b %ErrorLevel%

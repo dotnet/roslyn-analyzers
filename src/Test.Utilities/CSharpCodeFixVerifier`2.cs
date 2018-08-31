5d04b3bd09b31791a42b56d1bc83a0404ab72446
Sam Harwell <sam.harwell@microsoft.com>
2018-08-31 01:20:16 -0500

    Add helper classes for analyzer and code fix testing

---
+++ src/Test.Utilities/CSharpCodeFixVerifier`2.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+#pragma warning disable CA1000 // Do not declare static members on generic types
+
+namespace Test.Utilities
+{
+    public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
+        where TAnalyzer : DiagnosticAnalyzer, new()
+        where TCodeFix : CodeFixProvider, new()
+    {
+        public static DiagnosticResult Diagnostic(string diagnosticId = null)
+            => CSharpCodeFixVerifier<TAnalyzer, TCodeFix, XUnitVerifier>.Diagnostic(diagnosticId);
+
+        public static DiagnosticResult Diagnostic(DiagnosticDescriptor descriptor)
+            => new DiagnosticResult(descriptor);
+
+        public static Task VerifyAnalyzerAsync(string source, params DiagnosticResult[] expected)
+        {
+            var test = new Test { TestCode = source };
+            test.ExpectedDiagnostics.AddRange(expected);
+            return test.RunAsync();
+        }
+
+        public static Task VerifyCodeFixAsync(string source, string fixedSource)
+            => VerifyCodeFixAsync(source, DiagnosticResult.EmptyDiagnosticResults, fixedSource);
+
+        public static Task VerifyCodeFixAsync(string source, DiagnosticResult expected, string fixedSource)
+            => VerifyCodeFixAsync(source, new[] { expected }, fixedSource);
+
+        public static Task VerifyCodeFixAsync(string source, DiagnosticResult[] expected, string fixedSource)
+        {
+            var test = new Test
+            {
+                TestCode = source,
+                FixedCode = fixedSource,
+            };
+
+            test.ExpectedDiagnostics.AddRange(expected);
+            return test.RunAsync();
+        }
+    }
+}
---
+++ src/Test.Utilities/VisualBasicCodeFixVerifier`2+Test.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.VisualBasic.Testing;
+
+namespace Test.Utilities
+{
+    public static partial class VisualBasicCodeFixVerifier<TAnalyzer, TCodeFix>
+    {
+#pragma warning disable CA1724 // Type names should not match namespaces
+#pragma warning disable CA1034 // Nested types should not be visible
+        public class Test : VisualBasicCodeFixTest<TAnalyzer, TCodeFix, XUnitVerifier>
+#pragma warning restore CA1034 // Nested types should not be visible
+#pragma warning restore CA1724 // Type names should not match namespaces
+        {
+            public Test()
+            {
+                SolutionTransforms.Add((solution, projectId) =>
+                {
+                    if (typeof(object).Assembly.GetType("System.ValueTuple`2", throwOnError: false) != null
+                        && MetadataReferences.SystemValueTupleReference != null)
+                    {
+                        solution = solution.RemoveMetadataReference(projectId, MetadataReferences.SystemValueTupleReference);
+                    }
+
+                    if (IncludeSystemData)
+                    {
+                        solution = solution.AddMetadataReference(projectId, MetadataReference.CreateFromFile(typeof(System.Data.DataSet).Assembly.Location));
+                    }
+
+                    return solution;
+                });
+            }
+
+            public bool IncludeSystemData { get; set; } = true;
+        }
+    }
+}

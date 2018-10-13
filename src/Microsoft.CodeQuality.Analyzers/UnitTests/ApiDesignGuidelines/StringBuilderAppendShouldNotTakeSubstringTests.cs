6518c74077982e47ca3f1b49a8b3eff88548650b
Peter Amrehn <peter.wendorff@gmx.de>
2018-10-13 09:49:55 +0200

    #1638: change expected diagnostics pattern as @sharwell suggested

--- src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/StringBuilderAppendShouldNotTakeSubstringTests.cs
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/StringBuilderAppendShouldNotTakeSubstringTests.cs
@@ -49,8 +49,7 @@ public class C
     }
 }";
             var expectedDiagnostic = new DiagnosticResult(
-                    StringBuilderAppendShouldNotTakeSubstring.RuleIdTwoParameterId,
-                    DiagnosticHelpers.DefaultDiagnosticSeverity)
+                    StringBuilderAppendShouldNotTakeSubstring.RuleReplaceTwoParameter)
                 .WithLocation("Test0.cs", 9, 9);
 
             VerifyCSharp(code, expectedDiagnostic);

967d87bf83939d1501399b2324e46169e96e26e3
Peter Amrehn <peter.wendorff@gmx.de>
2018-10-13 09:56:58 +0200

    #1638: add unit tests for Basic

--- src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/StringBuilderAppendShouldNotTakeSubstringTests.cs
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/StringBuilderAppendShouldNotTakeSubstringTests.cs
@@ -34,6 +34,29 @@ public class C
         }
 
         [Fact]
+        public void TestFromProposingTicketFirstLineBasic()
+        {
+            const string code = @"
+Imports System.Text
+
+Public Class C
+    Public Function Append(ByVal text As String) As String
+        Dim sb = New StringBuilder()
+        sb.Append(text.Substring(2))
+        Return sb.ToString()
+    End Function
+End Class
+";
+
+            var expectedDiagnostic =
+                new DiagnosticResult(
+                        StringBuilderAppendShouldNotTakeSubstring.RuleReplaceOneParameter)
+                    .WithLocation("Test0.vb", 7, 9);
+
+            VerifyBasic(code, expectedDiagnostic);
+        }
+
+        [Fact]
         public void TestFromProposingTicketSecondLine()
         {
             const string code = @"
@@ -53,6 +76,27 @@ public class C
                 .WithLocation("Test0.cs", 9, 9);
 
             VerifyCSharp(code, expectedDiagnostic);
+        }
+
+        [Fact]
+        public void TestFromProposingTicketSecondLineBasic()
+        {
+            const string code = @"
+Imports System.Text
+
+Public Class C
+    Public Function Append(ByVal text As String) As String
+        Dim sb = New StringBuilder()
+        sb.Append(text.Substring(0,6))
+        Return sb.ToString()
+    End Function
+End Class
+";
+            var expectedDiagnostic = new DiagnosticResult(
+                    StringBuilderAppendShouldNotTakeSubstring.RuleReplaceTwoParameter)
+                .WithLocation("Test0.vb", 7, 9);
+
+            VerifyBasic(code, expectedDiagnostic);
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()

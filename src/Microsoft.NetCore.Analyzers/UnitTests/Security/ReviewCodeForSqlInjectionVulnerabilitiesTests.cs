5f2b56aced11d635be1550a565df408b253cab7e
Paul Ming <paulming@microsoft.com>
2018-11-14 19:28:38 -0800

    Unit test for #1891

--- src/Microsoft.NetCore.Analyzers/UnitTests/Security/ReviewCodeForSqlInjectionVulnerabilitiesTests.cs
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/ReviewCodeForSqlInjectionVulnerabilitiesTests.cs
@@ -2680,5 +2680,173 @@ public class Class1
 }
 ");
         }
+
+        [Fact(Skip = "https://github.com/dotnet/roslyn-analyzers/issues/1891")]
+        public void PointsToAnalysisAssertsLocationSetsComparison()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Threading;
+using System.Web;
+
+public interface IContext
+{
+    HttpContext HttpContext { get; }
+}
+
+public class CaptureStream : Stream
+{
+    public CaptureStream(Stream innerStream)
+    {
+        _innerStream = innerStream;
+        _captureStream = new MemoryStream();
+    }
+
+    private readonly Stream _innerStream;
+    private readonly MemoryStream _captureStream;
+
+    public override bool CanRead
+    {
+        get { return _innerStream.CanRead; }
+    }
+
+    public override bool CanSeek
+    {
+        get { return _innerStream.CanSeek; }
+    }
+
+    public override bool CanWrite
+    {
+        get { return _innerStream.CanWrite; }
+    }
+
+    public override long Length
+    {
+        get { return _innerStream.Length; }
+    }
+
+    public override long Position
+    {
+        get { return _innerStream.Position; }
+        set { _innerStream.Position = value; }
+    }
+
+    public override long Seek(long offset, SeekOrigin direction)
+    {
+        return _innerStream.Seek(offset, direction);
+    }
+
+    public override void SetLength(long length)
+    {
+        _innerStream.SetLength(length);
+    }
+
+    public override void Close()
+    {
+        _innerStream.Close();
+    }
+
+    public override void Flush()
+    {
+        if (_captureStream.Length > 0)
+        {
+            OnCaptured();
+            _captureStream.SetLength(0);
+        }
+
+        _innerStream.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return _innerStream.Read(buffer, offset, count);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _captureStream.Write(buffer, offset, count);
+        _innerStream.Write(buffer, offset, count);
+    }
+
+    public event Action<byte[]> Captured;
+
+    protected virtual void OnCaptured()
+    {
+        Captured(_captureStream.ToArray());
+    }
+}
+
+public class Class1
+{
+    private string AField;
+    private bool ASwitch;
+
+    public void Method(IContext aContext)
+    {
+        var captureHandlerIsAttached = false;
+
+        try
+        {
+            if (!ASwitch)
+                return;
+
+            Console.WriteLine(AField);
+
+            if (!HasUrl(aContext))
+            {
+                return;
+            }
+
+            var response = aContext.HttpContext.Response;
+            var captureStream = new CaptureStream(null);
+            response.Filter = captureStream;
+
+            captureStream.Captured += (output) => {
+                try
+                {
+                    if (response.StatusCode != 200)
+                    {
+                        Console.WriteLine(AField);
+                        return;
+                    }
+
+                    Console.WriteLine(aContext.HttpContext.Request.Url.AbsolutePath);
+                }
+                finally
+                {
+                    ReleaseTheLock();
+                }
+            };
+
+            captureHandlerIsAttached = true;
+        }
+        finally
+        {
+            if (!captureHandlerIsAttached)
+                ReleaseTheLock();
+        }
+    }
+
+    private void ReleaseTheLock()
+    {
+        if (AField != null && Monitor.IsEntered(AField))
+        {
+            Monitor.Exit(AField);
+            AField = null;
+        }
+    }
+
+    protected virtual bool HasUrl(IContext filterContext)
+    {
+        if (filterContext.HttpContext.Request.Url == null)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+");
+        }
     }
 }

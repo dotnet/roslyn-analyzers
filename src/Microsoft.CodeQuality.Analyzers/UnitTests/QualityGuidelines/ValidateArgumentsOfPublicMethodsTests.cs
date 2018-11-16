95232bf9efb77ca65a8b9a8f46af6cf318366faf
Manish Vasani <mavasani@microsoft.com>
2018-11-15 16:32:03 -0800

    Fix couple of asserts in PointsToAnalysis
    1) Fixes #1886: Loosen the assert in DefaultPointsToValueGenerator to account for lvalue flow captures. We already handle flow captures in the method implementation. I was able to find a minimal repro for this assert and used it in the added test.
    2) Fixes #1891: PointsToAbstractValue.MakeNonNull should attempt to use analysis entity based location if the underlying operation binds to an entity. I was unable to create a smaller repro then what @dotpaul provided in the issue.

--- src/Microsoft.CodeQuality.Analyzers/UnitTests/QualityGuidelines/ValidateArgumentsOfPublicMethodsTests.cs
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/QualityGuidelines/ValidateArgumentsOfPublicMethodsTests.cs
@@ -3464,5 +3464,203 @@ namespace ANamespace
     }
 }");
         }
+
+        [Fact, WorkItem(1886, "https://github.com/dotnet/roslyn-analyzers/issues/1886")]
+        public void Issue1886()
+        {
+            VerifyCSharp(@"
+public enum Status
+{
+    Status1,
+    Status2,
+}
+
+public class C1
+{
+    public Status MyStatus { get; set; }
+}
+
+public class C2
+{
+    public static void M(C1 c, bool f)
+    {
+        if (c== null)
+        {
+            return;
+        }
+
+        c.MyStatus = f ? Status.Status1 : Status.Status2;
+    }
+}");
+        }
+
+        [Fact, WorkItem(1891, "https://github.com/dotnet/roslyn-analyzers/issues/1891")]
+        public void Issue1891()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Threading;
+using System.Web;
+
+public interface IContext
+{
+    HttpContext HttpContext { get; }
+}
+
+public class CaptureStream : Stream
+{
+    public CaptureStream(Stream innerStream)
+    {
+        _innerStream = innerStream;
+        _captureStream = new MemoryStream();
+    }
+
+    private readonly Stream _innerStream;
+    private readonly MemoryStream _captureStream;
+
+    public override bool CanRead
+    {
+        get { return _innerStream.CanRead; }
+    }
+
+    public override bool CanSeek
+    {
+        get { return _innerStream.CanSeek; }
+    }
+
+    public override bool CanWrite
+    {
+        get { return _innerStream.CanWrite; }
+    }
+
+    public override long Length
+    {
+        get { return _innerStream.Length; }
+    }
+
+    public override long Position
+    {
+        get { return _innerStream.Position; }
+        set { _innerStream.Position = value; }
+    }
+
+    public override long Seek(long offset, SeekOrigin direction)
+    {
+        return _innerStream.Seek(offset, direction);
+    }
+
+    public override void SetLength(long length)
+    {
+        _innerStream.SetLength(length);
+    }
+
+    public override void Close()
+    {
+        _innerStream.Close();
+    }
+
+    public override void Flush()
+    {
+        if (_captureStream.Length > 0)
+        {
+            OnCaptured();
+            _captureStream.SetLength(0);
+        }
+
+        _innerStream.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return _innerStream.Read(buffer, offset, count);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _captureStream.Write(buffer, offset, count);
+        _innerStream.Write(buffer, offset, count);
+    }
+
+    public event Action<byte[]> Captured;
+
+    protected virtual void OnCaptured()
+    {
+        Captured(_captureStream.ToArray());
+    }
+}
+
+public class Class1
+{
+    private string AField;
+    private bool ASwitch;
+
+    public void Method(IContext aContext)
+    {
+        var captureHandlerIsAttached = false;
+
+        try
+        {
+            if (!ASwitch)
+                return;
+
+            Console.WriteLine(AField);
+
+            if (!HasUrl(aContext))
+            {
+                return;
+            }
+
+            var response = aContext.HttpContext.Response;
+            var captureStream = new CaptureStream(null);
+            response.Filter = captureStream;
+
+            captureStream.Captured += (output) => {
+                try
+                {
+                    if (response.StatusCode != 200)
+                    {
+                        Console.WriteLine(AField);
+                        return;
+                    }
+
+                    Console.WriteLine(aContext.HttpContext.Request.Url.AbsolutePath);
+                }
+                finally
+                {
+                    ReleaseTheLock();
+                }
+            };
+
+            captureHandlerIsAttached = true;
+        }
+        finally
+        {
+            if (!captureHandlerIsAttached)
+                ReleaseTheLock();
+        }
+    }
+
+    private void ReleaseTheLock()
+    {
+        if (AField != null && Monitor.IsEntered(AField))
+        {
+            Monitor.Exit(AField);
+            AField = null;
+        }
+    }
+
+    protected virtual bool HasUrl(IContext filterContext)
+    {
+        if (filterContext.HttpContext.Request.Url == null)
+        {
+            return false;
+        }
+        return true;
+    }
+}",
+            // Test0.cs(115,28): warning CA1062: In externally visible method 'void Class1.Method(IContext aContext)', validate parameter 'aContext' is non-null before using it. If appropriate, throw an ArgumentNullException when the argument is null or add a Code Contract precondition asserting non-null argument.
+            GetCSharpResultAt(115, 28, "void Class1.Method(IContext aContext)", "aContext"));
+        }
     }
 }

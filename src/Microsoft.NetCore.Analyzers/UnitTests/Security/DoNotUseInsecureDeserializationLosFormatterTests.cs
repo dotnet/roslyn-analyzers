82f66d899ec905e1cc9bde08156fc44f0d953771
Manish Vasani <mavasani@microsoft.com>
2018-12-22 00:06:25 -0800

    Fix test failure and also mark the failing tests with DFA trait

--- src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotUseInsecureDeserializationLosFormatterTests.cs
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotUseInsecureDeserializationLosFormatterTests.cs
@@ -8,6 +8,7 @@ using Xunit;
 
 namespace Microsoft.NetCore.Analyzers.Security.UnitTests
 {
+    [Trait(Traits.DataflowAnalysis, Traits.Dataflow.PropertySetAnalysis)]
     public class DoNotUseInsecureDeserializerLosFormatterTests : DiagnosticAnalyzerTestBase
     {
         private static readonly DiagnosticDescriptor InvocationRule = DoNotUseInsecureDeserializerLosFormatter.RealInvocationDescriptor;

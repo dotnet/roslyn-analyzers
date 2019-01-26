d62d750a1ae50f87e1cbe99e88f90ac55dd6d1cd
Sam Harwell <sam@tunnelvisionlabs.com>
2019-01-25 21:41:04 -0600

    Enable concurrent execution for analyzers with no actions
    Fixes #2044
    Fixes #2045
    Fixes #2046
    Fixes #2047
    Fixes #2048
    Fixes #2049
    Fixes #2050
    Fixes #2051
    Fixes #2054
    Fixes #2056
    Fixes #2057
    Fixes #2058
    Fixes #2060
    Fixes #2061
    Fixes #2063
    Fixes #2064
    Fixes #2065
    Fixes #2069
    Fixes #2070
    Fixes #2071
    Fixes #2072
    Fixes #2073
    Fixes #2074
    Fixes #2076

--- src/Microsoft.CodeQuality.Analyzers/Core/Maintainability/VariableNamesShouldNotMatchFieldNames.cs
+++ src/Microsoft.CodeQuality.Analyzers/Core/Maintainability/VariableNamesShouldNotMatchFieldNames.cs
@@ -44,8 +44,7 @@ namespace Microsoft.CodeQuality.Analyzers.Maintainability
 
         public override void Initialize(AnalysisContext analysisContext)
         {
-            // TODO: Enable concurrent execution of analyzer actions.
-            //analysisContext.EnableConcurrentExecution();
+            analysisContext.EnableConcurrentExecution();
 
             // TODO: Configure generated code analysis.
             //analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);

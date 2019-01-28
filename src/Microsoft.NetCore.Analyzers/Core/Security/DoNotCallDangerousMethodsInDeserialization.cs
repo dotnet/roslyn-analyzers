344f100c455d89147b9dd05fbbe63289f2bc386f
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-01-28 17:37:00 +0800

    Do Not Call Dangerous Methods In Deserialization

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotCallDangerousMethodsInDeserialization.cs
@@ -0,0 +1,214 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotCallDangerousMethodsInDeserialization : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5360";
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotCallDangerousMethodsInDeserialization),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotCallDangerousMethodsInDeserializationMessage),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotCallDangerousMethodsInDeserializationDescription),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+
+        private readonly ImmutableHashSet<string> SystemIOFileMethodMetadataNames = ImmutableHashSet.Create(
+                StringComparer.Ordinal,
+                "WriteAllBytes",
+                "WriteAllLines",
+                "WriteAllText",
+                "Copy",
+                "Move",
+                "AppendAllLines",
+                "AppendAllText",
+                "AppendText",
+                "Delete");
+
+        private readonly ImmutableHashSet<string> SystemReflectionAssemblyMethodMetadataNames = ImmutableHashSet.Create(
+                StringComparer.Ordinal,
+                "GetLoadedModules",
+                "Load",
+                "LoadFile",
+                "LoadFrom",
+                "LoadModule",
+                "LoadWithPartialName",
+                "ReflectionOnlyLoad",
+                "ReflectionOnlyLoadFrom",
+                "UnsafeLoadFrom");
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                DiagnosticHelpers.DefaultDiagnosticSeverity,
+                isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                description: s_Description,
+                helpLinkUri: null,
+                customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public sealed override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(
+                (CompilationStartAnalysisContext compilationStartAnalysisContext) =>
+                {
+                    var compilation = compilationStartAnalysisContext.Compilation;
+                    var attributeTypeSymbols = ImmutableArray.Create(
+                        WellKnownTypes.OnDeserializingAttribute(compilation),
+                        WellKnownTypes.OnDeserializedAttribute(compilation));
+                    var serializableAttributeTypeSymbol = WellKnownTypes.SerializableAttribute(compilation);
+                    var streamingContextTypeSymbol = WellKnownTypes.StreamingContext(compilation);
+
+                    if (serializableAttributeTypeSymbol == null || streamingContextTypeSymbol == null)
+                    {
+                        return;
+                    }
+
+                    var builder = ImmutableArray.CreateBuilder<IMethodSymbol>();
+                    var systemIOFileTypeSymbol = WellKnownTypes.SystemIOFile(compilation);
+
+                    if (systemIOFileTypeSymbol != null)
+                    {
+                        foreach (var targetMethodName in SystemIOFileMethodMetadataNames)
+                        {
+                            builder.AddRange(systemIOFileTypeSymbol.GetMembers(targetMethodName).Cast<IMethodSymbol>());
+                        }
+                    }
+
+                    var systemReflectionAssemblyTypeSymbol = WellKnownTypes.SystemReflectionAssembly(compilation);
+
+                    if (systemReflectionAssemblyTypeSymbol != null)
+                    {
+                        foreach (var targetMethodName in SystemReflectionAssemblyMethodMetadataNames)
+                        {
+                            builder.AddRange(systemReflectionAssemblyTypeSymbol.GetMembers(targetMethodName).Cast<IMethodSymbol>());
+                        }
+                    }
+
+                    var dangerousMethodSymbols = builder.ToImmutable();
+
+                    compilationStartAnalysisContext.RegisterSymbolAction(
+                        (SymbolAnalysisContext symbolAnalysisContext) =>
+                        {
+                            var classSymbol = symbolAnalysisContext.Symbol as INamedTypeSymbol;
+
+                            if (!classSymbol.GetAttributes().Any(s => s.AttributeClass.Equals(serializableAttributeTypeSymbol)))
+                            {
+                                return;
+                            }
+                            
+                            foreach (var member in classSymbol.GetMembers())
+                            {
+                                if (member.Kind == SymbolKind.Method)
+                                {
+                                    var methodSymbol = member as IMethodSymbol;
+
+                                    if (methodSymbol.GetAttributes().Any(s => attributeTypeSymbols.Contains(s.AttributeClass))
+                                        || methodSymbol.MetadataName == "OnDeserialization")
+                                    {
+                                        var parameters = methodSymbol.GetParameters();
+
+                                        if (parameters == null
+                                            || parameters.Length != 1
+                                            || !parameters[0].Type.Equals(streamingContextTypeSymbol))
+                                        {
+                                            return;
+                                        }
+
+                                        var visitedMethodSymbols = new List<IMethodSymbol>();
+                                        var invocationOperations = FindDangerousMethodInvocatioinOperation(methodSymbol, compilation, dangerousMethodSymbols, ref visitedMethodSymbols);
+
+                                        foreach (var item in invocationOperations)
+                                        {
+                                            symbolAnalysisContext.ReportDiagnostic(
+                                                item.CreateDiagnostic(
+                                                    Rule,
+                                                    classSymbol.MetadataName,
+                                                    methodSymbol.MetadataName,
+                                                    item.TargetMethod.MetadataName));
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        SymbolKind.NamedType);
+                });
+
+            /// <summary>
+            /// Traverse every invocation in the method to find all the dangerous function it invokes.
+            /// </summary>
+            /// <param name="methodSymbol">The Symbol of the method which is going to be analyzed</param>
+            /// <param name="compilation">Compilation context</param>
+            /// <param name="dangerousMethodSymbols">Symbols reprensent dangerous methods</param>
+            /// <param name="visitedMethodSymbols">Methods that have been analyzed</param>
+            IEnumerable<IInvocationOperation> FindDangerousMethodInvocatioinOperation(
+                IMethodSymbol methodSymbol,
+                Compilation compilation,
+                ImmutableArray<IMethodSymbol> dangerousMethodSymbols,
+                ref List<IMethodSymbol> visitedMethodSymbols)
+            {
+                var result = new List<IInvocationOperation>();
+
+                // Every method in the source just needs to be analyzed once.
+                if (visitedMethodSymbols.Find((IMethodSymbol m) => m.Equals(methodSymbol)) == null)
+                {
+                    var blockOperation = methodSymbol.GetTopmostOperationBlock(compilation);
+
+                    if (blockOperation != null)
+                    {
+                        foreach (var operation in blockOperation.Descendants())
+                        {
+                            if (operation.Kind.Equals(OperationKind.Invocation))
+                            {
+                                var invocationOperation = operation as IInvocationOperation;
+                                var invokedMethodSymbol = invocationOperation.TargetMethod;
+
+                                // If it calls a dengerous method, add the invocation operation to the result.
+                                if (dangerousMethodSymbols.Contains(invokedMethodSymbol))
+                                {
+                                    result.Add(invocationOperation);
+                                }
+
+                                // If it calls a method which is reachable, analyze that method recursively.
+                                if (invokedMethodSymbol.IsInSource())
+                                {
+                                    result.AddRange(FindDangerousMethodInvocatioinOperation(invokedMethodSymbol, compilation, dangerousMethodSymbols, ref visitedMethodSymbols));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                visitedMethodSymbols.Add(methodSymbol);
+                return result;
+            }
+        }
+    }
+}
+
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotCallDangerousMethodsInDeserializationTests.cs
@@ -0,0 +1,434 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    public class DoNotCallDangerousMethodsInDeserializationTests : DiagnosticAnalyzerTestBase
+    {
+        [Fact]
+        public void TestOnDeserializingDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserializing()]
+    internal void OnDeserializingMethod(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}",
+            GetCSharpResultAt(15, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserializingMethod", "WriteAllBytes"));
+        }
+
+        [Fact]
+        public void TestOnDeserializedDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserialized()]
+    internal void OnDeserializedMethod(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}",
+            GetCSharpResultAt(15, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserializedMethod", "WriteAllBytes"));
+
+            VerifyBasic(@"
+Imports System
+Imports System.IO
+Imports System.Runtime.Serialization
+
+Namespace TestNamespace
+    <Serializable()>
+    Class TestClass
+        Private member As String
+        
+        <OnDeserialized()>
+        Sub OnDeserializedMethod(ByVal context As StreamingContext)
+            Dim bytes(9) As Byte
+            File.WriteAllBytes(""C:\\"", bytes)
+        End Sub
+    End Class
+End Namespace",
+            GetBasicResultAt(14, 13, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserializedMethod", "WriteAllBytes"));
+        }
+
+        [Fact]
+        public void TestOnMultiAttributesDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserialized()]
+    [OnSerialized()]
+    internal void OnDeserializedMethod(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}",
+            GetCSharpResultAt(16, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserializedMethod", "WriteAllBytes"));
+        }
+
+        [Fact]
+        public void TestOnDeserializedMediateInvocationDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserialized()]
+    internal void OnDeserializedMethod(StreamingContext context)
+    {
+        var obj = new TestClass();
+        obj.TestMethod();
+        File.WriteAllText(""C:\\"", ""contents"");
+    }
+    
+    private void TestMethod()
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}",
+            GetCSharpResultAt(16, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserializedMethod", "WriteAllText"),
+            GetCSharpResultAt(22, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserializedMethod", "WriteAllBytes"));
+        }
+
+        [Fact]
+        public void TestOnDeserializationDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    internal void OnDeserialization(StreamingContext context)
+    {
+        //File
+        var path = ""C:\\"";
+        var bytes = new byte[] {0x20, 0x20, 0x20};
+        var strings = new string[]{""111"", ""222""};
+        var contents = ""This is the contents."";
+        var sourceFileName = ""source file"";
+        var destFileName = ""dest file"";
+        File.WriteAllBytes(path, bytes);
+        File.WriteAllLines(path, strings, Encoding.ASCII);
+        File.WriteAllText(path, contents);
+        File.Copy(sourceFileName, destFileName);
+        File.Move(sourceFileName, destFileName);
+        File.AppendAllLines(path, strings);
+        File.AppendAllText(path, contents);
+        File.AppendText(path);
+        File.Delete(path);
+
+        //Assembly
+        var fileName = ""C:\\test.txt"";
+        var assemblyName = ""assembly file"";
+        var moduleName = ""module name"";
+        var partialName = ""partial name"";
+        var fullName = ""sysglobl, Version = 4.0.0.0, Culture = neutral, "" +
+                       ""PublicKeyToken=b03f5f7f11d50a3a, processor architecture=MSIL"";
+        var rawModule = new byte[] {0x20, 0x20, 0x20};
+        var rawAssembly = new byte[] {0x20, 0x20, 0x20};
+        var assem = typeof(TestClass).Assembly;
+        var modules = assem.GetLoadedModules();
+        var an = new AssemblyName(fullName);
+        var assem2 = Assembly.Load(an);
+        var assem3 = Assembly.LoadFile(fileName);
+        var assem4 = Assembly.LoadFrom(assemblyName);
+        var module = assem.LoadModule(moduleName, rawModule);
+        var assem5 = Assembly.LoadWithPartialName(partialName);
+        var assem6 = Assembly.ReflectionOnlyLoad(rawAssembly);
+        var assem7 = Assembly.ReflectionOnlyLoadFrom(assemblyName);
+        var assem8 = Assembly.UnsafeLoadFrom(assemblyName);
+    }
+}",
+            GetCSharpResultAt(22, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "WriteAllBytes"),
+            GetCSharpResultAt(23, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "WriteAllLines"),
+            GetCSharpResultAt(24, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "WriteAllText"),
+            GetCSharpResultAt(25, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "Copy"),
+            GetCSharpResultAt(26, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "Move"),
+            GetCSharpResultAt(27, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "AppendAllLines"),
+            GetCSharpResultAt(28, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "AppendAllText"),
+            GetCSharpResultAt(29, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "AppendText"),
+            GetCSharpResultAt(30, 9, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "Delete"),
+            GetCSharpResultAt(42, 23, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "GetLoadedModules"),
+            GetCSharpResultAt(44, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "Load"),
+            GetCSharpResultAt(45, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "LoadFile"),
+            GetCSharpResultAt(46, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "LoadFrom"),
+            GetCSharpResultAt(47, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "LoadModule"),
+            GetCSharpResultAt(48, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "LoadWithPartialName"),
+            GetCSharpResultAt(49, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "ReflectionOnlyLoad"),
+            GetCSharpResultAt(50, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "ReflectionOnlyLoadFrom"),
+            GetCSharpResultAt(51, 22, DoNotCallDangerousMethodsInDeserialization.Rule, "TestClass", "OnDeserialization", "UnsafeLoadFrom"));
+        }
+
+        [Fact]
+        public void TestOnDeserializingNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserializing()]
+    internal void OnDeserializingMethod(StreamingContext context)
+    {
+        var obj = new TestClass();
+        obj.TestMethod();
+    }
+    
+    private void TestMethod()
+    {
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOnDeserializedNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserialized()]
+    internal void OnDeserializedMethod(StreamingContext context)
+    {
+        var obj = new TestClass();
+        obj.TestMethod();
+    }
+    
+    private void TestMethod()
+    {
+    }
+}");
+
+            VerifyBasic(@"
+Imports System
+Imports System.Runtime.Serialization
+
+Namespace TestNamespace
+    <Serializable()>
+    Class TestClass
+        Private member As String
+        
+        <OnDeserialized()>
+        Sub OnDeserializedMethod(ByVal context As StreamingContext)
+            Dim obj As New TestClass()
+            obj.TestMethod()
+        End Sub
+
+        Sub TestMethod()
+        End Sub
+    End Class
+End Namespace");
+        }
+
+        [Fact]
+        public void TestOnDeserializationNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    internal void OnDeserialization(StreamingContext context)
+    {
+        var obj = new TestClass();
+        obj.TestMethod();
+    }
+    
+    private void TestMethod()
+    {
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOnDeserializationWithoutParameterNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    internal void OnDeserialization()
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOnDeserializingWithoutSerializableNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+public class TestClass
+{
+    private string member;
+
+    [OnDeserializing()]
+    internal void OnDeserializingMethod(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOnDeserializationWithoutSerializableNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+public class TestClass
+{
+    private string member;
+
+    internal void OnDeserialization(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOnDeserializedWithEmptyMethodBodyNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnDeserialized()]
+    internal void OnDeserializedMethod(StreamingContext context)
+    {
+    }
+}");
+        }
+
+        [Fact]
+        public void TestWithoutOnDeserializingAttributesNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    internal void OnDeserializingMethod(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOnSerializedNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+[Serializable()]
+public class TestClass
+{
+    private string member;
+
+    [OnSerialized()]
+    internal void OnDeserializedMethod(StreamingContext context)
+    {
+        byte[] bytes = new byte[] {0x20, 0x20, 0x20};
+        File.WriteAllBytes(""C:\\"", bytes);
+    }
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotCallDangerousMethodsInDeserialization();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotCallDangerousMethodsInDeserialization();
+        }
+    }
+}

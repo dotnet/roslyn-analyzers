e10c8fac13ae313a596f02a2a6f5fddd397316e1
Mert Akinc <mert.akinc@ni.com>
2019-01-30 11:07:17 -0600

    Implement CA1712: Do not prefix enum values with type name

---
+++ src/Microsoft.CodeQuality.Analyzers/Core/ApiDesignGuidelines/DoNotPrefixEnumValuesWithTypeName.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using static Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.MicrosoftApiDesignGuidelinesAnalyzersResources;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
+{
+    /// <summary>
+    /// CA1712: Do not prefix enum values with type name
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotPrefixEnumValuesWithTypeNameAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1712";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(DoNotPrefixEnumValuesWithTypeNameTitle), ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(DoNotPrefixEnumValuesWithTypeNameMessage), ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(DoNotPrefixEnumValuesWithTypeNameDescription), ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule =
+            new DiagnosticDescriptor(
+                RuleId,
+                s_localizableTitle,
+                s_localizableMessage,
+                DiagnosticCategory.Naming,
+                DiagnosticSeverity.Error,
+                isEnabledByDefault: true,
+                description: s_localizableDescription,
+                helpLinkUri: "https://msdn.microsoft.com/en-us/library/ms182237.aspx",
+                customTags: FxCopWellKnownDiagnosticTags.PortedFxCopRule);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.EnableConcurrentExecution();
+            analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            analysisContext.RegisterSymbolAction(AnalyzeNamedType, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeNamedType(SymbolAnalysisContext context)
+        {
+            var symbol = (INamedTypeSymbol)context.Symbol;
+            if (symbol.TypeKind == TypeKind.Enum && 
+                symbol.GetMembers().Any(m => m.Kind == SymbolKind.Field && m.Name.StartsWith(symbol.Name, StringComparison.OrdinalIgnoreCase)))
+            {
+                context.ReportDiagnostic(symbol.CreateDiagnostic(Rule, symbol.Name));
+            }
+        }
+    }
+}
---
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/DoNotPrefixEnumValuesWithTypeNameTests.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeQuality.Analyzers.UnitTests.ApiDesignGuidelines
+{
+    public class DoNotPrefixEnumValuesWithTypeNameTests : DiagnosticAnalyzerTestBase
+    {
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotPrefixEnumValuesWithTypeNameAnalyzer();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotPrefixEnumValuesWithTypeNameAnalyzer();
+        }
+
+        [Fact]
+        public void CSharp_NoDiagnostic_NoPrefix()
+        {
+            VerifyCSharp(@" 
+                class A
+                { 
+                    enum State
+                    {
+                        Ok = 0,
+                        Error = 1,
+                        Unknown = 2
+                    };
+                }");
+        }
+
+        [Fact]
+        public void Basic_NoDiagnostic_NoPrefix()
+        {
+            VerifyBasic(@"
+                Class A
+                    Private Enum State
+                        Ok = 0
+                        Err = 1
+                        Unknown = 2
+                    End Enum
+                End Class");
+        }
+
+        [Fact]
+        public void CSharp_Diagnostic_OneValuePrefixed()
+        {
+            VerifyCSharp(@"
+                class A
+                {
+                    enum State
+                    {
+                        Ok = 0,
+                        StateError = 1,
+                        Unknown = 2
+                    };
+                }",
+                GetCSharpResultAt(4, 26, DoNotPrefixEnumValuesWithTypeNameAnalyzer.Rule, "State"));
+        }
+
+        [Fact]
+        public void Basic_Diagnostic_OneValuePrefixed()
+        {
+            VerifyBasic(@"
+                Class A
+                    Private Enum State
+                        Ok = 0
+                        StateErr = 1
+                        Unknown = 2
+                    End Enum
+                End Class
+                ",
+                GetBasicResultAt(3, 34, DoNotPrefixEnumValuesWithTypeNameAnalyzer.Rule, "State"));
+        }
+
+        [Fact]
+        public void CSharp_Diagnostic_EachValuePrefixed()
+        {
+            VerifyCSharp(@"
+                class A
+                {
+                    enum State
+                    {
+                        StateOk = 0,
+                        StateError = 1,
+                        StateUnknown = 2
+                    };
+                }",
+                GetCSharpResultAt(4, 26, DoNotPrefixEnumValuesWithTypeNameAnalyzer.Rule, "State"));
+        }
+
+        [Fact]
+        public void CSharp_Diagnostic_PrefixCaseDiffers()
+        {
+            VerifyCSharp(@"
+                class A
+                {
+                    enum State
+                    {
+                        stateOk = 0,
+                        Error = 1,
+                        Unknown = 2
+                    };
+                }",
+                GetCSharpResultAt(4, 26, DoNotPrefixEnumValuesWithTypeNameAnalyzer.Rule, "State"));
+        }
+    }
+}

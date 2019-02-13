ef5defc22e9ac765a143b383d1e3d84837451cce
Gen Lu <gel@microsoft.com>
2019-02-12 18:09:32 -0800

    Fix EnumeratorAllocationAnalyzerTests

--- src/PerformanceSensitive.Analyzers/UnitTests/EnumeratorAllocationAnalyzerTests.cs
+++ src/PerformanceSensitive.Analyzers/UnitTests/EnumeratorAllocationAnalyzerTests.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using PerformanceSensitive.CSharp.Analyzers;
-using System.Collections.Immutable;
 using Xunit;
 
 namespace PerformanceSensitive.Analyzers.UnitTests
 {
-    internal class EnumeratorAllocationAnalyzerTests : AllocationAnalyzerTestsBase
+    public class EnumeratorAllocationAnalyzerTests : AllocationAnalyzerTestsBase
     {
         [Fact]
         public void EnumeratorAllocation_Basic()
@@ -17,39 +15,43 @@ namespace PerformanceSensitive.Analyzers.UnitTests
 @"using System.Collections.Generic;
 using System;
 using System.Linq;
+using Roslyn.Utilities;
 
-int[] intData = new[] { 123, 32, 4 };
-IList<int> iListData = new[] { 123, 32, 4 };
-List<int> listData = new[] { 123, 32, 4 }.ToList();
-
-foreach (var i in intData)
+public class MyClass
 {
-    Console.WriteLine(i);
-}
+    [PerformanceSensitive(""uri"")]
+    public void Foo() 
+    {
+        int[] intData = new[] { 123, 32, 4 };
+        IList<int> iListData = new[] { 123, 32, 4 };
+        List<int> listData = new[] { 123, 32, 4 }.ToList();
 
-foreach (var i in listData)
-{
-    Console.WriteLine(i);
-}
+        foreach (var i in intData)
+        {
+            Console.WriteLine(i);
+        }
 
-foreach (var i in iListData) // Allocations (line 19)
-{
-    Console.WriteLine(i);
-}
+        foreach (var i in listData)
+        {
+            Console.WriteLine(i);
+        }
 
-foreach (var i in (IEnumerable<int>)intData) // Allocations (line 24)
-{
-    Console.WriteLine(i);
+        foreach (var i in iListData) // Allocations (line 19)
+        {
+            Console.WriteLine(i);
+        }
+
+        foreach (var i in (IEnumerable<int>)intData) // Allocations (line 24)
+        {
+            Console.WriteLine(i);
+        }
+    }
 }";
-
-            var analyser = new EnumeratorAllocationAnalyzer();
-            var info = ProcessCode(analyser, sampleProgram, ImmutableArray.Create(SyntaxKind.ForEachStatement));
-
-            Assert.Equal(2, info.Allocations.Length);
-            // Diagnostic: (19,16): warning HeapAnalyzerEnumeratorAllocationRule: Non-ValueType enumerator may result in a heap allocation
-            AssertEx.ContainsDiagnostic(info.Allocations, id: EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule.Id, line: 19, character: 16);
-            // Diagnostic: (24,16): warning HeapAnalyzerEnumeratorAllocationRule: Non-ValueType enumerator may result in a heap allocation
-            AssertEx.ContainsDiagnostic(info.Allocations, id: EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule.Id, line: 24, character: 16);
+            VerifyCSharp(sampleProgram, withAttribute: true,
+                        // Test0.cs(25,24): warning HAA0401: Non-ValueType enumerator may result in a heap allocation
+                        GetCSharpResultAt(25, 24, EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule),
+                        // Test0.cs(30,24): warning HAA0401: Non-ValueType enumerator may result in a heap allocation
+                        GetCSharpResultAt(30, 24, EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule));
         }
 
         [Fact]
@@ -58,29 +60,33 @@ foreach (var i in (IEnumerable<int>)intData) // Allocations (line 24)
             var sampleProgram =
 @"using System.Collections.Generic;
 using System;
+using Roslyn.Utilities;
 
-// These next 3 are from the YouTube video 
-foreach (object a in new[] { 1, 2, 3}) // Allocations 'new [] { 1. 2, 3}'
+public class MyClass
 {
-    Console.WriteLine(a.ToString());
-}
+    [PerformanceSensitive(""uri"")]
+    public void Foo() 
+    {
+        // These next 3 are from the YouTube video 
+        foreach (object a in new[] { 1, 2, 3}) // Allocations 'new [] { 1. 2, 3}'
+        {
+            Console.WriteLine(a.ToString());
+        }
 
-IEnumerable<string> fx1 = default(IEnumerable<string>);
-foreach (var f in fx1) // Allocations 'in'
-{
-}
+        IEnumerable<string> fx1 = default(IEnumerable<string>);
+        foreach (var f in fx1) // Allocations 'in'
+        {
+        }
 
-List<string> fx2 = default(List<string>);
-foreach (var f in fx2) // NO Allocations
-{
+        List<string> fx2 = default(List<string>);
+        foreach (var f in fx2) // NO Allocations
+        {
+        }
+    }
 }";
-
-            var analyser = new EnumeratorAllocationAnalyzer();
-            var info = ProcessCode(analyser, sampleProgram, ImmutableArray.Create(SyntaxKind.ForEachStatement, SyntaxKind.InvocationExpression));
-
-            Assert.Single(info.Allocations);
-            // Diagnostic: (11,16): warning HeapAnalyzerEnumeratorAllocationRule: Non-ValueType enumerator may result in a heap allocation
-            AssertEx.ContainsDiagnostic(info.Allocations, id: EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule.Id, line: 11, character: 16);
+            VerifyCSharp(sampleProgram, withAttribute: true,
+                        // Test0.cs(17,24): warning HAA0401: Non-ValueType enumerator may result in a heap allocation
+                        GetCSharpResultAt(17, 24, EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule));
         }
 
         [Fact]
@@ -90,53 +96,63 @@ foreach (var f in fx2) // NO Allocations
 @"using System.Collections.Generic;
 using System.Collections;
 using System;
+using Roslyn.Utilities;
 
-var enumeratorRaw = GetIEnumerableRaw();
-while (enumeratorRaw.MoveNext())
+public class MyClass
 {
-    Console.WriteLine(enumeratorRaw.Current.ToString());
-}
+    [PerformanceSensitive(""uri"")]
+    public void Foo() 
+    {
+        var enumeratorRaw = GetIEnumerableRaw();
+        while (enumeratorRaw.MoveNext())
+        {
+            Console.WriteLine(enumeratorRaw.Current.ToString());
+        }
 
-var enumeratorRawViaIEnumerable = GetIEnumeratorViaIEnumerable();
-while (enumeratorRawViaIEnumerable.MoveNext())
-{
-    Console.WriteLine(enumeratorRawViaIEnumerable.Current.ToString());
-}
+        var enumeratorRawViaIEnumerable = GetIEnumeratorViaIEnumerable();
+        while (enumeratorRawViaIEnumerable.MoveNext())
+        {
+            Console.WriteLine(enumeratorRawViaIEnumerable.Current.ToString());
+        }
+    }
 
-private IEnumerator GetIEnumerableRaw()
-{
-    return new[] { 123, 32, 4 }.GetEnumerator();
-}
+    private IEnumerator GetIEnumerableRaw()
+    {
+        return new[] { 123, 32, 4 }.GetEnumerator();
+    }
 
-private IEnumerator<int> GetIEnumeratorViaIEnumerable()
-{
-    int[] intData = new[] { 123, 32, 4 };
-    return (IEnumerator<int>)intData.GetEnumerator();
+    private IEnumerator<int> GetIEnumeratorViaIEnumerable()
+    {
+        int[] intData = new[] { 123, 32, 4 };
+        return (IEnumerator<int>)intData.GetEnumerator();
+    }
 }";
-
-            var analyser = new EnumeratorAllocationAnalyzer();
-            var expectedNodes = ImmutableArray.Create(SyntaxKind.InvocationExpression);
-            var info = ProcessCode(analyser, sampleProgram, expectedNodes);
-
-            Assert.Single(info.Allocations);
-            // Diagnostic: (11,35): warning HeapAnalyzerEnumeratorAllocationRule: Non-ValueType enumerator may result in a heap allocation ***
-            AssertEx.ContainsDiagnostic(info.Allocations, id: EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule.Id, line: 11, character: 35);
+            VerifyCSharp(sampleProgram, withAttribute: true,
+                        // Test0.cs(17,43): warning HAA0401: Non-ValueType enumerator may result in a heap allocation
+                        GetCSharpResultAt(17, 43, EnumeratorAllocationAnalyzer.ReferenceTypeEnumeratorRule));
         }
 
         [Fact]
         public void EnumeratorAllocation_IterateOverString_NoWarning()
         {
-            var sampleProgram = "foreach (char c in \"foo\") { }";
+            var sampleProgram =
+@"using System;
+using Roslyn.Utilities;
 
-            var analyser = new EnumeratorAllocationAnalyzer();
-            var info = ProcessCode(analyser, sampleProgram, ImmutableArray.Create(SyntaxKind.ForEachStatement));
-
-            Assert.Empty(info.Allocations);
+public class MyClass
+{
+    [PerformanceSensitive(""uri"")]
+    public void Foo() 
+    {
+        foreach (char c in ""foo"") { };
+    }
+}";
+            VerifyCSharp(sampleProgram, withAttribute: true);
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
-            throw new System.NotImplementedException();
+            return new EnumeratorAllocationAnalyzer();
         }
 
         protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()

2dabfa76a91400a6c2ff3b439bdec47811d425ee
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-02-19 16:49:24 +0800

    Do Not Disable SChannel Use of Strong Crypto

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotDisableSchUseStrongCrypto.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotDisableSchUseStrongCrypto : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5361";
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotDisableSchUseStrongCrypto),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotDisableSchUseStrongCryptoMessage),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotDisableSchUseStrongCryptoDescription),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                DiagnosticSeverity.Error,
+                isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                description: s_Description,
+                helpLinkUri: null,
+                customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+            {
+                var compilation = compilationStartAnalysisContext.Compilation;
+                var appContextTypeSymbol = compilation.GetTypeByMetadataName(WellKnownTypes.SystemAppContext);
+
+                if (appContextTypeSymbol == null)
+                {
+                    return;
+                }
+
+                var setSwitchMemberWithStringAndStringParameter = appContextTypeSymbol.GetMembers("SetSwitch").OfType<IMethodSymbol>().GetSingleOrDefaultMemberWithParameterInfos(
+                                                                        GetParameterInfo(compilation.GetSpecialType(SpecialType.System_String)),
+                                                                        GetParameterInfo(compilation.GetSpecialType(SpecialType.System_Boolean)));
+
+                if (setSwitchMemberWithStringAndStringParameter == null)
+                {
+                    return;
+                }
+
+                compilationStartAnalysisContext.RegisterOperationAction(operationAnalysisContext =>
+                {
+                    var invocationOperation = (IInvocationOperation)operationAnalysisContext.Operation;
+                    var methodSymbol = invocationOperation.TargetMethod;
+
+                    if (methodSymbol.Equals(setSwitchMemberWithStringAndStringParameter))
+                    {
+                        var values = invocationOperation.Arguments.Select(s => s.Value.ConstantValue).ToArray();
+
+                        if (values[0].HasValue &&
+                            values[0].Value != null &&
+                            values[0].Value.Equals("Switch.System.Net.DontEnableSchUseStrongCrypto") &&
+                            values[1].HasValue &&
+                            values[1].Value.Equals(true))
+                        {
+                            operationAnalysisContext.ReportDiagnostic(
+                                invocationOperation.CreateDiagnostic(
+                                    Rule,
+                                    methodSymbol.Name));
+                        }
+                    }
+                }, OperationKind.Invocation);
+            });
+        }
+
+        private static ParameterInfo GetParameterInfo(INamedTypeSymbol type, bool isArray = false, int arrayRank = 0, bool isParams = false)
+        {
+            return ParameterInfo.GetParameterInfo(type, isArray, arrayRank, isParams);
+        }
+    }
+}

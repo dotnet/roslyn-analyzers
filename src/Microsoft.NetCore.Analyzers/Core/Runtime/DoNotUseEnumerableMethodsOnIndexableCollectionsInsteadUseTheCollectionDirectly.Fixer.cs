95f37e35fdfc7ecb8b98d2304930f5cd167ea5a3
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2019-03-06 22:03:38 +0400

    Fix for CA1826 Enumerable.First

--- src/Microsoft.NetCore.Analyzers/Core/Runtime/DoNotUseEnumerableMethodsOnIndexableCollectionsInsteadUseTheCollectionDirectly.Fixer.cs
+++ src/Microsoft.NetCore.Analyzers/Core/Runtime/DoNotUseEnumerableMethodsOnIndexableCollectionsInsteadUseTheCollectionDirectly.Fixer.cs
@@ -1,7 +1,15 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.CodeAnalysis.Text;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.NetCore.Analyzers.Runtime
@@ -21,9 +29,51 @@ namespace Microsoft.NetCore.Analyzers.Runtime
 
         public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
         {
+            var diagnostic = context.Diagnostics[0];
+            var codeAction = CodeAction.Create("Use indexer", c => UseCollectionDirectly(context.Document, context.Span, c));
+            context.RegisterCodeFix(codeAction, diagnostic);
+
             // Fixer not yet implemented.
             return Task.CompletedTask;
 
+        }
+
+        private async Task<Document> UseCollectionDirectly(Document document, TextSpan span, CancellationToken c)
+        {
+            var syntaxRoot = await document.GetSyntaxRootAsync(c);
+            var semanticModel = await document.GetSemanticModelAsync(c);
+
+            var invocationSyntax = GetSyntaxOfType<InvocationExpressionSyntax>(syntaxRoot.FindNode(span));
+            var invocationOp = (IInvocationOperation)semanticModel.GetOperation(invocationSyntax);
+
+            //implemented for "Enumerable.First" only
+            if (invocationOp.TargetMethod.Name != "First")
+            {
+                return document;
+            }
+
+            var collectionSyntax = GetSyntaxOfType<ExpressionSyntax>(invocationOp.Arguments[0].Syntax);
+
+            var generator = SyntaxGenerator.GetGenerator(document);
+            var trailing = collectionSyntax.GetTrailingTrivia();
+            collectionSyntax = collectionSyntax.WithTrailingTrivia(SyntaxTriviaList.Empty);
+            var indexSyntax = generator.LiteralExpression(0);
+            var elementAccessSyntax = generator.ElementAccessExpression(collectionSyntax, indexSyntax).WithTrailingTrivia(trailing);
+
+            var newSyntaxRoot = syntaxRoot.ReplaceNode(invocationSyntax, elementAccessSyntax);
+            return document.WithSyntaxRoot(newSyntaxRoot);
+        }
+
+        private T GetSyntaxOfType<T>(SyntaxNode node)
+        {
+            if (!(node is T result))
+            {
+                result = node
+                    .ChildNodes()
+                    .OfType<T>()
+                    .First();
+            }
+            return result;
         }
     }
 }

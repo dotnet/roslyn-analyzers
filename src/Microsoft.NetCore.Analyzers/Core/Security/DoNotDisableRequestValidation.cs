2535d787090aa13c69b4ecf093e5bc22665e14b1
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-03-08 10:51:01 +0800

    Do Not Disable Request Validation

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotDisableRequestValidation.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotDisableRequestValidation : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5363";
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotDisableRequestValidation),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotDisableRequestValidationMessage),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotDisableRequestValidationDescription),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                DiagnosticHelpers.DefaultDiagnosticSeverity,
+                isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                description: s_Description,
+                helpLinkUri: null,
+                customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(
+                (CompilationStartAnalysisContext compilationStartAnalysisContext) =>
+                {
+                    var validateInputAttributeTypeSymbol = compilationStartAnalysisContext.Compilation.GetTypeByMetadataName(WellKnownTypeNames.SystemWebMvcValidateInputAttribute);
+
+                    if (validateInputAttributeTypeSymbol == null)
+                    {
+                        return;
+                    }
+
+                    compilationStartAnalysisContext.RegisterSymbolAction(
+                        (SymbolAnalysisContext symbolAnalysisContext) =>
+                        {
+                            var methodSymbol = (IMethodSymbol)symbolAnalysisContext.Symbol;
+                            var attr = methodSymbol.GetAttributes().FirstOrDefault(s => s.AttributeClass.Equals(validateInputAttributeTypeSymbol));
+
+                            if (attr == null)
+                            {
+                                return;
+                            }
+
+                            var constructorArguments = attr.ConstructorArguments;
+
+                            if (constructorArguments != null &&
+                                constructorArguments.Length == 1 &&
+                                !constructorArguments[0].IsNull &&
+                                constructorArguments[0].Value.Equals(false))
+                            {
+                                symbolAnalysisContext.ReportDiagnostic(
+                                    methodSymbol.CreateDiagnostic(
+                                        Rule,
+                                        methodSymbol.Name));
+                            }
+                        }, SymbolKind.Method);
+                });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotDisableRequestValidationTests.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    public class DoNotDisableRequestValidationTests : DiagnosticAnalyzerTestBase
+    {
+        protected void VerifyCSharpWithDependencies(string source, params DiagnosticResult[] expected)
+        {
+            string validateInputAttributeCSharpSourceCode = @"
+using System.IO;
+
+namespace System.Web.Mvc
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Method, AllowMultiple=false, Inherited=true)]
+    public class ValidateInputAttribute : System.Attribute
+    {
+        public ValidateInputAttribute (bool enableValidation)
+        {
+        }
+    }
+}";
+            this.VerifyCSharp(
+                new[] { source, validateInputAttributeCSharpSourceCode }.ToFileAndSource(),
+                expected);
+        }
+
+        [Fact]
+        public void TestLiteralDiagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System;
+using System.Web.Mvc;
+
+class TestClass
+{
+    [ValidateInput(false)]
+    public void TestMethod()
+    {
+    }
+}",
+            GetCSharpResultAt(8, 17, DoNotDisableRequestValidation.Rule, "TestMethod"));
+        }
+
+        [Fact]
+        public void TestConstDiagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System;
+using System.Web.Mvc;
+
+class TestClass
+{
+    private const bool flag = false;
+
+    [ValidateInput(flag)]
+    public void TestMethod()
+    {
+    }
+}",
+            GetCSharpResultAt(10, 17, DoNotDisableRequestValidation.Rule, "TestMethod"));
+        }
+
+        [Fact]
+        public void TestLiteralNoDiagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System;
+using System.Web.Mvc;
+
+class TestClass
+{
+    [ValidateInput(true)]
+    public void TestMethod()
+    {
+    }
+}");
+        }
+
+        [Fact]
+        public void TestConstNoDiagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System;
+using System.Web.Mvc;
+
+class TestClass
+{
+    private const bool flag = true;
+
+    [ValidateInput(flag)]
+    public void TestMethod()
+    {
+    }
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotDisableRequestValidation();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotDisableRequestValidation();
+        }
+    }
+}

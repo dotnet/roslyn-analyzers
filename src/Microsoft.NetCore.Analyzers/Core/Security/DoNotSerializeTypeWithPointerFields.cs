12cc02467ece6f980e61e66b9d918f0e8eb928c7
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-03-19 16:11:25 +0800

    Do Not Serialize Type With Pointer Fields

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotSerializeTypeWithPointerFields.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotSerializeTypeWithPointerFields : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5367";
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotSerializeTypesWithPointerFields),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotSerializeTypesWithPointerFieldsMessage),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotSerializeTypesWithPointerFieldsDescription),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                DiagnosticHelpers.DefaultDiagnosticSeverity,
+                isEnabledByDefault: false,
+                description: s_Description,
+                helpLinkUri: null,
+                customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public sealed override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(
+                (CompilationStartAnalysisContext compilationStartAnalysisContext) =>
+                {
+                    var compilation = compilationStartAnalysisContext.Compilation;
+                    var serializableAttributeTypeSymbol = WellKnownTypes.SerializableAttribute(compilation);
+
+                    if (serializableAttributeTypeSymbol == null)
+                    {
+                        return;
+                    }
+
+                    var nonSerializedAttribute = WellKnownTypes.NonSerializedAttribute(compilation);
+                    var visitedType = new ConcurrentDictionary<ITypeSymbol, bool>();
+                    var pointerFields = new ConcurrentDictionary<IFieldSymbol, bool>();
+
+                    compilationStartAnalysisContext.RegisterSymbolAction(
+                        (SymbolAnalysisContext symbolAnalysisContext) =>
+                        {
+                            LookForSerializationWithPointerFields((ITypeSymbol)symbolAnalysisContext.Symbol);
+                        }, SymbolKind.NamedType);
+
+                    compilationStartAnalysisContext.RegisterCompilationEndAction(
+                        (CompilationAnalysisContext compilationAnalysisContext) =>
+                        {
+                            foreach (var pointerField in pointerFields.Keys)
+                            {
+                                compilationAnalysisContext.ReportDiagnostic(
+                                    pointerField.CreateDiagnostic(
+                                        Rule,
+                                        pointerField.Name));
+                            }
+                        });
+
+
+                    /// <summary>
+                    /// Look for serialization of a type with pointer type fields directly and indirectly.
+                    /// </summary>
+                    /// <param name="typeSymbol">The symbol of the type to be analyzed</param>
+                    void LookForSerializationWithPointerFields(ITypeSymbol typeSymbol)
+                    {
+                        if (typeSymbol.IsInSource() &&
+                            typeSymbol.HasAttribute(serializableAttributeTypeSymbol) &&
+                            visitedType.TryAdd(typeSymbol, true))
+                        {
+                            var fields = typeSymbol.GetMembers().OfType<IFieldSymbol>().Where(s => nonSerializedAttribute != null &&
+                                                                                                !s.HasAttribute(nonSerializedAttribute) &&
+                                                                                                !s.IsStatic);
+
+                            foreach (var field in fields)
+                            {
+                                var fieldType = field.Type;
+
+                                if (fieldType is IPointerTypeSymbol pointerTypeField &&
+                                    (pointerTypeField.PointedAtType.TypeKind == TypeKind.Struct ||
+                                    pointerTypeField.PointedAtType.TypeKind == TypeKind.Pointer))
+                                {
+                                    pointerFields.TryAdd(field, true);
+                                }
+                                else
+                                {
+                                    LookForSerializationWithPointerFields(fieldType);
+                                }
+                            }
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotSerializeTypeWithPointerFieldsTests.cs
@@ -0,0 +1,257 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    public class DoNotSerializeTypeWithPointerFieldsTests : DiagnosticAnalyzerTestBase
+    {
+        [Fact]
+        public void TestChildPointerToStructureDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private TestClassB* pointer;
+}
+
+[Serializable()]
+struct TestClassB
+{
+}",
+            GetCSharpResultAt(7, 25, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestChildPointerToIntegerDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private int* pointer;
+}",
+            GetCSharpResultAt(7, 18, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestChildPointerToBooleanDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private bool* pointer;
+}",
+            GetCSharpResultAt(7, 19, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestChildPointerToPointerDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private int** pointer;
+}",
+            GetCSharpResultAt(7, 19, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestGrandchildPointerToIntegerDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private TestClassB testClassB;
+}
+
+[Serializable()]
+unsafe struct TestClassB
+{
+    public int* pointer;
+}",
+            GetCSharpResultAt(13, 17, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestChildPointerAndGrandchildPointerDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private TestClassB* pointer1;
+}
+
+[Serializable()]
+unsafe struct TestClassB
+{
+    public int* pointer2;
+}",
+            GetCSharpResultAt(7, 25, DoNotSerializeTypeWithPointerFields.Rule, "pointer1"),
+            GetCSharpResultAt(13, 17, DoNotSerializeTypeWithPointerFields.Rule, "pointer2"));
+        }
+
+        [Fact]
+        public void TestMultiChildPointersDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private int* pointer1;
+    
+    private int* pointer2;
+}",
+            GetCSharpResultAt(7, 18, DoNotSerializeTypeWithPointerFields.Rule, "pointer1"),
+            GetCSharpResultAt(9, 18, DoNotSerializeTypeWithPointerFields.Rule, "pointer2"));
+        }
+
+        [Fact]
+        public void TestChildPointerToSelfDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe struct TestClassA
+{
+    private TestClassA* pointer;
+}",
+            GetCSharpResultAt(7, 25, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestGrandchildPointerToSelfDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe struct TestClassA
+{
+    public TestClassB testClassB;
+}
+
+[Serializable()]
+unsafe struct TestClassB
+{
+    public TestClassB* pointer;
+}",
+            GetCSharpResultAt(13, 24, DoNotSerializeTypeWithPointerFields.Rule, "pointer"));
+        }
+
+        [Fact]
+        public void TestSubclassWithPointerFieldsDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    protected TestClassB* pointer1;
+}
+
+[Serializable()]
+struct TestClassB
+{
+}
+
+[Serializable()]
+unsafe class TestClassC : TestClassA
+{
+    private int* pointer2;
+}",
+            GetCSharpResultAt(7, 27, DoNotSerializeTypeWithPointerFields.Rule, "pointer1"),
+            GetCSharpResultAt(18, 18, DoNotSerializeTypeWithPointerFields.Rule, "pointer2"));
+        }
+
+        [Fact]
+        public void TestWithoutPointerFieldNoDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private int normalField;
+}");
+        }
+
+        [Fact]
+        public void TestWithoutSerializableAttributeNoDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+unsafe class TestClassA
+{
+    private int* pointer;
+}");
+        }
+
+        [Fact]
+        public void TestChildPointerWithNonSerializedNoDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    [NonSerialized]
+    private int* pointer;
+}");
+        }
+
+        [Fact]
+        public void TestChildPointerWithStaticNoDiagnostic()
+        {
+            VerifyCSharpUnsafeCode(@"
+using System;
+
+[Serializable()]
+unsafe class TestClassA
+{
+    private static int* pointer;
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotSerializeTypeWithPointerFields();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotSerializeTypeWithPointerFields();
+        }
+    }
+}

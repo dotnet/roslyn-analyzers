1e9ec42af5b114236e2a60097d515f7c9826cdeb
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-03-19 15:43:50 +0800

    SetViewStateUserKey

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/SetViewStateUserKey.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class SetViewStateUserKey : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5368";
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.SetViewStateUserKey),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.SetViewStateUserKeyMessage),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.SetViewStateUserKeyDescription),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                DiagnosticHelpers.DefaultDiagnosticSeverity,
+                isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                description: s_Description,
+                helpLinkUri: null,
+                customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+            {
+                var compilation = compilationStartAnalysisContext.Compilation;
+                var pageTypeSymbol = compilation.GetTypeByMetadataName(WellKnownTypeNames.SystemWebUIPage);
+
+                if (pageTypeSymbol == null)
+                {
+                    return;
+                }
+
+                var eventArgsTypeSymbol = compilation.GetTypeByMetadataName(WellKnownTypeNames.SystemEventArgs);
+
+                if (eventArgsTypeSymbol == null)
+                {
+                    return;
+                }
+
+                compilationStartAnalysisContext.RegisterSymbolAction(symbolAnalysisContext =>
+                {
+                    var classSymbol = (INamedTypeSymbol)symbolAnalysisContext.Symbol;
+                    var baseClassSymbol = classSymbol.BaseType;
+
+                    if (baseClassSymbol.Equals(pageTypeSymbol))
+                    {
+                        var onInitMethodSymbol = classSymbol.GetMembers().OfType<IMethodSymbol>().FirstOrDefault(s => s.Name == "OnInit" &&
+                                                                                                        s.Parameters.Length == 1 &&
+                                                                                                        s.Parameters[0].Type.Equals(eventArgsTypeSymbol) &&
+                                                                                                        s.IsProtected());
+
+                        if (onInitMethodSymbol != null)
+                        {
+                            if (onInitMethodSymbol.GetTopmostOperationBlock(compilation)
+                                                    .Descendants()
+                                                    .Where(s => s is ISimpleAssignmentOperation simpleAssignmentOperation &&
+                                                                simpleAssignmentOperation.Target is IPropertyReferenceOperation propertyReferenceOperation &&
+                                                                propertyReferenceOperation.Property.Name == "ViewStateUserKey")
+                                                    .Count() != 0)
+                            {
+                                return;
+                            }
+                        }
+
+                        symbolAnalysisContext.ReportDiagnostic(
+                                    classSymbol.CreateDiagnostic(
+                                        Rule,
+                                        classSymbol.Name));
+                    }
+                }, SymbolKind.NamedType);
+            });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/SetViewStateUserKeyTests.cs
@@ -0,0 +1,196 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    public class SetViewStateUserKeyTests : DiagnosticAnalyzerTestBase
+    {
+        [Fact]
+        public void TestSubclassWithoutOnInitMethodDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected void TestMethod (EventArgs e)
+    {
+        ViewStateUserKey = ""ViewStateUserKey"";
+    }
+}",
+            GetCSharpResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+
+            VerifyBasic(@"
+Imports System
+Imports System.Web.UI
+
+class TestClass
+    Inherits Page
+    protected Sub TestMethod (ByVal e As EventArgs)
+        ViewStateUserKey = ""ViewStateUserKey""
+    End Sub
+End Class",
+            GetBasicResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+        }
+
+        [Fact]
+        public void TestOverrideModifierWithoutSettingViewStateUserKeyDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected override void OnInit (EventArgs e)
+    {
+    }
+}",
+            GetCSharpResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+
+            VerifyBasic(@"
+Imports System
+Imports System.Web.UI
+
+class TestClass
+    Inherits Page
+    protected Sub OnInit (ByVal e As EventArgs)
+    End Sub
+End Class",
+            GetBasicResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+        }
+
+        [Fact]
+        public void TestNewModifierWithoutSettingViewStateUserKeyDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected new void OnInit (EventArgs e)
+    {
+    }
+}",
+            GetCSharpResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+        }
+
+        [Fact]
+        public void TestNoModifierWithoutSettingViewStateUserKeyDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected void OnInit (EventArgs e)
+    {
+    }
+}",
+            GetCSharpResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+        }
+
+        [Fact]
+        public void TestOverloadOnInitWithSettingViewStateUserKeyDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected internal void OnInit ()
+    {
+        ViewStateUserKey = ""ViewStateUserKey"";
+    }
+}",
+            GetCSharpResultAt(5, 7, SetViewStateUserKey.Rule, "TestClass"));
+        }
+
+        [Fact]
+        public void TestSubclassWithSettingViewStateUserKeyNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected override void OnInit (EventArgs e)
+    {
+        ViewStateUserKey = ""ViewStateUserKey"";
+    }
+}");
+        }
+
+        [Fact]
+        public void TestNewModifierNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected new void OnInit (EventArgs e)
+    {
+        ViewStateUserKey = ""ViewStateUserKey"";
+    }
+}");
+        }
+
+        [Fact]
+        public void TestWithoutModifierNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Web.UI;
+
+class TestClass : Page
+{
+    protected void OnInit (EventArgs e)
+    {
+        ViewStateUserKey = ""ViewStateUserKey"";
+    }
+}");
+        }
+
+        [Fact]
+        public void TestOrinaryClassWithSettingViewStateUserKeyNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+
+class TestClass
+{
+    public string ViewStateUserKey { get; set; }
+
+    protected void OnInit (EventArgs e)
+    {
+        ViewStateUserKey = ""ViewStateUserKey"";
+    }
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new SetViewStateUserKey();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SetViewStateUserKey();
+        }
+    }
+}

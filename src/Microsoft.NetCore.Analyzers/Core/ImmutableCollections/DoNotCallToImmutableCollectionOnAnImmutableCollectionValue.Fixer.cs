0b42798e53d91cbc00cb6f2d0fdc2fb3265c402a
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2019-03-22 01:48:18 +0400

    CA2009 fixer implemented

--- src/Microsoft.NetCore.Analyzers/Core/ImmutableCollections/DoNotCallToImmutableCollectionOnAnImmutableCollectionValue.Fixer.cs
+++ src/Microsoft.NetCore.Analyzers/Core/ImmutableCollections/DoNotCallToImmutableCollectionOnAnImmutableCollectionValue.Fixer.cs
@@ -1,8 +1,15 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.NetCore.Analyzers.ImmutableCollections
 {
@@ -19,10 +26,70 @@ namespace Microsoft.NetCore.Analyzers.ImmutableCollections
             return WellKnownFixAllProviders.BatchFixer;
         }
 
-        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        private static readonly ImmutableArray<string> ToImmutableMethodNames = new[]
         {
-            // Fixer not yet implemented.
-            return Task.CompletedTask;
+            "ToImmutableArray",
+            "ToImmutableDictionary",
+            "ToImmutableHashSet",
+            "ToImmutableList",
+            "ToImmutableSortedDictionary",
+            "ToImmutableSortedSet"
+        }.ToImmutableArray();
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            Diagnostic diagnostic = context.Diagnostics.FirstOrDefault();
+            if (diagnostic == null)
+            {
+                return;
+            }
+
+            Document document = context.Document;
+            TextSpan span = context.Span;
+            SyntaxNode root = await document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            SyntaxNode invocationNode = root.FindNode(span, getInnermostNodeForTie: true);
+            if (invocationNode == null)
+            {
+                return;
+            }
+
+            SemanticModel semanticModel = await document.GetSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
+            var invocationOp = semanticModel.GetOperation(invocationNode) as IInvocationOperation;
+            if (invocationOp == null || !ToImmutableMethodNames.Contains(invocationOp.TargetMethod.Name))
+            {
+                return;
+            }
+
+            string title = SystemCollectionsImmutableAnalyzersResources.RemoveRedundantCall;
+
+            context.RegisterCodeFix(new MyCodeAction(title,
+                                        async cancellationToken => await RemoveRedundantCall(document, root, invocationNode, invocationOp).ConfigureAwait(false),
+                                        equivalenceKey: title),
+                                    diagnostic);
+        }
+
+        private static Task<Document> RemoveRedundantCall(Document document, SyntaxNode root, SyntaxNode invocationNode, IInvocationOperation invocationOp)
+        {
+            SyntaxNode instance = GetInstance(invocationOp).WithTrailingTrivia(invocationNode.GetTrailingTrivia());
+            SyntaxNode newRoot = root.ReplaceNode(invocationNode, instance);
+            Document newDocument = document.WithSyntaxRoot(newRoot);
+            return Task.FromResult(newDocument);
+        }
+
+        private static SyntaxNode GetInstance(IInvocationOperation invocationOp)
+        {
+            return invocationOp.TargetMethod.IsExtensionMethod && invocationOp.Language != LanguageNames.VisualBasic ?
+                invocationOp.Arguments[0].Value.Syntax :
+                invocationOp.Instance.Syntax;
+        }
+
+        // Needed for Telemetry (https://github.com/dotnet/roslyn-analyzers/issues/192)
+        private class MyCodeAction : DocumentChangeAction
+        {
+            public MyCodeAction(string title, Func<CancellationToken, Task<Document>> createChangedDocument, string equivalenceKey)
+                : base(title, createChangedDocument, equivalenceKey)
+            {
+            }
         }
     }
 }
--- src/Microsoft.NetCore.Analyzers/UnitTests/ImmutableCollections/DoNotCallToImmutableCollectionOnAnImmutableCollectionValueTests.Fixer.cs
+++ src/Microsoft.NetCore.Analyzers/UnitTests/ImmutableCollections/DoNotCallToImmutableCollectionOnAnImmutableCollectionValueTests.Fixer.cs
@@ -5,6 +5,9 @@ using Microsoft.NetCore.VisualBasic.Analyzers.ImmutableCollections;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Test.Utilities;
+using Xunit;
+using System.Collections.Immutable;
+using Test.Utilities.MinimalImplementations;
 
 namespace Microsoft.NetCore.Analyzers.ImmutableCollections.UnitTests
 {
@@ -28,6 +31,200 @@ namespace Microsoft.NetCore.Analyzers.ImmutableCollections.UnitTests
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
             return new CSharpDoNotCallToImmutableCollectionOnAnImmutableCollectionValueFixer();
+        }
+
+        public static readonly TheoryData<string> CollectionNames_Arity1 = new TheoryData<string>
+        {
+            nameof(ImmutableArray),
+            nameof(ImmutableHashSet),
+            nameof(ImmutableList),
+            nameof(ImmutableSortedSet)
+        };
+
+        public static readonly TheoryData<string> CollectionNames_Arity2 = new TheoryData<string>
+        {
+            nameof(ImmutableDictionary),
+            nameof(ImmutableSortedDictionary)
+        };
+
+        [Theory]
+        [MemberData(nameof(CollectionNames_Arity1))]
+        public void CA2009_Arity1_CSharp(string collectionName)
+        {
+            var initial = $@"
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+static class Extensions
+{{
+     public static {collectionName}<TSource> To{collectionName}<TSource>(this IEnumerable<TSource> items)
+     {{
+         return default({collectionName}<TSource>);
+     }}
+}}
+
+class C
+{{
+    public void M(IEnumerable<int> p1, List<int> p2, {collectionName}<int> p3)
+    {{
+        var x = p1.To{collectionName}().To{collectionName}();
+        var y = p3.To{collectionName}();
+    }}
+}}";
+
+            var expected = $@"
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+static class Extensions
+{{
+     public static {collectionName}<TSource> To{collectionName}<TSource>(this IEnumerable<TSource> items)
+     {{
+         return default({collectionName}<TSource>);
+     }}
+}}
+
+class C
+{{
+    public void M(IEnumerable<int> p1, List<int> p2, {collectionName}<int> p3)
+    {{
+        var x = p1.To{collectionName}();
+        var y = p3;
+    }}
+}}";
+            VerifyCSharpFix(new[] { initial, ImmutableCollectionsSource.CSharp }, new[] { expected, ImmutableCollectionsSource.CSharp });
+        }
+
+        [Theory]
+        [MemberData(nameof(CollectionNames_Arity1))]
+        public void CA2009_Arity1_Basic(string collectionName)
+        {
+            var initial = $@"
+Imports System.Collections.Generic
+Imports System.Collections.Immutable
+
+Module Extensions
+	<System.Runtime.CompilerServices.Extension> _
+	Public Function To{collectionName}(Of TSource)(items As IEnumerable(Of TSource)) As {collectionName}(Of TSource)
+		Return Nothing
+	End Function
+End Module
+
+Class C
+	Public Sub M(p1 As IEnumerable(Of Integer), p2 As List(Of Integer), p3 As {collectionName}(Of Integer))
+		Dim x = p1.To{collectionName}().To{collectionName}()
+		Dim y = p3.To{collectionName}()
+	End Sub
+End Class";
+
+            var expected = $@"
+Imports System.Collections.Generic
+Imports System.Collections.Immutable
+
+Module Extensions
+	<System.Runtime.CompilerServices.Extension> _
+	Public Function To{collectionName}(Of TSource)(items As IEnumerable(Of TSource)) As {collectionName}(Of TSource)
+		Return Nothing
+	End Function
+End Module
+
+Class C
+	Public Sub M(p1 As IEnumerable(Of Integer), p2 As List(Of Integer), p3 As {collectionName}(Of Integer))
+		Dim x = p1.To{collectionName}()
+		Dim y = p3
+	End Sub
+End Class";
+            VerifyBasicFix(new[] { initial, ImmutableCollectionsSource.Basic }, new[] { expected, ImmutableCollectionsSource.Basic });
+        }
+
+        [Theory]
+        [MemberData(nameof(CollectionNames_Arity2))]
+        public void CA2009_Arity2_CSharp(string collectionName)
+        {
+            var initial = $@"
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+static class Extensions
+{{
+     public static {collectionName}<TKey, TValue> To{collectionName}<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> items)
+     {{
+         return default({collectionName}<TKey, TValue>);
+     }}
+}}
+
+class C
+{{
+    public void M(IEnumerable<KeyValuePair<int, int>> p1, List<KeyValuePair<int, int>> p2, {collectionName}<int, int> p3)
+    {{
+        var x = p1.To{collectionName}().To{collectionName}();
+        var y = p3.To{collectionName}();
+    }}
+}}";
+
+            var expected = $@"
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+static class Extensions
+{{
+     public static {collectionName}<TKey, TValue> To{collectionName}<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> items)
+     {{
+         return default({collectionName}<TKey, TValue>);
+     }}
+}}
+
+class C
+{{
+    public void M(IEnumerable<KeyValuePair<int, int>> p1, List<KeyValuePair<int, int>> p2, {collectionName}<int, int> p3)
+    {{
+        var x = p1.To{collectionName}();
+        var y = p3;
+    }}
+}}";
+            VerifyCSharpFix(new[] { initial, ImmutableCollectionsSource.CSharp }, new[] { expected, ImmutableCollectionsSource.CSharp });
+        }
+
+        [Theory]
+        [MemberData(nameof(CollectionNames_Arity2))]
+        public void CA2009_Arity2_Basic(string collectionName)
+        {
+            var initial = $@"
+Imports System.Collections.Generic
+Imports System.Collections.Immutable
+
+Module Extensions
+	<System.Runtime.CompilerServices.Extension> _
+	Public Function To{collectionName}(Of TKey, TValue)(items As IEnumerable(Of KeyValuePair(Of TKey, TValue))) As {collectionName}(Of TKey, TValue)
+		Return Nothing
+	End Function
+End Module
+
+Class C
+	Public Sub M(p1 As IEnumerable(Of KeyValuePair(Of Integer, Integer)), p2 As List(Of KeyValuePair(Of Integer, Integer)), p3 As {collectionName}(Of Integer, Integer))
+		Dim x = p1.To{collectionName}().To{collectionName}()
+		Dim y = p3.To{collectionName}()
+	End Sub
+End Class";
+
+            var expected = $@"
+Imports System.Collections.Generic
+Imports System.Collections.Immutable
+
+Module Extensions
+	<System.Runtime.CompilerServices.Extension> _
+	Public Function To{collectionName}(Of TKey, TValue)(items As IEnumerable(Of KeyValuePair(Of TKey, TValue))) As {collectionName}(Of TKey, TValue)
+		Return Nothing
+	End Function
+End Module
+
+Class C
+	Public Sub M(p1 As IEnumerable(Of KeyValuePair(Of Integer, Integer)), p2 As List(Of KeyValuePair(Of Integer, Integer)), p3 As {collectionName}(Of Integer, Integer))
+		Dim x = p1.To{collectionName}()
+		Dim y = p3
+	End Sub
+End Class";
+            VerifyBasicFix(new[] { initial, ImmutableCollectionsSource.Basic }, new[] { expected, ImmutableCollectionsSource.Basic });
         }
     }
 }

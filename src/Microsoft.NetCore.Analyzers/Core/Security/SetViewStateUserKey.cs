59b2a8c311a2cf2206279f20d59b08b110055952
Paul Ming <paulming@microsoft.com>
2019-03-29 13:12:50 -0700

    Disabling rules

--- src/Microsoft.NetCore.Analyzers/Core/Security/SetViewStateUserKey.cs
+++ src/Microsoft.NetCore.Analyzers/Core/Security/SetViewStateUserKey.cs
@@ -33,7 +33,7 @@ namespace Microsoft.NetCore.Analyzers.Security
                 s_Message,
                 DiagnosticCategory.Security,
                 DiagnosticHelpers.DefaultDiagnosticSeverity,
-                isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                isEnabledByDefault: false,    // https://github.com/dotnet/roslyn-analyzers/issues/2258
                 description: s_Description,
                 helpLinkUri: null,
                 customTags: WellKnownDiagnosticTags.Telemetry);
@@ -63,6 +63,9 @@ namespace Microsoft.NetCore.Analyzers.Security
                 {
                     return;
                 }
+
+                if (!System.Diagnostics.Debugger.IsAttached)
+                    System.Diagnostics.Debugger.Launch();
 
                 var objectTypeSymbol = WellKnownTypes.Object(compilation);
 

d16ae2842197db72d7ae7a5329839ca616627a23
Lin Chen (RichCaptions) <linche@microsoft.com>
2019-06-05 14:20:12 -0700

    Implement the Semmle rule ZipSlip.ql.

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotAddArchiveItemPathToTheTargetFileSystemPath.cs
@@ -0,0 +1,141 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Analyzer.Utilities.FlowAnalysis.Analysis.TaintedDataAnalysis;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotAddArchiveItemPathToTheTargetFileSystemPath : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5389";
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotAddArchiveItemPathToTheTargetFileSystemPath),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotAddArchiveItemPathToTheTargetFileSystemPathMessage),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(SystemSecurityCryptographyResources.DoNotAddArchiveItemPathToTheTargetFileSystemPathDescription),
+            SystemSecurityCryptographyResources.ResourceManager,
+            typeof(SystemSecurityCryptographyResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                DiagnosticHelpers.DefaultDiagnosticSeverity,
+                isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                description: s_Description,
+                helpLinkUri: null,
+                customTags: WellKnownDiagnosticTagsExtensions.DataflowAndTelemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        /// <summary>
+        /// Kind of tainted data sink.
+        /// </summary>
+        private static SinkKind SinkKind { get { return SinkKind.ZipSlip; } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(
+                (CompilationStartAnalysisContext compilationStartAnalysisContext) =>
+                {
+                    var wellKnownTypeProvider = WellKnownTypeProvider.GetOrCreate(compilationStartAnalysisContext.Compilation);
+                    var taintedDataConfig = TaintedDataConfig.GetOrCreate(compilationStartAnalysisContext.Compilation);
+                    var sourceInfoSymbolMap = taintedDataConfig.GetSourceSymbolMap(SinkKind);
+
+                    if (sourceInfoSymbolMap.IsEmpty)
+                    {
+                        return;
+                    }
+
+                    var sinkInfoSymbolMap = taintedDataConfig.GetSinkSymbolMap(SinkKind);
+
+                    if (sinkInfoSymbolMap.IsEmpty)
+                    {
+                        return;
+                    }
+
+                    compilationStartAnalysisContext.RegisterOperationBlockStartAction(
+                        operationBlockStartContext =>
+                        {
+                            var owningSymbol = operationBlockStartContext.OwningSymbol;
+                            var rootOperationsNeedingAnalysis = new HashSet<IOperation>();
+
+                            operationBlockStartContext.RegisterOperationAction(
+                                operationAnalysisContext =>
+                                {
+                                    var propertyReferenceOperation = (IPropertyReferenceOperation)operationAnalysisContext.Operation;
+
+                                    if (sourceInfoSymbolMap.IsSourceProperty(propertyReferenceOperation.Property))
+                                    {
+                                        rootOperationsNeedingAnalysis.Add(operationAnalysisContext.Operation.GetRoot());
+                                    }
+                                },
+                                OperationKind.PropertyReference);
+
+                            operationBlockStartContext.RegisterOperationBlockEndAction(
+                                operationBlockAnalysisContext =>
+                                {
+                                    if (!rootOperationsNeedingAnalysis.Any())
+                                    {
+                                        return;
+                                    }
+
+                                    foreach (IOperation rootOperation in rootOperationsNeedingAnalysis)
+                                    {
+                                        var taintedDataAnalysisResult = TaintedDataAnalysis.GetOrComputeResult(
+                                            rootOperation.GetEnclosingControlFlowGraph(),
+                                            operationBlockAnalysisContext.Compilation,
+                                            operationBlockAnalysisContext.OwningSymbol,
+                                            operationBlockAnalysisContext.Options,
+                                            Rule,
+                                            sourceInfoSymbolMap,
+                                            taintedDataConfig.GetSanitizerSymbolMap(SinkKind),
+                                            sinkInfoSymbolMap,
+                                            operationBlockAnalysisContext.CancellationToken);
+
+                                        foreach (TaintedDataSourceSink sourceSink in taintedDataAnalysisResult.TaintedDataSourceSinks)
+                                        {
+                                            if (!sourceSink.SinkKinds.Contains(SinkKind))
+                                            {
+                                                continue;
+                                            }
+
+                                            foreach (SymbolAccess sourceOrigin in sourceSink.SourceOrigins)
+                                            {
+                                                Diagnostic diagnostic = Diagnostic.Create(
+                                                    Rule,
+                                                    sourceOrigin.Location,
+                                                    additionalLocations: new Location[] { sourceSink.Sink.Location },
+                                                    messageArgs: new object[] {
+                                                        sourceOrigin.Symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
+                                                        "",
+                                                        sourceSink.Sink.Symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
+                                                        ""});
+                                                operationBlockAnalysisContext.ReportDiagnostic(diagnostic);
+                                            }
+                                        }
+                                    }
+                                });
+                        });
+                });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotAddArchiveItemPathToTheTargetFileSystemPathTests.cs
@@ -0,0 +1,220 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    public class DoNotAddArchiveItemPathToTheTargetFileSystemPathTests : TaintedDataAnalyzerTestBase
+    {
+        protected override DiagnosticDescriptor Rule => DoNotAddArchiveItemPathToTheTargetFileSystemPath.Rule;
+
+        protected void VerifyCSharpWithDependencies(string source, params DiagnosticResult[] expected)
+        {
+            string zipArchiveEntryAndZipFileExtensionsCSharpSourceCode = @"
+namespace System.IO.Compression
+{
+    public class ZipArchiveEntry
+    {
+        public string FullName { get; }
+    }
+
+    public static class ZipFileExtensions
+    {
+        public static void ExtractToFile (this ZipArchiveEntry source, string destinationFileName)
+        {
+        }
+    }
+}";
+            this.VerifyCSharp(
+                new[] { source, zipArchiveEntryAndZipFileExtensionsCSharpSourceCode }.ToFileAndSource(),
+                expected);
+        }
+
+        [Fact]
+        public void Test_Sink_ZipArchiveEntry_ExtractToFile_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry)
+    {
+        zipArchiveEntry.ExtractToFile(zipArchiveEntry.FullName);
+    }
+}",
+            GetCSharpResultAt(8, 39, 8, 9, "string ZipArchiveEntry.FullName", "", "void ZipFileExtensions.ExtractToFile(ZipArchiveEntry source, string destinationFileName)", ""));
+        }
+
+        [Fact]
+        public void Test_Sink_File_Open_WithStringAndFileModeParameters_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry, FileMode mode)
+    {
+        File.Open(zipArchiveEntry.FullName, mode);
+    }
+}",
+            GetCSharpResultAt(9, 19, 9, 9, "string ZipArchiveEntry.FullName", "", "FileStream File.Open(string path, FileMode mode)", ""));
+        }
+
+        [Fact]
+        public void Test_Sink_File_Open_WithStringAndFileModeAndFileAccessParameters_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry, FileMode mode, FileAccess access)
+    {
+        File.Open(zipArchiveEntry.FullName, mode, access);
+    }
+}",
+            GetCSharpResultAt(9, 19, 9, 9, "string ZipArchiveEntry.FullName", "", "FileStream File.Open(string path, FileMode mode, FileAccess access)", ""));
+        }
+
+        [Fact]
+        public void Test_Sink_File_Open_WithStringAndFileModeAndFileAccessAndFileShareParamters_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry, FileMode mode, FileAccess access, FileShare share)
+    {
+        File.Open(zipArchiveEntry.FullName, mode, access, share);
+    }
+}",
+            GetCSharpResultAt(9, 19, 9, 9, "string ZipArchiveEntry.FullName", "", "FileStream File.Open(string path, FileMode mode, FileAccess access, FileShare share)", ""));
+        }
+
+        [Fact]
+        public void Test_Sink_FileStream_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry, FileMode mode)
+    {
+        var fileStream = new FileStream(zipArchiveEntry.FullName, mode);
+    }
+}",
+            GetCSharpResultAt(9, 41, 9, 26, "string ZipArchiveEntry.FullName", "", "FileStream.FileStream(string path, FileMode mode)", ""));
+        }
+
+        [Fact]
+        public void Test_Sink_FileInfo_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry)
+    {
+        var fileInfo = new FileInfo(zipArchiveEntry.FullName);
+    }
+}",
+            GetCSharpResultAt(9, 37, 9, 24, "string ZipArchiveEntry.FullName", "", "FileInfo.FileInfo(string fileName)", ""));
+        }
+
+        //Ideally, we wouldn't generate a diagnostic in this case.
+        [Fact]
+        public void Test_Sanitizer_String_StartsWith_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry)
+    {
+        var destinationFileName = zipArchiveEntry.FullName;
+
+        if(destinationFileName.StartsWith(""Start""))
+        {
+            zipArchiveEntry.ExtractToFile(destinationFileName);
+        }
+    }
+}",
+            GetCSharpResultAt(9, 35, 13, 13, "string ZipArchiveEntry.FullName", "", "void ZipFileExtensions.ExtractToFile(ZipArchiveEntry source, string destinationFileName)", ""));
+        }
+
+        [Fact]
+        public void Test_Sink_ZipArchiveEntry_ExtractToFile_NoDiagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry, string destinationFileName)
+    {
+        zipArchiveEntry.ExtractToFile(destinationFileName);
+    }
+}");
+        }
+
+        [Fact]
+        public void Test_Sanitizer_Path_GetFileName_NoDiagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System.IO;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry)
+    {
+        var destinationFileName = Path.GetFileName(zipArchiveEntry.FullName);
+        zipArchiveEntry.ExtractToFile(destinationFileName);
+    }
+}");
+        }
+
+        [Fact]
+        public void Test_Sanitizer_String_Substring_Diagnostic()
+        {
+            VerifyCSharpWithDependencies(@"
+using System;
+using System.IO.Compression;
+
+class TestClass
+{
+    public void TestMethod(ZipArchiveEntry zipArchiveEntry)
+    {
+        var destinationFileName = zipArchiveEntry.FullName;
+        zipArchiveEntry.ExtractToFile(destinationFileName.Substring(1));
+    }
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotAddArchiveItemPathToTheTargetFileSystemPath();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotAddArchiveItemPathToTheTargetFileSystemPath();
+        }
+    }
+}

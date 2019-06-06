f97b83ab8f9f9f31fa6b9a67ad7f8c6aa2d7653e
reflectronic <john-tur@outlook.com>
2019-06-06 16:27:34 -0400

    Port CA1002: Do not expose generic lists

---
+++ src/Microsoft.CodeQuality.Analyzers/Core/ApiDesignGuidelines/DoNotExposeGenericLists.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
+{
+    /// <summary>
+    /// CA1002: Do not expose generic lists
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotExposeGenericListsAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1002";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.DoNotExposeGenericListsTitle), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.DoNotExposeGenericListsMessage), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.DoNotExposeGenericListsDescription), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(RuleId,
+            s_localizableTitle,
+            s_localizableMessage,
+            DiagnosticCategory.Design,
+            DiagnosticHelpers.DefaultDiagnosticSeverity,
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            description: s_localizableDescription,
+            helpLinkUri: "https://docs.microsoft.com/en-us/visualstudio/code-quality/ca1002-do-not-expose-generic-lists",
+            customTags: FxCopWellKnownDiagnosticTags.PortedFxCopRule);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.EnableConcurrentExecution();
+            analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            analysisContext.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method, SymbolKind.Parameter, SymbolKind.Field, SymbolKind.Property);
+        }
+
+        private void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            if (!context.Symbol.IsExternallyVisible()) return;
+
+            // This would report twice for properties; once on
+            // the property type and once on the getter return,
+            // so we need to filter them out
+            if (context.Symbol.IsAccessorMethod()) return;
+
+            ITypeSymbol symbol;
+            switch (context.Symbol.Kind)
+            {
+                case SymbolKind.Method:
+                    symbol = ((IMethodSymbol) context.Symbol).ReturnType;
+                    break;
+                case SymbolKind.Parameter:
+                    symbol = ((IParameterSymbol) context.Symbol).Type;
+                    break;
+                case SymbolKind.Field:
+                    symbol = ((IFieldSymbol) context.Symbol).Type;
+                    break;
+                case SymbolKind.Property:
+                    symbol = ((IPropertySymbol) context.Symbol).Type;
+                    break;
+                default:
+                    return;
+            }
+
+            if (!(symbol is INamedTypeSymbol type)) return;
+
+            var listSymbol = context.Compilation.GetTypeByMetadataName("System.Collections.Generic.List`1");
+
+            if (type.ConstructedFrom.Equals(listSymbol))
+            {
+                context.ReportDiagnostic(context.Symbol.CreateDiagnostic(Rule));
+            }
+
+        }
+    }
+}
---
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/DoNotExposeGenericListsTests.cs
@@ -0,0 +1,209 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.DoNotExposeGenericListsAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.DoNotExposeGenericListsAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
+{
+    public class DoNotExposeGenericListsTests
+    {
+        [Fact]
+        public async Task PublicListProperty_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    public System.Collections.Generic.List<int> [|Ints|] { get; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Public Property [|Ints|] As System.Collections.Generic.List(Of Integer)
+                End Class");
+        }
+
+        [Fact]
+        public async Task PrivateListProperty_NoWarns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    private System.Collections.Generic.List<int> Ints { get; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Private Property Ints As System.Collections.Generic.List(Of Integer)
+                End Class");
+        }
+
+        [Fact]
+        public async Task ProtectedListProperty_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    protected System.Collections.Generic.List<int> [|Ints|] { get; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Protected Property [|Ints|] As System.Collections.Generic.List(Of Integer)
+                End Class");
+        }
+
+
+        [Fact]
+        public async Task PublicListField_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    public System.Collections.Generic.List<int> [|Ints|];
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Public [|Ints|] As System.Collections.Generic.List(Of Integer)
+                End Class");
+        }
+
+        [Fact]
+        public async Task PrivateListField_NoWarn()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    private System.Collections.Generic.List<int> Ints;
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Private Ints As System.Collections.Generic.List(Of Integer)
+                End Class");
+        }
+
+        [Fact]
+        public async Task ProtectedListField_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    protected System.Collections.Generic.List<int> [|Ints|];
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Protected [|Ints|] As System.Collections.Generic.List(Of Integer)
+                End Class");
+        }
+
+
+        [Fact]
+        public async Task PublicListReturnValue_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    public System.Collections.Generic.List<int> [|Ints|]() { throw null; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Public Function [|Ints|]() As System.Collections.Generic.List(Of Integer)
+                        Return Nothing
+                    End Function
+                End Class");
+        }
+
+        [Fact]
+        public async Task PrivateListReturnValue_NoWarn()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    private System.Collections.Generic.List<int> Ints() { throw null; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Private Function Ints() As System.Collections.Generic.List(Of Integer)
+                        Return Nothing
+                    End Function
+                End Class");
+        }
+
+        [Fact]
+        public async Task ProtectedListReturnValue_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    protected System.Collections.Generic.List<int> [|Ints|]() { throw null; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Protected Function [|Ints|]() As System.Collections.Generic.List(Of Integer)
+                        Return Nothing
+                    End Function
+                End Class");
+        }
+
+
+        [Fact]
+        public async Task PublicListParameter_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    public void Ints(System.Collections.Generic.List<int> [|ints|]) { throw null; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Public Sub Ints([|ints|] As System.Collections.Generic.List(Of Integer))
+                    End Sub
+                End Class");
+        }
+
+        [Fact]
+        public async Task PrivateListParameter_NoWarn()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    private void Ints(System.Collections.Generic.List<int> ints) { throw null; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Private Sub Ints(ints As System.Collections.Generic.List(Of Integer))
+                    End Sub
+                End Class");
+        }
+
+        [Fact]
+        public async Task ProtectedListParameter_Warns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+                public class Class
+                {
+                    protected void Ints(System.Collections.Generic.List<int> [|ints|]) { throw null; }
+                }");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+                Public Class [MyClass]
+                    Protected Sub Ints([|ints|] As System.Collections.Generic.List(Of Integer))
+                    End Sub
+                End Class");
+        }
+
+    }
+}

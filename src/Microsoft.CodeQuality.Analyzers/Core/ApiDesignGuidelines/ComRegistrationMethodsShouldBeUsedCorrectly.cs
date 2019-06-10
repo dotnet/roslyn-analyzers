b5bb84e0005a5ce94eb1ddaa37d9a995b81d00a6
reflectronic <john-tur@outlook.com>
2019-06-09 21:35:43 -0400

    Port CA1411: COM registration methods should not be visible

---
+++ src/Microsoft.CodeQuality.Analyzers/Core/ApiDesignGuidelines/ComRegistrationMethodsShouldBeUsedCorrectly.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class ComRegistrationMethodsShouldBeUsedCorrectlyAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string MatchedRuleId = "CA1410";
+        internal const string VisibleRuleId = "CA1411";
+
+        private static readonly LocalizableString s_matchedTitle = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.ComRegistrationMethodsShouldBeMatchedTitle), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_matchedMessage = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.ComRegistrationMethodsShouldBeMatchedMessage), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_matchedDescription = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.ComRegistrationMethodsShouldBeMatchedDescription), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_visibleTitle = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.ComRegistrationMethodsShouldNotBeVisibleTitle), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_visibleMessage = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.ComRegistrationMethodsShouldNotBeVisibleMessage), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+        private static readonly LocalizableString s_visibleDescription = new LocalizableResourceString(nameof(MicrosoftApiDesignGuidelinesAnalyzersResources.ComRegistrationMethodsShouldNotBeVisibleDescription), MicrosoftApiDesignGuidelinesAnalyzersResources.ResourceManager, typeof(MicrosoftApiDesignGuidelinesAnalyzersResources));
+
+
+        internal static DiagnosticDescriptor MatchedRule = new DiagnosticDescriptor(MatchedRuleId,
+            s_matchedTitle,
+            s_matchedMessage,
+            DiagnosticCategory.Interoperability,
+            DiagnosticHelpers.DefaultDiagnosticSeverity,
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            description: s_matchedDescription,
+            helpLinkUri: "https://docs.microsoft.com/en-us/visualstudio/code-quality/ca1410-com-registration-methods-should-be-matched",
+            customTags: FxCopWellKnownDiagnosticTags.PortedFxCopRule);
+
+        internal static DiagnosticDescriptor VisibleRule = new DiagnosticDescriptor(VisibleRuleId,
+            s_visibleTitle,
+            s_visibleMessage,
+            DiagnosticCategory.Interoperability,
+            DiagnosticHelpers.DefaultDiagnosticSeverity,
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            description: s_visibleDescription,
+            helpLinkUri: "https://docs.microsoft.com/en-us/visualstudio/code-quality/ca1411-com-registration-methods-should-not-be-visible",
+            customTags: FxCopWellKnownDiagnosticTags.PortedFxCopRule);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(MatchedRule, VisibleRule);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.EnableConcurrentExecution();
+            analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            analysisContext.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var type = (INamedTypeSymbol)context.Symbol;
+            if (!type.IsValidComExport()) return;
+
+            var comRegisterFunc = context.Compilation.GetTypeByMetadataName("System.Runtime.InteropServices.ComRegisterFunctionAttribute");
+            var comUnregisterFunc = context.Compilation.GetTypeByMetadataName("System.Runtime.InteropServices.ComUnregisterFunctionAttribute");
+
+            var registerMethod = type.GetMembers().OfType<IMethodSymbol>().FirstOrDefault(a => a.HasAttribute(comRegisterFunc));
+            var unregisterMethod = type.GetMembers().OfType<IMethodSymbol>().FirstOrDefault(a => a.HasAttribute(comUnregisterFunc));
+
+            // trueValue: true if left parameter is true, false if right parameter is true, null if neither parameter is true
+            static (bool result, bool? trueValue) XorWithResult(bool left, bool right)
+            {
+                var result = left ^ right;
+                if (left)
+                    return (result, true);
+                else if (right)
+                    return (result, false);
+                else
+                    return (result, null);
+            }
+
+            // If the type has either/or function, but not both
+            var (result, trueValue) = XorWithResult(!(registerMethod is null), !(unregisterMethod is null));
+
+            if (result)
+            {
+#pragma warning disable IDE0055 // Fix formatting
+                var diagnostic = trueValue switch
+                {
+                    // The left value was true (register method is present)
+                    true => type.CreateDiagnostic(MatchedRule, "ComRegisterFunction", "ComUnregisterFunction"),
+
+                    // The right value was true (unregister method is present)
+                    false => type.CreateDiagnostic(MatchedRule, "ComUnregisterFunction", "ComRegisterFunction")
+                };
+#pragma warning enable IDE0055 // Fix formatting
+
+                if (!(diagnostic is null)) context.ReportDiagnostic(diagnostic);
+            }
+
+            // Warn if registration methods are visible
+
+            if (registerMethod?.IsExternallyVisible() == true)
+            {
+                context.ReportDiagnostic(registerMethod.CreateDiagnostic(VisibleRule));
+            }
+
+            if (unregisterMethod?.IsExternallyVisible() == true)
+            {
+                context.ReportDiagnostic(unregisterMethod.CreateDiagnostic(VisibleRule));
+            }
+
+        }
+    }
+}
---
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/ApiDesignGuidelines/ComRegistrationMethodsShouldBeUsedCorrectlyTests.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Test.Utilities;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.ComRegistrationMethodsShouldBeUsedCorrectlyAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.ComRegistrationMethodsShouldBeUsedCorrectlyAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
+{
+    public class ComRegistrationMethodsShouldBeUsedCorrectlyTests
+    {
+        const string Matched = ComRegistrationMethodsShouldBeUsedCorrectlyAnalyzer.MatchedRuleId;
+        const string Visible = ComRegistrationMethodsShouldBeUsedCorrectlyAnalyzer.VisibleRuleId;
+
+        [Theory]
+        [AccessibilityTest(AccessibilityTestTarget.Class)]
+        public async Task RegisterMethodOnly_WarnsWhenExposed(AccessibilityContext ctx)
+        {
+            await VerifyCS.VerifyAnalyzerAsync($@"
+                using System.Runtime.InteropServices;
+
+                {ctx.AccessCS} class {ctx.Left(true, Matched)}Test{ctx.Right(true)}
+                {{
+                    [ComRegisterFunction]
+                    internal static void Register() {{ }}
+                }}");
+
+            await VerifyVB.VerifyAnalyzerAsync($@"
+                Imports System.Runtime.InteropServices
+
+                {ctx.AccessVB} Class {ctx.Left(true, Matched)}Test{ctx.Right(true)}
+                    <ComRegisterFunction>
+                    Friend Shared Sub Register
+                    End Sub
+                End Class");
+        }
+
+        [Theory]
+        [AccessibilityTest(AccessibilityTestTarget.InsideClass)]
+        public async Task RegistrationMethods_WarnWhenExposed(AccessibilityContext ctx)
+        {
+            await VerifyCS.VerifyAnalyzerAsync($@"
+                using System.Runtime.InteropServices;
+
+                public class Test
+                {{
+                    [ComRegisterFunction]
+                    {ctx.AccessCS} static void {ctx.Left(true, Visible)}Register{ctx.Right(true)}() {{ }}
+
+                    [ComUnregisterFunction]
+                    {ctx.AccessCS} static void {ctx.Left(true, Visible)}Unregister{ctx.Right(true)}() {{ }}
+                }}");
+
+            await VerifyVB.VerifyAnalyzerAsync($@"
+                Imports System.Runtime.InteropServices
+
+                Public Class Test
+                    <ComRegisterFunction>
+                    {ctx.AccessVB} Shared Sub {ctx.Left(true, Visible)}Register{ctx.Right(true)}
+                    End Sub
+
+                    <ComUnregisterFunction>
+                    {ctx.AccessVB} Shared Sub {ctx.Left(true, Visible)}Unregister{ctx.Right(true)}
+                    End Sub
+                End Class");
+        }
+
+
+        [Theory]
+        [AccessibilityTest(AccessibilityTestTarget.Class)]
+        public async Task UnregisterMethodOnly_WarnsWhenExposed(AccessibilityContext ctx)
+        {
+            await VerifyCS.VerifyAnalyzerAsync($@"
+                using System.Runtime.InteropServices;
+
+                {ctx.AccessCS} class {ctx.Left(true, Matched)}Test{ctx.Right(true)}
+                {{
+                    [ComUnregisterFunction]
+                    internal static void Unregister() {{ }}
+                }}");
+
+            await VerifyVB.VerifyAnalyzerAsync($@"
+                Imports System.Runtime.InteropServices
+
+                {ctx.AccessVB} Class {ctx.Left(true, Matched)}Test{ctx.Right(true)}
+                    <ComUnregisterFunction>
+                    Friend Shared Sub Unregister
+                    End Sub
+                End Class");
+        }
+
+        [Fact]
+        public async Task BothFunctions_NeverWarns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync($@"
+                using System.Runtime.InteropServices;
+
+                public class Test
+                {{
+                    [ComRegisterFunction]
+                    internal static void Register() {{ }}
+                    [ComUnregisterFunction]
+                    internal static void Unregister() {{ }}
+                }}");
+
+            await VerifyVB.VerifyAnalyzerAsync($@"
+                Imports System.Runtime.InteropServices
+
+                Class Test
+                    <ComRegisterFunction>
+                    Friend Shared Sub Register
+                    End Sub
+                    <ComUnregisterFunction>
+                    Friend Shared Sub Unregister
+                    End Sub
+                End Class");
+        }
+    }
+}

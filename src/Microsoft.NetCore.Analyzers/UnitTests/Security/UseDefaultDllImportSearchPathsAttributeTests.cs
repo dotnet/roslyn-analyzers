c9e610803a81947ebb57886ace5f9c4eeaec3f5d
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-07-15 10:21:05 +0800

    Add comment to test cases.

--- src/Microsoft.NetCore.Analyzers/UnitTests/Security/UseDefaultDllImportSearchPathsAttributeTests.cs
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/UseDefaultDllImportSearchPathsAttributeTests.cs
@@ -8,6 +8,13 @@ namespace Microsoft.NetCore.Analyzers.Security.UnitTests
 {
     public class UseDefaultDllImportSearchPathsAttributeTests : DiagnosticAnalyzerTestBase
     {
+        // It will try to retrieve the MessageBox from user32.dll, which will be searched in a default order:
+        //  1. The directory from which the application is loaded.
+        //  2. The current directory.
+        //  3. The system directory, usually C:\\Windows\\System32\\ (The GetSystemDirectory function is called to obtain this directory.).
+        //  4. The 16-bit system directory – There is no dedicated function to retrieve the path of this directory, but it is searched as well.
+        //  5. The Windows directory. The GetWindowsDirectory function is called to obtain this directory.
+        //  6. The directories that are listed in the PATH environment variable
         [Fact]
         public void Test_DllImportAttribute_Diagnostic()
         {
@@ -28,6 +35,8 @@ class TestClass
             GetCSharpResultAt(8, 30, UseDefaultDllImportSearchPathsAttribute.Rule, "MessageBox"));
         }
 
+        // [DllImport] is set with an absolute path, which will let the [DefaultDllImportSearchPaths] be ignored.
+        // So user32.dll will also be searched in the default order.
         [Fact]
         public void Test_DllImportAttributeWithAbsolutePath_Diagnostic()
         {
@@ -49,6 +58,7 @@ class TestClass
             GetCSharpResultAt(9, 30, UseDefaultDllImportSearchPathsAttribute.Rule, "MessageBox"));
         }
 
+        // It will have a compiler warning and recommend to use [DllImport]. So, there's no need to flag a diagnostic for this case.
         [Fact]
         public void Test_NoAttribute_NoDiagnostic()
         {
@@ -67,6 +77,7 @@ class TestClass
 }");
         }
 
+        // user32.dll will be searched in UserDirectories, which is specified by DllImportSearchPath and is good.
         [Fact]
         public void Test_DllImportAndDefaultDllImportSearchPathsAttributes_NoDiagnostic()
         {
@@ -87,6 +98,8 @@ class TestClass
 }");
         }
 
+        // In this case, [DefaultDllImportSearchPaths] is applied to the assembly.
+        // So, this attribute specifies the paths that are used by default to search for any DLL that provides a function for a platform invoke, in any code in the assembly.
         [Fact]
         public void Test_DllImportAndAssemblyDefaultDllImportSearchPathsAttributes_NoDiagnostic()
         {
@@ -108,6 +121,7 @@ class TestClass
 }");
         }
 
+        // It will have a compiler warning and recommend to use [DllImport] also.
         [Fact]
         public void Test_DefaultDllImportSearchPaths_NoDiagnostic()
         {
@@ -126,6 +140,7 @@ class TestClass
 }");
         }
 
+        // It will have a compiler warning and recommend to use [DllImport] also.
         [Fact]
         public void Test_AssemblyDefaultDllImportSearchPaths_NoDiagnostic()
         {

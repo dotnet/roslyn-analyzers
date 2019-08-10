1f5f2b4ccdde3e69cf591aa3e07ae03b41f512db
Paul Ming <paulming@microsoft.com>
2019-08-10 13:49:16 -0700

    Working on SslProtocols

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/SslProtocolsAnalyzer.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.NetCore.Analyzers.Security.Helpers;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class SslProtocolsAnalyzer : DiagnosticAnalyzer
+    {
+        internal static DiagnosticDescriptor DeprecatedRule = SecurityHelpers.CreateDiagnosticDescriptor(
+            "CA5395",
+            nameof(MicrosoftNetCoreAnalyzersResources.DeprecatedSslProtocolsTitle),
+            nameof(MicrosoftNetCoreAnalyzersResources.DeprecatedSslProtocolsMessage),
+            descriptionResourceStringName: nameof(MicrosoftNetCoreAnalyzersResources.DeprecatedSslProtocolsDescription),
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            helpLinkUri: "https://docs.microsoft.com/visualstudio/code-quality/ca5395",
+            customTags: WellKnownDiagnosticTags.Telemetry);
+        internal static DiagnosticDescriptor HardcodedRule = SecurityHelpers.CreateDiagnosticDescriptor(
+            "CA5396",
+            nameof(MicrosoftNetCoreAnalyzersResources.HardcodedSslProtocolsTitle),
+            nameof(MicrosoftNetCoreAnalyzersResources.HardcodedSslProtocolsMessage),
+            descriptionResourceStringName: nameof(MicrosoftNetCoreAnalyzersResources.HardcodedSslProtocolsDescription),
+            isEnabledByDefault: false,
+            helpLinkUri: "https://docs.microsoft.com/visualstudio/code-quality/ca5396",
+            customTags: WellKnownDiagnosticTags.Telemetry);
+
+        private readonly ImmutableHashSet<string> HardcodedSslProtocolsMetadataNames = ImmutableHashSet.Create(
+            StringComparer.Ordinal,
+            "Tls12",
+            "Tls13");
+
+        private const int UnsafeBits = 12 | 48 | 192 | 768;    // SslProtocols Ssl2 Ssl3 Tls10 Tls11
+
+        private const int HardcodedBits = 3072 | 12288;    // SslProtocols Tls12 Tls13
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(DeprecatedRule, HardcodedRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(
+                (CompilationStartAnalysisContext compilationStartAnalysisContext) =>
+                {
+                    WellKnownTypeProvider wellKnownTypeProvider = WellKnownTypeProvider.GetOrCreate(compilationStartAnalysisContext.Compilation);
+                    if (!wellKnownTypeProvider.TryGetTypeByMetadataName(
+                            WellKnownTypeNames.SystemSecurityAuthenticationSslProtocols,
+                            out INamedTypeSymbol sslProtocolsSymbol))
+                    {
+                        return;
+                    }
+
+                    compilationStartAnalysisContext.RegisterOperationAction(
+                        (OperationAnalysisContext operationAnalysisContext) =>
+                        {
+                            IFieldReferenceOperation fieldReferenceOperation = (IFieldReferenceOperation)operationAnalysisContext.Operation;
+                            if (IsReferencingSslProtocols(
+                                    fieldReferenceOperation,
+                                    out bool isDeprecatedProtocol,
+                                    out bool isHardCodedOkayProtocol))
+                            {
+                                if (isDeprecatedProtocol)
+                                {
+                                    operationAnalysisContext.ReportDiagnostic(fieldReferenceOperation.CreateDiagnostic(DeprecatedRule, fieldReferenceOperation.Field.Name));
+                                }
+                                else if (isHardCodedOkayProtocol)
+                                {
+                                    operationAnalysisContext.ReportDiagnostic(fieldReferenceOperation.CreateDiagnostic(HardcodedRule, fieldReferenceOperation.Field.Name));
+                                }
+                            }
+                        },
+                        OperationKind.FieldReference);
+
+                    compilationStartAnalysisContext.RegisterOperationAction(
+                        (OperationAnalysisContext operationAnalysisContext) =>
+                        {
+                            IAssignmentOperation assignmentOperation = (IAssignmentOperation)operationAnalysisContext.Operation;
+
+                            // Make sure this is an assignment operation for a SslProtocols value.
+                            if (!sslProtocolsSymbol.Equals(assignmentOperation.Target.Type))
+                            {
+                                return;
+                            }
+
+                            // Find the topmost operation with a bad bit set, unless we find an operation that would've been
+                            // flagged by the FieldReference callback above.
+                            IOperation foundDeprecatedOperation = null;
+                            bool foundDeprecatedReference = false;
+                            IOperation foundHardCodedOperation = null;
+                            bool foundHardCodedReference = false;
+                            foreach (IOperation childOperation in assignmentOperation.Value.DescendantsAndSelf())
+                            {
+                                if (childOperation is IFieldReferenceOperation fieldReferenceOperation
+                                    && IsReferencingSslProtocols(fieldReferenceOperation, out var isDeprecatedProtocol, out var isHardCodedOkayProtocol))
+                                {
+                                    if (isDeprecatedProtocol)
+                                    {
+                                        foundDeprecatedReference = true;
+                                    }
+                                    else if (isHardCodedOkayProtocol)
+                                    {
+                                        foundHardCodedReference = true;
+                                    }
+
+                                    if (foundDeprecatedReference && foundHardCodedReference)
+                                    {
+                                        return;
+                                    }
+                                }
+
+                                if (childOperation.ConstantValue.HasValue
+                                    && childOperation.ConstantValue.Value is int integerValue)
+                                {
+                                    if (foundDeprecatedOperation == null    // Only want the first.
+                                        && (integerValue & UnsafeBits) != 0)
+                                    {
+                                        foundDeprecatedOperation = childOperation;
+                                    }
+
+                                    if (foundHardCodedOperation == null    // Only want the first.
+                                        && (integerValue & HardcodedBits) != 0)
+                                    {
+                                        foundHardCodedOperation = childOperation;
+                                    }
+                                }
+                            }
+
+                            if (foundDeprecatedOperation != null && !foundDeprecatedReference)
+                            {
+                                operationAnalysisContext.ReportDiagnostic(foundDeprecatedOperation.CreateDiagnostic(DeprecatedRule, foundDeprecatedOperation.ConstantValue));
+                            }
+
+                            if (foundHardCodedOperation != null && !foundHardCodedReference)
+                            {
+                                operationAnalysisContext.ReportDiagnostic(foundHardCodedOperation.CreateDiagnostic(HardcodedRule, foundHardCodedOperation.ConstantValue));
+                            }
+                        },
+                        OperationKind.SimpleAssignment,
+                        OperationKind.CompoundAssignment);
+
+                    return;
+
+                    // Local function(s).
+                    bool IsReferencingSslProtocols(
+                        IFieldReferenceOperation fieldReferenceOperation,
+                        out bool isDeprecated,
+                        out bool isHardcoded)
+                    {
+                        if (sslProtocolsSymbol.Equals(fieldReferenceOperation.Field.ContainingType))
+                        {
+                            if (HardcodedSslProtocolsMetadataNames.Contains(fieldReferenceOperation.Field.Name))
+                            {
+                                isHardcoded = true;
+                                isDeprecated = false;
+                            }
+                            else
+                            {
+                                isDeprecated = true;
+                                isHardcoded = false;
+                            }
+
+                            return true;
+                        }
+                        else
+                        {
+                            isHardcoded = false;
+                            isDeprecated = false;
+                            return false;
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/SslProtocolsAnalyzerTests.cs
@@ -0,0 +1,478 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Test.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    public class SslProtocolsAnalyzerTests : DiagnosticAnalyzerTestBase
+    {
+        public SslProtocolsAnalyzerTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        public void DocSample1_CSharp_Violation()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+public class ExampleClass
+{
+    public void ExampleMethod()
+    {
+        // CA5395 violation for using Tls11
+        SslProtocols protocols = SslProtocols.Tls11 | SslProtocols.Tls12;
+    }
+}",
+            GetCSharpResultAt(10, 34, SslProtocolsAnalyzer.DeprecatedRule, "Tls11"),
+            GetCSharpResultAt(10, 55, SslProtocolsAnalyzer.HardcodedRule, "Tls12"));
+        }
+
+        [Fact]
+        public void DocSample1_VB_Violation()
+        {
+            VerifyBasic(@"
+Imports System
+Imports System.Security.Authentication
+
+Public Class TestClass
+    Public Sub ExampleMethod()
+        ' CA5395 violation for using Tls11
+        Dim sslProtocols As SslProtocols = SslProtocols.Tls11 Or SslProtocols.Tls12
+    End Sub
+End Class
+",
+            GetBasicResultAt(8, 44, SslProtocolsAnalyzer.DeprecatedRule, "Tls11"),
+            GetBasicResultAt(8, 66, SslProtocolsAnalyzer.HardcodedRule, "Tls12"));
+        }
+
+        [Fact]
+        public void DocSample2_CSharp_Violation()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+public class ExampleClass
+{
+    public SslProtocols ExampleMethod()
+    {
+        // CA5395 violation
+        return (SslProtocols) 768;    // TLS 1.1
+    }
+}",
+            GetCSharpResultAt(12, 29, SslProtocolsAnalyzer.DeprecatedRule, "768"));
+        }
+
+        [Fact]
+        public void DocSample2_VB_Violation()
+        {
+            VerifyBasic(@"
+Imports System
+Imports System.Security.Authentication
+
+Public Class TestClass
+    Public Sub ExampleMethod()
+        ' CA5395 violation
+        Dim sslProtocols As SslProtocols = CType(768, SecurityProtocolType)   ' TLS 1.1
+    End Sub
+End Class
+",
+            GetBasicResultAt(8, 48, SslProtocolsAnalyzer.DeprecatedRule, "768"));
+        }
+
+        [Fact]
+        public void DocSample1_CSharp_Solution()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+public class TestClass
+{
+    public void TestMethod()
+    {
+        // In .NET Framework 4.7.1 or later, let the operating system decide what TLS protocol version to use.
+        // See https://docs.microsoft.com/dotnet/framework/network-programming/tls
+    }
+}");
+        }
+
+        [Fact]
+        public void DocSample1_VB_Solution()
+        {
+            VerifyBasic(@"
+Imports System
+Imports System.Security.Authentication
+
+Public Class TestClass
+    Public Sub ExampleMethod()
+        ' In .NET Framework 4.7.1 or later, let the operating system decide what TLS protocol version to use.
+        ' See https://docs.microsoft.com/dotnet/framework/network-programming/tls
+    End Sub
+End Class
+");
+        }
+
+        [Fact]
+        public void DocSample3_CSharp_Violation()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+public class ExampleClass
+{
+    public void ExampleMethod()
+    {
+        // CA5396 violation
+        SslProtocols protocols = SecurityProtocolType.Tls12;
+    }
+}",
+            GetCSharpResultAt(10, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls12"));
+        }
+
+        [Fact]
+        public void DocSample3_VB_Violation()
+        {
+            VerifyBasic(@"
+Imports System
+Imports System.Security.Authentication
+
+Public Class TestClass
+    Public Sub ExampleMethod()
+        ' CA5396 violation
+        SslProtocols protocols = SslProtocols.Tls12
+    End Sub
+End Class
+",
+            GetBasicResultAt(8, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls12"));
+        }
+
+        [Fact]
+        public void DocSample4_CSharp_Violation()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+public class ExampleClass
+{
+    public void ExampleMethod()
+    {
+        // CA5396 violation
+        SslProtocols protocols = (SslProtocols) 3072;    // TLS 1.2
+    }
+}",
+            GetCSharpResultAt(10, 48, SslProtocolsAnalyzer.HardcodedRule, "3072"));
+        }
+
+        [Fact]
+        public void DocSample4_VB_Violation()
+        {
+            VerifyBasic(@"
+Imports System
+Imports System.Security.Authentication
+
+Public Class TestClass
+    Public Sub ExampleMethod()
+        ' CA5396 violation
+        SslProtocols protocols = CType(3072, SslProtocols)   ' TLS 1.2
+    End Sub
+End Class
+",
+            GetBasicResultAt(8, 48, SslProtocolsAnalyzer.HardcodedRule, "3072"));
+        }
+
+        [Fact]
+        public void TestUseSsl3Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        var a = SslProtocols.Ssl3;
+    }
+}",
+            GetCSharpResultAt(9, 17, SslProtocolsAnalyzer.DeprecatedRule, "Ssl3"));
+        }
+
+        [Fact]
+        public void TestUseTlsDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        var a = SslProtocols.Tls;
+    }
+}",
+            GetCSharpResultAt(9, 17, SslProtocolsAnalyzer.DeprecatedRule, "Tls"));
+        }
+
+        [Fact]
+        public void TestUseTls11Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = SslProtocols.Tls11;
+    }
+}",
+            GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.DeprecatedRule, "Tls11"));
+        }
+
+        [Fact]
+        public void TestUseSystemDefaultNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        var a = SslProtocols.Default;
+    }
+}",
+                GetCSharpResultAt(9, 30, SslProtocolsAnalyzer.DeprecatedRule, "Default"));
+        }
+
+        [Fact]
+        public void TestUseTls12Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = SslProtocols.Tls12;
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls12"));
+        }
+
+        [FactUnlessTls13Unavailable]
+        public void TestUseTls13Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = SslProtocols.Tls13;
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls13"));
+        }
+
+        [Fact]
+        public void TestUseTls12OrdTls11Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = SslProtocols.Tls12 | SslProtocols.Tls11;
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls12"),
+                GetCSharpResultAt(9, 77, SslProtocolsAnalyzer.DeprecatedRule, "Tls11"));
+        }
+
+        [Fact]
+        public void TestUse192CompoundAssignmentDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        ServicePointManager.SecurityProtocol |= (SslProtocols)192;
+    }
+}",
+                GetCSharpResultAt(9, 49, SslProtocolsAnalyzer.DeprecatedRule, "192"));
+        }
+
+        [Fact]
+        public void TestUse384SimpleAssignmentDiagnostic()
+        {
+            // 384 = SchProtocols.Tls11Server | SchProtocols.Tls10Client
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = (SslProtocols)384;
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.DeprecatedRule, "384"));
+        }
+
+        [Fact]
+        public void TestUse768SimpleAssignmentOrExpressionDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod(SslProtocols in)
+    {
+        SslProtocols protocols = in | (SslProtocols)768;
+    }
+}",
+                GetCSharpResultAt(9, 87, SslProtocolsAnalyzer.DeprecatedRule, "768"));
+        }
+
+        [Fact]
+        public void TestUse12288SimpleAssignmentOrExpressionDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod(SslProtocols in)
+    {
+        SslProtocols protocols = in | (SslProtocols)12288;
+    }
+}",
+                GetCSharpResultAt(9, 87, SslProtocolsAnalyzer.HardcodedRule, "12288"));
+        }
+
+        [Fact]
+        public void TestUseTls12OrTls11Or192Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = SslProtocols.Tls12 | SslProtocols.Tls11 | (SslProtocols)192;
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls12"),
+                GetCSharpResultAt(9, 77, SslProtocolsAnalyzer.DeprecatedRule, "Tls11"));
+        }
+
+        [Fact]
+        public void TestUseTls12Or192Diagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = SslProtocols.Tls12 | (SslProtocols)192;
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.HardcodedRule, "Tls12"),
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.DeprecatedRule, "3264"));
+        }
+
+        [Fact]
+        public void TestUse768DeconstructionAssignmentNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        int i;
+        (ServicePointManager.SecurityProtocol, i) = ((SslProtocols)384, 384);
+    }
+}");
+            // Ideally we'd handle the IDeconstructionAssignment, but this code pattern seems unlikely.
+        }
+
+        [Fact]
+        public void TestUse24Plus24SimpleAssignmentDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        SslProtocols protocols = (SslProtocols)(24 + 24);
+    }
+}",
+                GetCSharpResultAt(9, 48, SslProtocolsAnalyzer.DeprecatedRule, "48"));
+        }
+
+        [Fact]
+        public void TestUse768NotSslProtocolsNoDiagnostic()
+        {
+            VerifyCSharp(@"
+using System;
+using System.Security.Authentication;
+
+class TestClass
+{
+    public void TestMethod()
+    {
+        int i = 384 | 768;
+    }
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new SslProtocolsAnalyzer();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SslProtocolsAnalyzer();
+        }
+    }
+}

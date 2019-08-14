1ce0aa1bf98520a6e34092851603f4336f4ee23a
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-08-14 18:22:06 +0800

    The intial version.

---
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotDisableHttpClientCRLCheck.cs
@@ -0,0 +1,202 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Analyzer.Utilities.FlowAnalysis.Analysis.PropertySetAnalysis;
+using Analyzer.Utilities.PooledObjects;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow.PointsToAnalysis;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow.ValueContentAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.NetCore.Analyzers.Security.Helpers;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    class DoNotDisableHttpClientCRLCheck : DiagnosticAnalyzer
+    {
+        internal static DiagnosticDescriptor DefinitelyDisableHttpClientCRLCheckRule = SecurityHelpers.CreateDiagnosticDescriptor(
+            "CA5397",
+            typeof(MicrosoftNetCoreAnalyzersResources),
+            nameof(MicrosoftNetCoreAnalyzersResources.DefinitelyDisableHttpClientCRLCheck),
+            nameof(MicrosoftNetCoreAnalyzersResources.DefinitelyDisableHttpClientCRLCheckMessage),
+            DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            helpLinkUri: null,
+            descriptionResourceStringName: nameof(MicrosoftNetCoreAnalyzersResources.DoNotDisableHttpClientCRLCheckDescription),
+            customTags: WellKnownDiagnosticTagsExtensions.DataflowAndTelemetry);
+        internal static DiagnosticDescriptor MaybeDisableHttpClientCRLCheckRule = SecurityHelpers.CreateDiagnosticDescriptor(
+            "CA5398",
+            typeof(MicrosoftNetCoreAnalyzersResources),
+            nameof(MicrosoftNetCoreAnalyzersResources.MaybeDisableHttpClientCRLCheck),
+            nameof(MicrosoftNetCoreAnalyzersResources.MaybeDisableHttpClientCRLCheckMessage),
+            DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            helpLinkUri: null,
+            descriptionResourceStringName: nameof(MicrosoftNetCoreAnalyzersResources.DoNotDisableHttpClientCRLCheckDescription),
+            customTags: WellKnownDiagnosticTagsExtensions.DataflowAndTelemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(
+                DefinitelyDisableHttpClientCRLCheckRule,
+                MaybeDisableHttpClientCRLCheckRule);
+
+        /// <summary>
+        /// PropertySetAbstractValue index for CheckCertificateRevocationList property.
+        /// </summary>
+        private const int CheckCertificateRevocationListIndex = 0;
+
+        private static readonly ConstructorMapper ConstructorMapper = new ConstructorMapper(
+            (IMethodSymbol constructorMethod, IReadOnlyList<PointsToAbstractValue> argumentPointsToAbstractValues) =>
+            {
+                return PropertySetAbstractValue.GetInstance(PropertySetAbstractValueKind.Flagged);
+            });
+
+        private static readonly PropertyMapperCollection PropertyMappers = new PropertyMapperCollection(
+            new PropertyMapper(
+                "CheckCertificateRevocationList",
+                (ValueContentAbstractValue valueContentAbstractValue) =>
+                    PropertySetCallbacks.EvaluateLiteralValues(
+                        valueContentAbstractValue,
+                        (object o) => o is bool booleanValue && booleanValue == false),
+                CheckCertificateRevocationListIndex));
+
+        private static readonly HazardousUsageEvaluatorCollection HazardousUsageEvaluators = new HazardousUsageEvaluatorCollection(
+            new HazardousUsageEvaluator(
+                WellKnownTypeNames.SystemNetHttpHttpClient,
+                ".ctor",
+                "handler",
+                PropertySetCallbacks.HazardousIfAllFlaggedAndAtLeastOneKnown));
+
+        private static readonly ImmutableHashSet<string> typeToTrackMetadataNames = ImmutableHashSet.Create<string>(
+            WellKnownTypeNames.SystemNetHttpWinHttpHandler,
+            WellKnownTypeNames.SystemNetHttpHttpClientHandler,
+            WellKnownTypeNames.SystemNetHttpUnixCurlHandler);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(
+                (CompilationStartAnalysisContext compilationStartAnalysisContext) =>
+                {
+                    var wellKnownTypeProvider = WellKnownTypeProvider.GetOrCreate(compilationStartAnalysisContext.Compilation);
+
+                    if (!wellKnownTypeProvider.TryGetTypeByMetadataName(WellKnownTypeNames.SystemNetHttpHttpClient, out INamedTypeSymbol httpClientTypeSymbol))
+                    {
+                        return;
+                    }
+
+                    if (typeToTrackMetadataNames.Any(s => !wellKnownTypeProvider.TryGetTypeByMetadataName(s, out _)))
+                    {
+                        return;
+                    }
+
+                    var rootOperationsNeedingAnalysis = PooledHashSet<(IOperation, ISymbol)>.GetInstance();
+
+                    compilationStartAnalysisContext.RegisterOperationBlockStartAction(
+                        (OperationBlockStartAnalysisContext operationBlockStartAnalysisContext) =>
+                        {
+                            operationBlockStartAnalysisContext.RegisterOperationAction(
+                                (OperationAnalysisContext operationAnalysisContext) =>
+                                {
+                                    var objectCreationOperation =
+                                        (IObjectCreationOperation)operationAnalysisContext.Operation;
+
+                                    if (httpClientTypeSymbol.Equals(objectCreationOperation.Type))
+                                    {
+                                        if (objectCreationOperation.Arguments.Length == 0)
+                                        {
+                                            operationAnalysisContext.ReportDiagnostic(
+                                                objectCreationOperation.CreateDiagnostic(
+                                                    DefinitelyDisableHttpClientCRLCheckRule));
+                                        }
+
+                                        lock (rootOperationsNeedingAnalysis)
+                                        {
+                                            rootOperationsNeedingAnalysis.Add(
+                                                (objectCreationOperation.GetRoot(), operationAnalysisContext.ContainingSymbol));
+                                        }
+                                    }
+                                },
+                                OperationKind.ObjectCreation);
+                        });
+
+                    compilationStartAnalysisContext.RegisterCompilationEndAction(
+                        (CompilationAnalysisContext compilationAnalysisContext) =>
+                        {
+                            PooledDictionary<(Location Location, IMethodSymbol Method), HazardousUsageEvaluationResult> allResults = null;
+
+                            try
+                            {
+                                lock (rootOperationsNeedingAnalysis)
+                                {
+                                    if (!rootOperationsNeedingAnalysis.Any())
+                                    {
+                                        return;
+                                    }
+
+                                    allResults = PropertySetAnalysis.BatchGetOrComputeHazardousUsages(
+                                        compilationAnalysisContext.Compilation,
+                                        rootOperationsNeedingAnalysis,
+                                        compilationAnalysisContext.Options,
+                                        typeToTrackMetadataNames,
+                                        ConstructorMapper,
+                                        PropertyMappers,
+                                        HazardousUsageEvaluators,
+                                        InterproceduralAnalysisConfiguration.Create(
+                                            compilationAnalysisContext.Options,
+                                            SupportedDiagnostics,
+                                            defaultInterproceduralAnalysisKind: InterproceduralAnalysisKind.ContextSensitive,
+                                            cancellationToken: compilationAnalysisContext.CancellationToken));
+                                }
+
+                                if (allResults == null)
+                                {
+                                    return;
+                                }
+
+                                foreach (KeyValuePair<(Location Location, IMethodSymbol Method), HazardousUsageEvaluationResult> kvp
+                                    in allResults)
+                                {
+                                    DiagnosticDescriptor descriptor;
+                                    switch (kvp.Value)
+                                    {
+                                        case HazardousUsageEvaluationResult.Flagged:
+                                            descriptor = DefinitelyDisableHttpClientCRLCheckRule;
+                                            break;
+
+                                        case HazardousUsageEvaluationResult.MaybeFlagged:
+                                            descriptor = MaybeDisableHttpClientCRLCheckRule;
+                                            break;
+
+                                        default:
+                                            Debug.Fail($"Unhandled result value {kvp.Value}");
+                                            continue;
+                                    }
+
+                                    compilationAnalysisContext.ReportDiagnostic(
+                                        Diagnostic.Create(
+                                            descriptor,
+                                            kvp.Key.Location,
+                                            kvp.Key.Method.ToDisplayString(
+                                                SymbolDisplayFormat.MinimallyQualifiedFormat)));
+                                }
+                            }
+                            finally
+                            {
+                                rootOperationsNeedingAnalysis.Free();
+                                allResults?.Free();
+                            }
+                        });
+                });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Security/DoNotDisableHttpClientCRLCheckTests.cs
@@ -0,0 +1,272 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Test.Utilities;
+using Test.Utilities.MinimalImplementations;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Security.UnitTests
+{
+    [Trait(Traits.DataflowAnalysis, Traits.Dataflow.PropertySetAnalysis)]
+    public class DoNotDisableHttpClientCRLCheckTests : DiagnosticAnalyzerTestBase
+    {
+        private static readonly DiagnosticDescriptor DefinitelyRule = DoNotDisableHttpClientCRLCheck.DefinitelyDisableHttpClientCRLCheckRule;
+        private static readonly DiagnosticDescriptor MaybeRule = DoNotDisableHttpClientCRLCheck.MaybeDisableHttpClientCRLCheckRule;
+
+        protected void VerifyCSharpWithDependencies(string source, params DiagnosticResult[] expected)
+        {
+            this.VerifyCSharp(
+                new[] { source, SystemNetHttpApis.CSharp }.ToFileAndSource(),
+                expected);
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_NotSet_DefaultWrong_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var winHttpHandler = new WinHttpHandler();
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}",
+                GetCSharpResultAt(9, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_Wrong_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = false;
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}",
+                GetCSharpResultAt(10, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_HttpClientHandler_CheckCertificateRevocationList_Wrong_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var httpClientHandler = new HttpClientHandler();
+        httpClientHandler.CheckCertificateRevocationList = false;
+        var httpClient = new HttpClient(httpClientHandler);
+    }
+}",
+                GetCSharpResultAt(10, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_CurlHandler_CheckCertificateRevocationList_Wrong_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+using System.Net.Http.Unix;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var curlHandler = new CurlHandler();
+        curlHandler.CheckCertificateRevocationList = false;
+        var httpClient = new HttpClient(curlHandler);
+    }
+}",
+                GetCSharpResultAt(11, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_HttpClientWithHttpMessageHandlerAndBooleanParameters_WinHttpHandler_CheckCertificateRevocationList_Wrong_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod(bool disposeHandler)
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = false;
+        var httpClient = new HttpClient(winHttpHandler, disposeHandler);
+    }
+}",
+                GetCSharpResultAt(10, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_PropertyInitializer_CheckCertificateRevocationList_Wrong_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var winHttpHandler = new WinHttpHandler() { CheckCertificateRevocationList = false };
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}",
+                GetCSharpResultAt(9, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_HttpClientConstructorWithoutParameter_handlerSetByDefault_DefinitelyDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = false;
+        var httpClient = new HttpClient();
+    }
+}",
+                GetCSharpResultAt(10, 26, DefinitelyRule));
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_UnknownOrRight_MaybeDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System;
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod(bool checkCertificateRevocationList)
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = checkCertificateRevocationList;
+        Random r = new Random();
+
+        if (r.Next(6) == 4)
+        {
+            winHttpHandler.CheckCertificateRevocationList = true;
+        }
+
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}",
+                GetCSharpResultAt(18, 26, MaybeRule));
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_UnknownOrWrong_MaybeDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System;
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod(bool checkCertificateRevocationList)
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = checkCertificateRevocationList;
+        Random r = new Random();
+
+        if (r.Next(6) == 4)
+        {
+            winHttpHandler.CheckCertificateRevocationList = false;
+        }
+
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}",
+                GetCSharpResultAt(18, 26, MaybeRule));
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_WrongOrRight_MaybeDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System;
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = false;
+        Random r = new Random();
+
+        if (r.Next(6) == 4)
+        {
+            winHttpHandler.CheckCertificateRevocationList = true;
+        }
+
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}",
+                GetCSharpResultAt(18, 26, MaybeRule));
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_Right_NoDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod()
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = true;
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}");
+        }
+
+        [Fact]
+        public void Test_WinHttpHandler_CheckCertificateRevocationList_Unknown_NoDiagnostic()
+        {
+            this.VerifyCSharpWithDependencies(@"
+using System.Net.Http;
+
+class TestClass
+{
+    void TestMethod(bool checkCertificateRevocationList)
+    {
+        var winHttpHandler = new WinHttpHandler();
+        winHttpHandler.CheckCertificateRevocationList = checkCertificateRevocationList;
+        var httpClient = new HttpClient(winHttpHandler);
+    }
+}");
+        }
+
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotDisableHttpClientCRLCheck();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotDisableHttpClientCRLCheck();
+        }
+    }
+}

892abe0fdae2762ac13d0f482f551f26319e169d
Andrew Hall (METAL) <andrha@microsoft.com>
2019-09-25 15:10:42 -0700

    WIP

---
+++ src/Microsoft.CodeAnalysis.Analyzers/Core/MetaAnalyzers/UseSymbolComparerInCollectionsAnalyzer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.CodeAnalysis.Analyzers.MetaAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class UseSymbolComparerInCollectionsAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(CodeAnalysisDiagnosticsResources.CompareSymbolsCorrectlyTitle), CodeAnalysisDiagnosticsResources.ResourceManager, typeof(CodeAnalysisDiagnosticsResources));
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(CodeAnalysisDiagnosticsResources.CompareSymbolsCorrectlyMessage), CodeAnalysisDiagnosticsResources.ResourceManager, typeof(CodeAnalysisDiagnosticsResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(CodeAnalysisDiagnosticsResources.CompareSymbolsCorrectlyDescription), CodeAnalysisDiagnosticsResources.ResourceManager, typeof(CodeAnalysisDiagnosticsResources));
+
+        private static readonly string s_symbolTypeFullName = typeof(ISymbol).FullName;
+
+        public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticIds.CompareSymbolsCorrectlyRuleId,
+            s_localizableTitle,
+            s_localizableMessage,
+            DiagnosticCategory.MicrosoftCodeAnalysisCorrectness,
+            DiagnosticHelpers.DefaultDiagnosticSeverity,
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            description: s_localizableDescription,
+            customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(context =>
+            {
+                var compilation = context.Compilation;
+                var symbolType = compilation.GetTypeByMetadataName(s_symbolTypeFullName);
+                if (symbolType is null)
+                {
+                    return;
+                }
+
+                context.RegisterOperationAction((context) =>
+                {
+                    switch (context.Operation)
+                    {
+                        case IMethodReferenceOperation _: OnMethodReferenceOperation(context); break;
+                        case IObjectCreationOperation _: OnObjectCreationOperation(context); break;
+                    }
+                }, OperationKind.ObjectCreation, OperationKind.MethodReference)
+            });
+        }
+
+        private void OnObjectCreationOperation(OperationAnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMethodReferenceOperation(OperationAnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

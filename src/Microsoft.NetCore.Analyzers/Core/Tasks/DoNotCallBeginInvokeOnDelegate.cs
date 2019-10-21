5af4a2533784f0ef838713b58be2319bd00bfb56
Rastislav Novotný <rasto@duracellko.net>
2019-10-21 23:07:06 +0800

    Add rule CA1069:DoNotCallBeginInvokeOnDelegate

---
+++ src/Microsoft.NetCore.Analyzers/Core/Tasks/DoNotCallBeginInvokeOnDelegate.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Tasks
+{
+    /// <summary>
+    /// CA1069: Do not call BeginInvoke on a delegate in .NET Core and .NET Standard.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotCallBeginInvokeOnDelegate : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1069";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.DoNotCallBeginInvokeOnDelegateTitle), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.DoNotCallBeginInvokeOnDelegateMessage), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.DoNotCallBeginInvokeOnDelegateDescription), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(RuleId,
+                                                                             s_localizableTitle,
+                                                                             s_localizableMessage,
+                                                                             DiagnosticCategory.Design,
+                                                                             DiagnosticHelpers.DefaultDiagnosticSeverity,
+                                                                             isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                                                                             description: s_localizableDescription,
+                                                                             helpLinkUri: null,
+                                                                             customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public sealed override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
+
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                compilationContext.RegisterOperationAction(operationContext =>
+                {
+                    var invocation = (IInvocationOperation)operationContext.Operation;
+                    var containingType = invocation.TargetMethod?.ContainingType;
+
+                    if (containingType != null &&
+                        containingType.TypeKind == TypeKind.Delegate &&
+                        invocation.TargetMethod.Name == "BeginInvoke")
+                    {
+                        operationContext.ReportDiagnostic(Diagnostic.Create(Rule, invocation.Syntax.GetLocation()));
+                    }
+                }, OperationKind.Invocation);
+            });
+        }
+    }
+}
---
+++ src/Microsoft.NetCore.Analyzers/UnitTests/Tasks/DoNotCallBeginInvokeOnDelegateTests.cs
@@ -0,0 +1,217 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.NetCore.Analyzers.Tasks.UnitTests
+{
+    public class DoNotCallBeginInvokeOnDelegateTests : DiagnosticAnalyzerTestBase
+    {
+        protected override DiagnosticAnalyzer GetBasicDiagnosticAnalyzer()
+        {
+            return new DoNotCallBeginInvokeOnDelegate();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotCallBeginInvokeOnDelegate();
+        }
+
+        #region CSharp Tests
+
+        [Fact]
+        public void BeginInvokeOnAction()
+        {
+            var code = @"
+using System;
+
+class C
+{
+    public void M()
+    {
+        var action = new Action(D);
+        action.BeginInvoke(Callback, null);
+    }
+
+    private void D()
+    {
+        Console.WriteLine(""Test"");
+    }
+
+    private void Callback(IAsyncResult ar)
+    {
+        Console.WriteLine(ar.ToString());
+    }
+}
+";
+            VerifyCSharp(code, GetCSharpResultAt(9, 9));
+        }
+
+        [Fact]
+        public void BeginInvokeOnFunc()
+        {
+            var code = @"
+using System;
+
+class C
+{
+    private Func<object> func;
+
+    public void M()
+    {
+        this.func.BeginInvoke(Callback, ""test"");
+    }
+
+    private void Callback(IAsyncResult ar)
+    {
+        Console.WriteLine(ar.ToString());
+    }
+}
+";
+            VerifyCSharp(code, GetCSharpResultAt(10, 9));
+        }
+
+        [Fact]
+        public void BeginInvokeOnActionWith2Parameters()
+        {
+            var code = @"
+using System;
+
+class C
+{
+    public void M()
+    {
+        var action = new Action<string, int>(D);
+        action.BeginInvoke(""Value: {0}"", 10, Callback, this);
+    }
+
+    private void D(string format, int value)
+    {
+        Console.WriteLine(format, value);
+    }
+
+    private void Callback(IAsyncResult ar)
+    {
+        Console.WriteLine(ar.ToString());
+    }
+}
+";
+            VerifyCSharp(code, GetCSharpResultAt(9, 9));
+        }
+
+        [Fact]
+        public void BeginInvokeOnFuncWith2Parameters()
+        {
+            var code = @"
+using System;
+
+class C
+{
+    private IAsyncResult asyncResult;
+
+    public void M()
+    {
+        var func = new Func<string, int, string>(D);
+        asyncResult = func.BeginInvoke(""Value: {0}"", 10, Callback, null);
+    }
+
+    private string D(string format, int value)
+    {
+        return string.Format(format, value);
+    }
+
+    private void Callback(IAsyncResult ar)
+    {
+        Console.WriteLine(ar.ToString());
+    }
+}
+";
+            VerifyCSharp(code, GetCSharpResultAt(11, 23));
+        }
+
+        [Fact]
+        public void BeginInvokeOnCustomDelegate()
+        {
+            var code = @"
+using System;
+
+public delegate void D(object sender, EventArgs e);
+
+class C
+{
+    private IAsyncResult asyncResult;
+
+    public void M()
+    {
+        var d = new D(H1);
+        d += new D(H2);
+        asyncResult = d.BeginInvoke(this, EventArgs.Empty, Callback, 10);
+    }
+
+    private void H1(object sender, EventArgs e)
+    {
+        Console.WriteLine(e);
+    }
+
+    private static void H2(object sender, EventArgs e)
+    {
+        Console.WriteLine(e);
+    }
+
+    private void Callback(IAsyncResult ar)
+    {
+        Console.WriteLine(ar.ToString());
+    }
+}
+";
+            VerifyCSharp(code, GetCSharpResultAt(14, 23));
+        }
+
+        [Fact]
+        public void BeginInvokeOnEvent()
+        {
+            var code = @"
+using System;
+
+class C
+{
+    public event EventHandler E;
+
+    public void M()
+    {
+        E.BeginInvoke(this, EventArgs.Empty, Callback, null);
+    }
+
+    private void Callback(IAsyncResult ar)
+    {
+        Console.WriteLine(ar.ToString());
+    }
+}
+";
+            VerifyCSharp(code, GetCSharpResultAt(10, 9));
+        }
+        #endregion
+
+        private object D(string format, int value)
+        {
+            return string.Format(format, value);
+        }
+
+        private static DiagnosticResult GetCSharpResultAt(int line, int column)
+        {
+            return GetCSharpResultAt(line, column, DoNotCallBeginInvokeOnDelegate.RuleId, MicrosoftNetCoreAnalyzersResources.DoNotCallBeginInvokeOnDelegateMessage);
+        }
+
+        private static DiagnosticResult GetBasicResultAt(int line, int column)
+        {
+            return GetBasicResultAt(line, column, DoNotCallBeginInvokeOnDelegate.RuleId, MicrosoftNetCoreAnalyzersResources.DoNotCallBeginInvokeOnDelegateMessage);
+        }
+    }
+}

8a7e678b9ec8979571ceabdfb5a9b89482587cc3
Evangelink <evangelink@gmail.com>
2019-11-19 18:06:05 +0100

    Rule CA2008: Avoid infinite recursion

---
+++ src/Microsoft.CodeQuality.Analyzers/Core/QualityGuidelines/AvoidInfiniteRecursion.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.CodeQuality.Analyzers.QualityGuidelines
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class AvoidInfiniteRecursion : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA2008";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(RuleId,
+            new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidInfiniteRecursionTitle), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources)),
+            new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidInfiniteRecursionMessage), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources)),
+            DiagnosticCategory.Reliability,
+            DiagnosticHelpers.DefaultDiagnosticSeverity,
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            helpLinkUri: null);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.EnableConcurrentExecution();
+            analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            analysisContext.RegisterOperationBlockStartAction(operationBlockStartContext =>
+            {
+                if (operationBlockStartContext.OwningSymbol is IMethodSymbol methodSymbol &&
+                    methodSymbol.MethodKind == MethodKind.PropertySet)
+                {
+                    operationBlockStartContext.RegisterOperationAction(operationContext =>
+                    {
+                        var assignmentOperation = (IAssignmentOperation)operationContext.Operation;
+
+                        if (assignmentOperation.Target is IPropertyReferenceOperation operationTarget &&
+                            operationTarget.Member.Equals(methodSymbol.AssociatedSymbol))
+                        {
+                            operationContext.ReportDiagnostic(Diagnostic.Create(Rule, assignmentOperation.Syntax.GetLocation(), operationTarget.Property.Name));
+                        }
+                    }, OperationKind.SimpleAssignment);
+                }
+            });
+        }
+    }
+}
---
+++ src/Microsoft.CodeQuality.Analyzers/UnitTests/QualityGuidelines/AvoidInfiniteRecursionTests.cs
@@ -0,0 +1,153 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.QualityGuidelines.AvoidInfiniteRecursion,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.QualityGuidelines.AvoidInfiniteRecursion,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeQuality.Analyzers.UnitTests.QualityGuidelines
+{
+    public class AvoidInfiniteRecursionTests
+    {
+        [Fact]
+        public async Task PropertySetterRecursion_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class A
+{
+    public int Abc
+    {
+        set
+        {
+            this.Abc = value;
+        }
+    }
+}",
+                GetCSharpResultAt(8, 13, "Abc"));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Public Class A
+    Public WriteOnly Property Abc As Integer
+        Set(ByVal value As Integer)
+            Me.Abc = value
+        End Set
+    End Property
+End Class
+",
+                GetBasicResultAt(5, 13, "Abc"));
+        }
+
+        [Fact]
+        public async Task PropertySetterMultipleRecursion_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class A
+{
+    public int Abc
+    {
+        set
+        {
+            this.Abc = value;
+            if (value > 42)
+            {
+                Abc = value;
+            }
+        }
+    }
+}",
+                GetCSharpResultAt(8, 13, "Abc"),
+                GetCSharpResultAt(11, 17, "Abc"));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Public Class A
+    Public WriteOnly Property Abc As Integer
+        Set(ByVal value As Integer)
+            Me.Abc = value
+
+            If value > 42 Then
+                Abc = value
+            End If
+        End Set
+    End Property
+End Class
+",
+                GetBasicResultAt(5, 13, "Abc"),
+                GetBasicResultAt(8, 17, "Abc"));
+        }
+
+        [Fact]
+        public async Task PropertySetterNoRecursionWithField_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class A
+{
+    private int abc;
+
+    public int Abc
+    {
+        set
+        {
+            this.abc = value;
+        }
+    }
+}");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Public Class A
+    Private abc As Integer
+
+    Public WriteOnly Property Abc As Integer
+        Set(ByVal value As Integer)
+            Me.abc = value
+        End Set
+    End Property
+End Class
+");
+        }
+
+        [Fact]
+        public async Task PropertySetterNoRecursionWithOtherProperty_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class A
+{
+    public int Foo { get; set; }
+
+    public int Abc
+    {
+        set
+        {
+            this.Foo = value;
+        }
+    }
+}");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Public Class A
+    Public Property Foo As Integer
+
+    Public WriteOnly Property Abc As Integer
+        Set(ByVal value As Integer)
+            Me.Foo = value
+        End Set
+    End Property
+End Class
+");
+        }
+
+        private DiagnosticResult GetCSharpResultAt(int line, int column, string symbolName)
+                => VerifyCS.Diagnostic()
+                    .WithLocation(line, column)
+                    .WithArguments(symbolName);
+
+        private DiagnosticResult GetBasicResultAt(int line, int column, string symbolName)
+            => VerifyVB.Diagnostic()
+                .WithLocation(line, column)
+                .WithArguments(symbolName);
+    }
+}

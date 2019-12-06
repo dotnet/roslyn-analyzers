92e5e612526974cc8b55788f521400adc0e08e21
Amaury Levé <evangelink@gmail.com>
2019-12-06 16:55:10 +0100

    Rule CA1069: Enums values should not be duplicated

---
+++ src/Microsoft.CodeQuality.Analyzers/Core/ApiDesignGuidelines/EnumShouldNotHaveDuplicatedValues.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
+{
+    public abstract class EnumShouldNotHaveDuplicatedValues : DiagnosticAnalyzer
+    {
+        public const string RuleId = "CA1069";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.EnumShouldNotHaveDuplicatedValuesTitle), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+
+        private static readonly LocalizableString s_localizableMessageRuleDuplicatedValue = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.EnumShouldNotHaveDuplicatedValuesMessageDuplicatedValue), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+        internal static DiagnosticDescriptor RuleDuplicatedValue = new DiagnosticDescriptor(RuleId,
+                                                                                            s_localizableTitle,
+                                                                                            s_localizableMessageRuleDuplicatedValue,
+                                                                                            DiagnosticCategory.Design,
+                                                                                            DiagnosticHelpers.DefaultDiagnosticSeverity,
+                                                                                            isEnabledByDefault: false,
+                                                                                            helpLinkUri: null,
+                                                                                            customTags: WellKnownDiagnosticTags.Telemetry);
+
+        private static readonly LocalizableString s_localizableMessageRuleDuplicatedBitwiseValuePart = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.EnumShouldNotHaveDuplicatedValuesMessageDuplicatedBitwiseValuePart), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+        internal static DiagnosticDescriptor RuleDuplicatedBitwiseValuePart = new DiagnosticDescriptor(RuleId,
+                                                                                                   s_localizableTitle,
+                                                                                                   s_localizableMessageRuleDuplicatedBitwiseValuePart,
+                                                                                                   DiagnosticCategory.Design,
+                                                                                                   DiagnosticHelpers.DefaultDiagnosticSeverity,
+                                                                                                   isEnabledByDefault: false,
+                                                                                                   helpLinkUri: null,
+                                                                                                   customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public sealed override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleDuplicatedValue, RuleDuplicatedBitwiseValuePart);
+
+        public sealed override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSymbolAction(symbolContext =>
+            {
+                var typeSymbol = (INamedTypeSymbol)symbolContext.Symbol;
+                if (typeSymbol.TypeKind != TypeKind.Enum)
+                {
+                    return;
+                }
+
+                var enumMemberValues = typeSymbol.GetMembers()
+                    .Where(m => m.Kind == SymbolKind.Field)
+                    .Cast<IFieldSymbol>()
+                    .Where(m => m.HasConstantValue && m.DeclaringSyntaxReferences.Length == 1)
+                    .Select(m => (Value: m.ConstantValue, DeclaringSyntax: m.DeclaringSyntaxReferences[0].GetSyntax(symbolContext.CancellationToken)))
+                    .GroupBy(ev => ev.Value, ev => ev.DeclaringSyntax)
+                    .ToDictionary(ev => ev.Key, ev => ev.ToList());
+
+                AnalyzeEnumMemberValues(enumMemberValues, symbolContext);
+            }, SymbolKind.NamedType);
+        }
+
+        protected abstract void AnalyzeEnumMemberValues(Dictionary<object, List<SyntaxNode>> membersByValue, SymbolAnalysisContext context);
+    }
+}
---
+++ src/Microsoft.CodeQuality.Analyzers/VisualBasic/ApiDesignGuidelines/BasicEnumShouldNotHaveDuplicatedValues.vb
@@ -0,0 +1,63 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Analyzer.Utilities.Extensions
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+Imports Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
+
+Namespace Microsoft.CodeQuality.VisualBasic.Analyzers.ApiDesignGuidelines
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Public Class BasicEnumShouldNotHaveDuplicatedValues
+        Inherits EnumShouldNotHaveDuplicatedValues
+
+        Protected Overrides Sub AnalyzeEnumMemberValues(membersByValue As Dictionary(Of Object, List(Of SyntaxNode)), context As SymbolAnalysisContext)
+            For Each kvp As KeyValuePair(Of Object, List(Of SyntaxNode)) In membersByValue
+                For index = 0 To kvp.Value.Count - 1
+                    Dim enumMember = CType(kvp.Value(index), EnumMemberDeclarationSyntax)
+
+
+                    If enumMember.Initializer Is Nothing Then
+                        ReportIfNotFirstItem(enumMember, index, context)
+                    Else
+                        If TypeOf enumMember.Initializer.Value Is IdentifierNameSyntax Then
+                            ' It is allowed to reference another enum value through its identifier
+                        ElseIf TypeOf enumMember.Initializer.Value Is BinaryExpressionSyntax Then
+                            AnalyzeBinaryExpression(CType(enumMember.Initializer.Value, BinaryExpressionSyntax), context, index > 0)
+                        Else
+                            ReportIfNotFirstItem(enumMember, index, context)
+                        End If
+                    End If
+                Next
+            Next
+        End Sub
+
+        Private Shared Sub ReportIfNotFirstItem(enumMember As EnumMemberDeclarationSyntax, index As Integer, context As SymbolAnalysisContext)
+            If index > 0 Then
+                context.ReportDiagnostic(enumMember.CreateDiagnostic(RuleDuplicatedValue))
+            End If
+        End Sub
+
+        Private Shared Sub AnalyzeBinaryExpression(binaryExpression As BinaryExpressionSyntax, context As SymbolAnalysisContext, isDuplicatedValue As Boolean)
+            Dim hasBitwiseIssue = False
+            Dim seen = New HashSet(Of Object)
+
+            For Each identifier As IdentifierNameSyntax In binaryExpression.DescendantNodes().OfType(Of IdentifierNameSyntax)
+                If identifier.Identifier.Value IsNot Nothing Then
+                    If seen.Contains(identifier.Identifier.Value) Then
+                        hasBitwiseIssue = True
+                        context.ReportDiagnostic(identifier.CreateDiagnostic(RuleDuplicatedBitwiseValuePart))
+                    Else
+                        seen.Add(identifier.Identifier.Value)
+                    End If
+                End If
+            Next
+
+            ' This enum value doesn't have any duplicated bitwise part but duplicates another enum value so let's report
+            If Not hasBitwiseIssue And isDuplicatedValue Then
+                context.ReportDiagnostic(binaryExpression.Parent.Parent.CreateDiagnostic(RuleDuplicatedValue))
+            End If
+        End Sub
+    End Class
+End Namespace
+

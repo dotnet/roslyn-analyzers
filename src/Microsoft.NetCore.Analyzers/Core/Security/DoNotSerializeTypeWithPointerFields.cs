f512fd8e41d492beffdc718d541b6f09f842b3b4
Paul Ming <paulming@microsoft.com>
2019-12-12 13:21:12 -0800

    Remove extraneous comments

--- src/Microsoft.NetCore.Analyzers/Core/Security/DoNotSerializeTypeWithPointerFields.cs
+++ src/Microsoft.NetCore.Analyzers/Core/Security/DoNotSerializeTypeWithPointerFields.cs
@@ -1,8 +1,5 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-// TODO(LLLXXXCCC): Enable nullable analysis.
-//#nullable disable
-
 using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Linq;

67213fdfd06bec23323f85d786deee5dcf1f21f4
Buyaa Namnan <bunamnan@microsoft.com>
2020-03-19 11:02:09 -0700

    Do not add Finalizer for MemoryManager derived types

---
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/DoNotAddFinalizerToMemoryManagerTDerivedTypes.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.NetCore.Analyzers.Runtime
+{
+    /// <summary>
+    /// CA2014: Do not add finilazer for MemoryManaged&lt;T&gt; derived types.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DoNotAddFinalizerToMemoryManagerTDerivedTypes : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA2014";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.DoNotAddFinalizerToMemoryManagedTDerivedTypesTitle), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.DoNotAddFinalizerToMemoryManagedTDerivedTypesMessage), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.DoNotAddFinalizerToMemoryManagedTDerivedTypesDescription), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+
+        internal static readonly DiagnosticDescriptor FinilizerRule = DiagnosticDescriptorHelper.Create(RuleId,
+            s_localizableTitle,
+            s_localizableMessage,
+            DiagnosticCategory.Reliability,
+            RuleLevel.BuildWarning,
+            description: s_localizableDescription,
+            isPortedFxCopRule: false,
+            isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(FinilizerRule);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.EnableConcurrentExecution();
+            analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            analysisContext.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var memoryManager1 = context.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemBuffersMemoryManager1);
+
+            if (memoryManager1 != null && namedTypeSymbol.DerivesFromOrImplementsAnyConstructionOf(memoryManager1))
+            {
+                if (namedTypeSymbol.HasFinalizer())
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(FinilizerRule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name));
+                }
+            }
+        }
+    }
+}
---
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Runtime/DoNotAddFinilizerToMemoryManagerTDerivedTypesTests.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Runtime.DoNotAddFinalizerToMemoryManagerTDerivedTypes,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.NetCore.Analyzers.Runtime.UnitTests
+{
+    public class DoNotAddFinilizerToMemoryManagerTDerivedTypesTests
+    {
+        [Fact]
+        public async Task ClassNotDerivedFromMemoryManagerOK()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+namespace TestNamespace
+{
+    class TestClass
+    {
+        private void TestMethod(string test)
+        {
+        }
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ClassDerivedFromMemoryManagerNoFinilizerOK()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+using System.Buffers;
+
+namespace TestNamespace
+{
+    abstract class TestClass<T> : MemoryManager<T>
+    {
+    }
+}");
+        }
+
+
+        [Fact]
+        public async Task ClassDerivedFromMemoryManagerWithFinilizerWarns()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+using System.Buffers;
+
+ namespace TestNamespace
+ {
+    class TestClass<T> : MemoryManager<T>
+    {
+        public override Span<T> GetSpan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MemoryHandle Pin(int elementIndex = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Unpin() { }
+
+        ~TestClass() { }
+
+        protected override void Dispose(bool disposing) { }
+    }
+}",
+                GetWarningResultAt(7, 17));
+        }
+        private DiagnosticResult GetWarningResultAt(int line, int column)
+           => VerifyCS.Diagnostic(DoNotAddFinalizerToMemoryManagerTDerivedTypes.FinilizerRule)
+               .WithLocation(line, column);
+    }
+}

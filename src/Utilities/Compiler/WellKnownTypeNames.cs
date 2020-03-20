345fa15617c8c531d3cdc2ac0ef29fabaf33ab9e
Amaury Levé <evangelink@gmail.com>
2020-03-20 11:11:09 +0100

    Remove un-needed usings

--- src/Utilities/Compiler/WellKnownTypeNames.cs
+++ src/Utilities/Compiler/WellKnownTypeNames.cs
@@ -1,8 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Runtime.CompilerServices;
-
 namespace Analyzer.Utilities
 {
     internal static class WellKnownTypeNames

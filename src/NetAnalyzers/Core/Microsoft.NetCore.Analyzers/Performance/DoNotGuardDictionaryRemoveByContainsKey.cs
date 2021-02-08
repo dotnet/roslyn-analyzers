7245b4e9cafad64cf32995b4cf85404a908c2d69
Sören Kuklau <kuklau@trenz.de>
2021-02-08 01:05:23 +0100

    Early stab at #33797 Don't guard Dictionary<K, V>.Remove(key) by ContainsKey(key)
    Diagnostic analyzer only; no CodeFixProvider yet

---
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Performance/DoNotGuardDictionaryRemoveByContainsKey.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Resx = Microsoft.NetCore.Analyzers.MicrosoftNetCoreAnalyzersResources;
+
+namespace Microsoft.NetCore.Analyzers.Performance
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class DoNotGuardDictionaryRemoveByContainsKey : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1841";
+
+        private static readonly LocalizableString s_localizableTitle = CreateResource(nameof(Resx.DoNotGuardDictionaryRemoveByContainsKeyTitle));
+        private static readonly LocalizableString s_localizableMessage = CreateResource(nameof(Resx.DoNotGuardDictionaryRemoveByContainsKeyMessage));
+        private static readonly LocalizableString s_localizableDescription = CreateResource(nameof(Resx.DoNotGuardDictionaryRemoveByContainsKeyDescription));
+
+        internal static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(
+            RuleId,
+            s_localizableTitle,
+            s_localizableMessage,
+            DiagnosticCategory.Performance,
+            RuleLevel.IdeSuggestion,
+            s_localizableDescription,
+            isPortedFxCopRule: false,
+            isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterCompilationStartAction(OnCompilationStart);
+        }
+
+        private static void OnCompilationStart(CompilationStartAnalysisContext context)
+        {
+            var compilation = context.Compilation;
+
+            if (!compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemCollectionsGenericDictionary2, out var dictionaryType))
+                return;
+
+            context.RegisterOperationAction(AnalyzeNamedType, OperationKind.Invocation);
+
+            static void AnalyzeNamedType(OperationAnalysisContext context)
+            {
+                if (context.Operation is not CodeAnalysis.Operations.IInvocationOperation invocationOperation)
+                    return;
+
+                if (invocationOperation.TargetMethod.Name != "ContainsKey")
+                    return;
+
+                if (invocationOperation.Parent is not CodeAnalysis.Operations.IConditionalOperation parentConditionalOperation)
+                    return;
+
+                if (parentConditionalOperation.WhenFalse == null &&
+                    parentConditionalOperation.WhenTrue.Children.HasExactly(1) &&
+                    parentConditionalOperation.WhenTrue.Children.First() is CodeAnalysis.Operations.IInvocationOperation childInvocationOperation &&
+                    childInvocationOperation.TargetMethod.Name == "Remove")
+                {
+                    var diagnostic = Diagnostic.Create(Rule, invocationOperation.Syntax.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+
+        private static LocalizableString CreateResource(string resourceName) 
+            => new LocalizableResourceString(resourceName, Resx.ResourceManager, typeof(Resx));
+    }
+}
---
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Performance/DoNotGuardDictionaryRemoveByContainsKeyTests.cs
@@ -0,0 +1,141 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Performance.DoNotGuardDictionaryRemoveByContainsKey,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Performance.DoNotGuardDictionaryRemoveByContainsKey,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.NetCore.Analyzers.Performance.UnitTests
+{
+    public class DoNotGuardDictionaryRemoveByContainsKeyTests
+    {
+        #region Reports Diagnostic
+        [Fact]
+        public async Task RemoveIsTheOnlyStatement_ReportsDiagnostic_CS()
+        {
+            string code = @"
+" + CSUsings + @"
+namespace Testopolis
+{
+    public class MyClass
+    {
+        private readonly Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+
+        public MyClass()
+        {
+            if ({|#0:MyDictionary.ContainsKey(""Key"")|})
+                MyDictionary.Remove(""Key"");
+        }
+    }
+}";
+
+            var diagnostic = VerifyCS.Diagnostic(Rule).WithLocation(0);
+            await new VerifyCS.Test
+            {
+                TestCode = code,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                ExpectedDiagnostics = { diagnostic }
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task AdditionalStatements_NoDiagnostic_CS()
+        {
+            string code = @"
+" + CSUsings + @"
+namespace Testopolis
+{
+    public class MyClass
+    {
+        private readonly Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+
+        public MyClass()
+        {
+            if (MyDictionary.ContainsKey(""Key""))
+            {
+                MyDictionary.Remove(""Key"");
+                Console.WriteLine();
+            }
+        }
+    }
+}";
+
+            await new VerifyCS.Test
+            {
+                TestCode = code,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                ExpectedDiagnostics = { }
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task RemoveIsTheOnlyStatement_ReportsDiagnostic_VB()
+        {
+            string code = @"
+" + VBUsings + @"
+Namespace Testopolis
+    Public Class MyClass
+        Public Dictionary(Of String, String) MyDictionary = New Dictionary(Of String, String)();
+
+        Public Sub New()
+            If {|#0:MyDictionary.ContainsKey(""Key"")|} Then
+                MyDictionary.Remove(""Key"")
+            End If
+        End Sub
+    End Class
+End Namespace";
+
+            var diagnostic = VerifyVB.Diagnostic(Rule).WithLocation(0);
+            await new VerifyVB.Test
+            {
+                TestCode = code,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                ExpectedDiagnostics = { diagnostic }
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task AdditionalStatements_NoDiagnostic_VB()
+        {
+            string code = @"
+" + VBUsings + @"
+Namespace Testopolis
+    Public Class MyClass
+        Public Dictionary(Of String, String) MyDictionary = New Dictionary(Of String, String)();
+
+        Public Sub New()
+            If MyDictionary.ContainsKey(""Key"") Then
+                MyDictionary.Remove(""Key"")
+                Console.WriteLine()
+            End If
+        End Sub
+    End Class
+End Namespace";
+
+            await new VerifyVB.Test
+            {
+                TestCode = code,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                ExpectedDiagnostics = { }
+            }.RunAsync();
+        }
+        #endregion
+
+        #region Helpers
+        private const string CSUsings = @"using System;
+using System.Collections.Generic;";
+
+        private const string VBUsings = @"Imports System
+Imports System.Collections.Generic";
+
+        private static DiagnosticDescriptor Rule => DoNotGuardDictionaryRemoveByContainsKey.Rule;
+        #endregion
+    }
+}

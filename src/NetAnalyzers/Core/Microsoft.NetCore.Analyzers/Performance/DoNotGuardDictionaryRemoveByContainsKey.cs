6fe0b876c4bdf68a626c07f471b2dd39e3e108ea
Sören Kuklau <kuklau@trenz.de>
2021-02-11 17:27:36 +0100

    Assign the correct rule ID, fix whitespace, add a comment

--- src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Performance/DoNotGuardDictionaryRemoveByContainsKey.cs
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Performance/DoNotGuardDictionaryRemoveByContainsKey.cs
@@ -14,7 +14,7 @@ namespace Microsoft.NetCore.Analyzers.Performance
     [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
     public class DoNotGuardDictionaryRemoveByContainsKey : DiagnosticAnalyzer
     {
-        internal const string RuleId = "CA1841";
+        internal const string RuleId = "CA1839";
 
         private static readonly LocalizableString s_localizableTitle = CreateResource(nameof(Resx.DoNotGuardDictionaryRemoveByContainsKeyTitle));
         private static readonly LocalizableString s_localizableMessage = CreateResource(nameof(Resx.DoNotGuardDictionaryRemoveByContainsKeyMessage));
@@ -47,7 +47,7 @@ namespace Microsoft.NetCore.Analyzers.Performance
                 return;
 
             context.RegisterOperationAction(AnalyzeNamedType, OperationKind.Invocation);
-            
+
             static void AnalyzeNamedType(OperationAnalysisContext context)
             {
                 if (context.Operation is not CodeAnalysis.Operations.IInvocationOperation invocationOperation)
@@ -58,6 +58,8 @@ namespace Microsoft.NetCore.Analyzers.Performance
 
                 if (invocationOperation.Parent is not CodeAnalysis.Operations.IConditionalOperation parentConditionalOperation)
                     return;
+
+                // we only want to report this diagnostic if the Contains/Remove pair is all there is
 
                 if (parentConditionalOperation.WhenFalse == null &&
                     parentConditionalOperation.WhenTrue.Children.HasExactly(1))
@@ -89,7 +91,7 @@ namespace Microsoft.NetCore.Analyzers.Performance
             }
         }
 
-        private static LocalizableString CreateResource(string resourceName) 
+        private static LocalizableString CreateResource(string resourceName)
             => new LocalizableResourceString(resourceName, Resx.ResourceManager, typeof(Resx));
     }
 }

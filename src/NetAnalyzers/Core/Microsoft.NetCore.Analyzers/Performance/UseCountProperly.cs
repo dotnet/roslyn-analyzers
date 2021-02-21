ef610669dd8b883cbd7e530db3835c95df45af16
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2021-02-21 16:51:57 +0200

    Fix formatting

--- src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Performance/UseCountProperly.cs
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Performance/UseCountProperly.cs
@@ -595,12 +595,12 @@ namespace Microsoft.NetCore.Analyzers.Performance
             return true;
         }
 
-        private static bool TypeContainsVisiblePropertyWithoutCircularity(OperationAnalysisContext context, IOperation operation, ITypeSymbol type, 
+        private static bool TypeContainsVisiblePropertyWithoutCircularity(OperationAnalysisContext context, IOperation operation, ITypeSymbol type,
         	string propertyName, SpecialType propertyType, [NotNullWhen(true)] out ISymbol? propertySymbol)
             => TypeContainsVisiblePropertyWithoutCircularity(context, operation, type, propertyName, propertyType, propertyType, out propertySymbol);
 
         private static bool TypeContainsVisiblePropertyWithoutCircularity(OperationAnalysisContext context, IOperation operation, ITypeSymbol type,
-        	string propertyName, SpecialType lowerBound, SpecialType upperBound, [NotNullWhen(true)] out ISymbol? propertySymbol)
+            string propertyName, SpecialType lowerBound, SpecialType upperBound, [NotNullWhen(true)] out ISymbol? propertySymbol)
         {
             if (TypeContainsMember(context, type, propertyName, lowerBound, upperBound, out bool isPropertyValidAndVisible, out propertySymbol) &&
                 !IsPropertyGetterUsingThisInstance(context, operation, propertySymbol))

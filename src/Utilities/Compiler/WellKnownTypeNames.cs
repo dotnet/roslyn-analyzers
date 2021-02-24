6b715777f2f34559467ccf800fb4f13feb80a195
psxvoid <pavel.sapehin@gmail.com>
2021-02-24 07:04:19 +0300

    Fix json-ctor order in well-known-names

--- src/Utilities/Compiler/WellKnownTypeNames.cs
+++ src/Utilities/Compiler/WellKnownTypeNames.cs
@@ -331,9 +331,9 @@ namespace Analyzer.Utilities
         public const string SystemStringComparison = "System.StringComparison";
         public const string SystemSystemException = "System.SystemException";
         public const string SystemTextEncoding = "System.Text.Encoding";
+        public const string SystemTextJsonSerializationJsonConstructorAttribute = "System.Text.Json.Serialization.JsonConstructorAttribute";
         public const string SystemTextRegularExpressionsRegex = "System.Text.RegularExpressions.Regex";
         public const string SystemTextStringBuilder = "System.Text.StringBuilder";
-        public const string SystemTextJsonSerializationJsonConstructorAttribute = "System.Text.Json.Serialization.JsonConstructorAttribute";
         public const string SystemThreadingCancellationToken = "System.Threading.CancellationToken";
         public const string SystemThreadingInterlocked = "System.Threading.Interlocked";
         public const string SystemThreadingMonitor = "System.Threading.Monitor";

07c9509f6ed66577b87f9ce268e72667e82ac188
Sam Harwell <Sam.Harwell@microsoft.com>
2021-07-26 12:24:09 -0700

    Suppress VSTHRD200 on benchmark methods

--- src/PerformanceTests/Tests/Enabled/VisualBasic_CA1416.cs
+++ src/PerformanceTests/Tests/Enabled/VisualBasic_CA1416.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+#pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
+
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;

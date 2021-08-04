634fc0494b490b24a54e078877d1b34b591d7c3a
Jeff Handley <jeff.handley@microsoft.com>
2021-08-03 19:26:50 -0700

    Implement the ModuleInitializerAttributeShouldNotBeUsedInLibraries analyzer

---
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/ModuleInitializerAttributeShouldNotBeUsedInLibraries.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.NetCore.Analyzers.Runtime
+{
+    /// <summary>
+    /// CS2255: ModuleInitializer attributes should not be used in libraries.
+    /// </summary>
+    /// <remarks>
+    /// ModuleInitializer methods must:
+    /// - Be parameterless
+    /// - Be void or async void
+    /// - Not be generic or contained in a generic type
+    /// - Be accessible in the module using public or internal
+    /// </remarks>
+#pragma warning disable RS1004 // The [ModuleInitializer] feature only applies to C#
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+#pragma warning restore RS1004
+    public sealed class ModuleInitializerAttributeShouldNotBeUsedInLibraries : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA2255";
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.ModuleInitializerAttributeShouldNotBeUsedInLibrariesTitle), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.ModuleInitializerAttributeShouldNotBeUsedInLibrariesMessage), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.ModuleInitializerAttributeShouldNotBeUsedInLibrariesDescription), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(RuleId,
+                                                                    s_localizableTitle,
+                                                                    s_localizableMessage,
+                                                                    DiagnosticCategory.Usage,
+                                                                    RuleLevel.BuildWarning,
+                                                                    s_localizableDescription,
+                                                                    isPortedFxCopRule: true,
+                                                                    isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                INamedTypeSymbol? moduleInitializerAttributeType = compilationContext.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemRuntimeCompilerServicesModuleInitializerAttribute);
+
+                if (moduleInitializerAttributeType is null) return;
+
+                compilationContext.RegisterSymbolAction(context =>
+                {
+                    if (context.Symbol is IMethodSymbol method)
+                    {
+                        if (method.IsPrivate() ||
+                            method.Parameters.Length > 0 ||
+                            method.IsGenericMethod ||
+                            !method.IsStatic ||
+                            !method.ReturnsVoid
+                        )
+                        {
+                            return;
+                        }
+
+                        AttributeData? initializerAttribute = context.Symbol.GetAttributes(moduleInitializerAttributeType).FirstOrDefault();
+
+                        if (initializerAttribute is not null)
+                        {
+                            context.ReportDiagnostic(initializerAttribute.ApplicationSyntaxReference.GetSyntax().CreateDiagnostic(Rule));
+                        }
+                    }
+                },
+                SymbolKind.Method);
+            });
+        }
+    }
+}
---
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Runtime/ModuleInitializerAttributeShouldNotBeUsedInLibrariesTests.cs
@@ -0,0 +1,207 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Runtime.ModuleInitializerAttributeShouldNotBeUsedInLibraries,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Runtime.ModuleInitializerAttributeShouldNotBeUsedInLibraries,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.NetCore.Analyzers.Runtime.UnitTests
+{
+    public class ModuleInitializerAttributeShouldNotBeUsedInLibrariesTests
+    {
+        [Theory]
+        [InlineData("public", "public", false)]
+        [InlineData("public", "internal", false)]
+        [InlineData("internal", "public", false)]
+        [InlineData("internal", "internal", false)]
+        [InlineData("public", "public", true)]
+        [InlineData("public", "internal", true)]
+        [InlineData("internal", "public", true)]
+        [InlineData("internal", "internal", true)]
+        public async Task CA2255ModuleInitializerOnMethod(string classModifier, string methodModifier, bool useAsync = false)
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        @$"
+                        {classModifier} class CA2255ModuleInitializerOnMethod
+                        {{
+                            [{{|CA2255:System.Runtime.CompilerServices.ModuleInitializer|}}]
+                            {methodModifier} static {(useAsync ? "async" : "")} void Initialize() {{ }}
+                        }}
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("public", "public", false)]
+        [InlineData("public", "internal", false)]
+        [InlineData("internal", "public", false)]
+        [InlineData("internal", "internal", false)]
+        [InlineData("public", "public", true)]
+        [InlineData("public", "internal", true)]
+        [InlineData("internal", "public", true)]
+        [InlineData("internal", "internal", true)]
+        public async Task CA2255ModuleInitializerOnMethod_WithParens(string classModifier, string methodModifier, bool useAsync = false)
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        @$"
+                        {classModifier} class CA2255ModuleInitializerOnMethod
+                        {{
+                            [{{|CA2255:System.Runtime.CompilerServices.ModuleInitializer()|}}]
+                            {methodModifier} static {(useAsync ? "async" : "")} void Initialize() {{ }}
+                        }}
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("public", "public", false)]
+        [InlineData("public", "internal", false)]
+        [InlineData("internal", "public", false)]
+        [InlineData("internal", "internal", false)]
+        [InlineData("public", "public", true)]
+        [InlineData("public", "internal", true)]
+        [InlineData("internal", "public", true)]
+        [InlineData("internal", "internal", true)]
+        public async Task CA2255ModuleInitializerOnMethod_Suppressed(string classModifier, string methodModifier, bool useAsync = false)
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        $@"
+                        #pragma warning disable CA2255
+                        {classModifier} class CA2255ModuleInitializerOnMethod
+                        {{
+                            [System.Runtime.CompilerServices.ModuleInitializer]
+                            {methodModifier} static {(useAsync ? "async" : "")} void Initialize() {{ }}
+                        }}
+                        #pragma warning restore CA2255
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task CA2255DoesNotApply_ToPrivateMethods()
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        @$"
+                        public class CA2255ModuleInitializerOnMethod
+                        {{
+                            [{{|CS8814:System.Runtime.CompilerServices.ModuleInitializer|}}]
+                            private static void Initialize() {{ }}
+                        }}
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task CA2255DoesNotApply_ToNonVoidMethods()
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        @$"
+                        public class CA2255ModuleInitializerOnMethod
+                        {{
+                            [{{|CS8815:System.Runtime.CompilerServices.ModuleInitializer|}}]
+                            public static bool Initialize() {{ return true; }}
+                        }}
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task CA2255DoesNotApply_ToInstanceMethods()
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        @$"
+                        public class CA2255ModuleInitializerOnMethod
+                        {{
+                            [{{|CS8815:System.Runtime.CompilerServices.ModuleInitializer|}}]
+                            public void Initialize() {{ }}
+                        }}
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+
+
+        [Fact]
+        public async Task CA2255DoesNotApply_ToGenericMethods()
+        {
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                    Sources = {
+                        @$"
+                        public class CA2255ModuleInitializerOnMethod<T>
+                        {{
+                            [{{|CS8816:System.Runtime.CompilerServices.ModuleInitializer|}}]
+                            public static void Initialize<T>() {{ }}
+                        }}
+                        "
+                    }
+                },
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                MarkupOptions = MarkupOptions.UseFirstDescriptor
+            }.RunAsync();
+        }
+    }
+}

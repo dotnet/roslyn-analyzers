c940dfb5bf0f24d96e8f53e0a3f38bb88dd0e692
Sören Kuklau <kuklau@trenz.de>
2021-09-04 20:28:03 +0200

    Claim/configure rule ID

---
+++ src/NetAnalyzers/Core/Microsoft.CodeQuality.Analyzers/Usage/DoNotCallOrderByMultipleTimes.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeQuality.Analyzers.Usage
+{
+    using static MicrosoftCodeQualityAnalyzersResources;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class DoNotCallOrderByMultipleTimes : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA2259";
+
+        internal static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(
+            RuleId,
+            "",
+            CreateLocalizableResourceString(""),
+            DiagnosticCategory.Maintainability,
+            RuleLevel.Disabled,
+            description: null,
+            isPortedFxCopRule: false,
+            isDataflowRule: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Usage/DoNotCallOrderByMultipleTimesTests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.NetAnalyzers.UnitTests.MicrosoftNetCoreAnalyzers.Usage
+{
+    public class DoNotCallOrderByMultipleTimesTests
+    {
+        [Fact]
+        public async Task OnlyOneOrderByCall_NoDiagnosticAsync()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public async Task OrderByCallsAreUnrelated_NoDiagnosticAsync()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public async Task AtLeastTwoOrderByCalls_OfferFixerAsync()
+        {
+            Assert.False(true);
+        }
+    }
+}

9659c242b04e495d5a2fe9e77684920efed1381b
Newell Clark <newellofthefuture@gmail.com>
2021-09-21 08:17:41 -0400

    Analyzer working

--- src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferReadOnlySpanPropertiesOverReadOnlyArrayFields.cs
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferReadOnlySpanPropertiesOverReadOnlyArrayFields.cs
@@ -3,10 +3,12 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using Analyzer.Utilities;
 using Analyzer.Utilities.Extensions;
+using Analyzer.Utilities.PooledObjects;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
@@ -14,7 +16,7 @@ using Resx = Microsoft.NetCore.Analyzers.MicrosoftNetCoreAnalyzersResources;
 
 namespace Microsoft.NetCore.Analyzers.Runtime
 {
-    //  Visual Basic doesn't allow ref-struct APIs
+    //  Visual Basic doesn't allow ref struct APIs
 #pragma warning disable RS1004 // Recommend adding language support to diagnostic analyzer
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
 #pragma warning restore RS1004 // Recommend adding language support to diagnostic analyzer
@@ -22,17 +24,13 @@ namespace Microsoft.NetCore.Analyzers.Runtime
     {
         internal const string RuleId = "CA1850";
 
-        private static readonly LocalizableString s_localizableTitle = Resx.CreateLocalizableResourceString(nameof(Resx.PreferReadOnlySpanPropertiesOverReadOnlyArrayFields_Title));
-        private static readonly LocalizableString s_localizableMessage = Resx.CreateLocalizableResourceString(nameof(Resx.PreferReadOnlySpanPropertiesOverReadOnlyArrayFields_Message));
-        private static readonly LocalizableString s_localizableDescription = Resx.CreateLocalizableResourceString(nameof(Resx.PreferReadOnlySpanPropertiesOverReadOnlyArrayFields_Description));
-
         internal static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(
             RuleId,
-            s_localizableTitle,
-            s_localizableMessage,
+            Resx.CreateLocalizableResourceString(nameof(Resx.PreferReadOnlySpanPropertiesOverReadOnlyArrayFields_Title)),
+            Resx.CreateLocalizableResourceString(nameof(Resx.PreferReadOnlySpanPropertiesOverReadOnlyArrayFields_Message)),
             DiagnosticCategory.Performance,
             RuleLevel.IdeSuggestion,
-            s_localizableDescription,
+            Resx.CreateLocalizableResourceString(nameof(Resx.PreferReadOnlySpanPropertiesOverReadOnlyArrayFields_Description)),
             isPortedFxCopRule: false,
             isDataflowRule: false);
 
@@ -42,6 +40,277 @@ namespace Microsoft.NetCore.Analyzers.Runtime
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
+            context.RegisterCompilationStartAction(OnCompilationStart);
+        }
+
+        private static void OnCompilationStart(CompilationStartAnalysisContext context)
+        {
+            //  Bail if we don't have all the required symbols.
+            if (!Cache.TryCreateCache(context.Compilation, out var cache))
+                return;
+            var fieldReferenceWalker = new FieldReferenceWalker(cache);
+
+            context.RegisterSymbolAction(context =>
+            {
+                var field = (IFieldSymbol)context.Symbol;
+
+                if (field.IsStatic && field.IsReadOnly && field.Type is IArrayTypeSymbol arrayType && cache.IsSupportedArrayElementType(arrayType.ElementType))
+                {
+                    cache.Candidates.Add(field);
+                }
+            }, SymbolKind.Field);
+
+            context.RegisterOperationAction(AnalyzeFieldReferenceOperation, OperationKind.FieldReference);
+            context.RegisterOperationAction(AnalyzeFieldInitializerOperation, OperationKind.FieldInitializer);
+
+            context.RegisterCompilationEndAction(context =>
+            {
+                foreach (var candidate in cache.Candidates)
+                {
+                    context.ReportDiagnostic(candidate.CreateDiagnostic(Rule));
+                }
+                cache.Dispose();
+            });
+
+            return;
+
+            //  Local functions
+
+            void AnalyzeFieldReferenceOperation(OperationAnalysisContext context)
+            {
+                var fieldReference = (IFieldReferenceOperation)context.Operation;
+                if (cache.Candidates.Contains(fieldReference.Field))
+                    fieldReference.Parent.Accept(fieldReferenceWalker);
+            }
+
+            void AnalyzeFieldInitializerOperation(OperationAnalysisContext context)
+            {
+                //  Eliminate candidates that are not initialized with an array initializer
+                //  containing all constant elements.
+                var fieldInitializer = (IFieldInitializerOperation)context.Operation;
+                if (fieldInitializer.Value is not IArrayCreationOperation arrayCreation ||
+                    (fieldInitializer.InitializedFields.Any(x => cache.Candidates.Contains(x)) &&
+                    arrayCreation.Initializer.ElementValues.Any(x => !x.ConstantValue.HasValue)))
+                {
+                    foreach (var field in fieldInitializer.InitializedFields)
+                        cache.Candidates.Remove(field);
+                }
+            }
+        }
+
+        private sealed class FieldReferenceWalker : OperationVisitor
+        {
+            private readonly Cache _cache;
+            private readonly ArrayElementWalker _elementWalker;
+
+            public FieldReferenceWalker(Cache cache)
+            {
+                _cache = cache;
+                _elementWalker = new ArrayElementWalker(cache);
+            }
+
+            public override void VisitArgument(IArgumentOperation operation)
+            {
+                if (operation.Parent is IInvocationOperation invocation && !_cache.IsAsSpanMethod(invocation.TargetMethod))
+                    _cache.Candidates.Remove(GetField(operation.Value));
+
+                base.VisitArgument(operation);
+            }
+
+            public override void VisitConversion(IConversionOperation operation)
+            {
+                if (!operation.Type.OriginalDefinition.Equals(_cache.ReadOnlySpanType, SymbolEqualityComparer.Default))
+                    _cache.Candidates.Remove(GetField(operation.Operand));
+
+                base.VisitConversion(operation);
+            }
+
+            public override void VisitArrayElementReference(IArrayElementReferenceOperation operation)
+            {
+                //  Eliminate candidates if any elements are assigned to ref locals, parameters, or returns.
+                if (operation.GetValueUsageInfo(operation.SemanticModel.GetEnclosingSymbol(operation.Syntax.SpanStart)) is
+                    ValueUsageInfo.WritableReference or ValueUsageInfo.ReadableWritableReference)
+                {
+                    _cache.Candidates.Remove(GetField(operation.ArrayReference));
+                }
+                else
+                {
+                    operation.Parent.Accept(_elementWalker);
+                }
+
+                base.VisitArrayElementReference(operation);
+            }
+
+            public override void VisitPropertyReference(IPropertyReferenceOperation operation)
+            {
+                if (!operation.Property.OriginalDefinition.Equals(_cache.ArrayLengthProperty, SymbolEqualityComparer.Default))
+                    _cache.Candidates.Remove(GetField(operation.Instance));
+
+                base.VisitPropertyReference(operation);
+            }
+
+            public override void VisitInvocation(IInvocationOperation operation)
+            {
+                _cache.Candidates.Remove(GetField(operation.Instance));
+
+                base.VisitInvocation(operation);
+            }
+
+            public override void VisitFieldInitializer(IFieldInitializerOperation operation)
+            {
+                if (operation.Value is IArrayCreationOperation arrayCreation)
+                {
+                    foreach (var element in arrayCreation.Initializer.ElementValues)
+                    {
+                        if (!element.ConstantValue.HasValue)
+                        {
+                            foreach (var field in operation.InitializedFields)
+                                _cache.Candidates.Remove(field);
+                            break;
+                        }
+                    }
+                }
+                base.VisitFieldInitializer(operation);
+            }
+
+            private static IFieldSymbol GetField(IOperation fieldReference) => ((IFieldReferenceOperation)fieldReference).Field;
+            private static bool TryGetField(IOperation? fieldReference, [NotNullWhen(true)] out IFieldSymbol? field)
+            {
+                return (field = (fieldReference as IFieldReferenceOperation)?.Field) is not null;
+            }
+        }
+
+        private sealed class ArrayElementWalker : OperationVisitor
+        {
+            private readonly Cache _cache;
+
+            public ArrayElementWalker(Cache cache)
+            {
+                _cache = cache;
+            }
+
+            public override void VisitSimpleAssignment(ISimpleAssignmentOperation operation)
+            {
+                if (TryGetField(operation.Target, out var field))
+                {
+                    _cache.Candidates.Remove(field);
+                }
+
+                base.VisitSimpleAssignment(operation);
+            }
+
+            public override void VisitCompoundAssignment(ICompoundAssignmentOperation operation)
+            {
+                if (TryGetField(operation.Target, out var field))
+                {
+                    _cache.Candidates.Remove(field);
+                }
+
+                base.VisitCompoundAssignment(operation);
+            }
+
+            public override void VisitTuple(ITupleOperation operation)
+            {
+                if (operation.Parent is IDeconstructionAssignmentOperation)
+                {
+                    foreach (var element in operation.Elements)
+                    {
+                        if (TryGetField(element, out var field))
+                            _cache.Candidates.Remove(field);
+                    }
+                }
+
+                base.VisitTuple(operation);
+            }
+
+            public override void VisitIncrementOrDecrement(IIncrementOrDecrementOperation operation)
+            {
+                _cache.Candidates.Remove(GetField(operation.Target));
+
+                base.VisitIncrementOrDecrement(operation);
+            }
+
+            private static bool TryGetField(IOperation elementReference, [NotNullWhen(true)] out IFieldSymbol? field)
+            {
+                return (field = ((elementReference as IArrayElementReferenceOperation)?.ArrayReference as IFieldReferenceOperation)?.Field) is not null;
+            }
+
+            private static IFieldSymbol GetField(IOperation elementReference)
+            {
+                return ((IFieldReferenceOperation)((IArrayElementReferenceOperation)elementReference).ArrayReference).Field;
+            }
+        }
+
+        private sealed class Cache : IDisposable
+        {
+            private readonly ImmutableHashSet<ITypeSymbol> _supportedArrayElementTypes;
+            private readonly ImmutableHashSet<IMethodSymbol> _asSpanMethods;
+            public INamedTypeSymbol ReadOnlySpanType { get; }
+            public IPropertySymbol ArrayLengthProperty { get; }
+
+            public PooledConcurrentSet<IFieldSymbol> Candidates { get; }
+
+            private Cache(Compilation compilation, INamedTypeSymbol readOnlySpanType, IPropertySymbol arrayLengthProperty)
+            {
+                _supportedArrayElementTypes = GetSupportedArrayElementTypes(compilation);
+                ReadOnlySpanType = readOnlySpanType;
+                ArrayLengthProperty = arrayLengthProperty;
+                Candidates = PooledConcurrentSet<IFieldSymbol>.GetInstance(SymbolEqualityComparer.Default);
+                _asSpanMethods = GetAsSpanMethods(compilation, readOnlySpanType);
+                return;
+
+                //  Local functions
+
+                static ImmutableHashSet<ITypeSymbol> GetSupportedArrayElementTypes(Compilation compilation)
+                {
+                    var builder = ImmutableHashSet.CreateBuilder<ITypeSymbol>(SymbolEqualityComparer.Default);
+
+                    builder.AddIfNotNull(compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemBoolean));
+                    builder.AddIfNotNull(compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemByte));
+                    builder.AddIfNotNull(compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemSByte));
+
+                    return builder.ToImmutable();
+                }
+
+                static ImmutableHashSet<IMethodSymbol> GetAsSpanMethods(Compilation compilation, ITypeSymbol readOnlySpanType)
+                {
+                    if (!compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemMemoryExtensions, out var memoryExtensionsType))
+                        return ImmutableHashSet<IMethodSymbol>.Empty;
+
+                    var spanType = compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemSpan1);
+                    var builder = ImmutableHashSet.CreateBuilder<IMethodSymbol>(SymbolEqualityComparer.Default);
+                    var asSpanMethods = memoryExtensionsType.GetMembers(nameof(MemoryExtensions.AsSpan)).OfType<IMethodSymbol>()
+                        .Where(x =>
+                        {
+                            return x.IsPublic() &&
+                                (x.ReturnType.OriginalDefinition.Equals(readOnlySpanType, SymbolEqualityComparer.Default) ||
+                                x.ReturnType.OriginalDefinition.Equals(spanType, SymbolEqualityComparer.Default));
+                        });
+                    builder.AddRange(asSpanMethods);
+
+                    return builder.ToImmutable();
+                }
+            }
+
+            public static bool TryCreateCache(Compilation compilation, [NotNullWhen(true)] out Cache? cache)
+            {
+                var arrayLengthProperty = compilation.GetSpecialType(SpecialType.System_Array).GetMembers(nameof(Array.Length)).OfType<IPropertySymbol>().FirstOrDefault();
+                if (compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemReadOnlySpan1, out var rosType) &&
+                    arrayLengthProperty is not null)
+                {
+                    cache = new Cache(compilation, rosType, arrayLengthProperty);
+                    return true;
+                }
+
+                cache = null;
+                return false;
+            }
+
+            public bool IsSupportedArrayElementType(ITypeSymbol type) => _supportedArrayElementTypes.Contains(type);
+
+            public bool IsAsSpanMethod(IMethodSymbol method) => _asSpanMethods.Contains(method.OriginalDefinition);
+
+            public void Dispose() => Candidates.Dispose();
         }
     }
 }
--- src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Runtime/PreferReadOnlySpanPropertiesOverReadOnlyArrayFieldsTests.cs
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Runtime/PreferReadOnlySpanPropertiesOverReadOnlyArrayFieldsTests.cs
@@ -19,9 +19,16 @@ namespace Microsoft.NetCore.Analyzers.Runtime.UnitTests
         [Theory]
         [InlineData("bool", "true, true, false, false, true")]
         [InlineData("bool", "false")]
+        [InlineData("bool", "ConstBool, true, false")]
+        [InlineData("bool", "true, ConstBool, true")]
+        [InlineData("bool", "")]
         [InlineData("byte", "7, 14, 128, 255")]
+        [InlineData("byte", "8, 16, ConstByte")]
+        [InlineData("byte", "")]
         [InlineData("sbyte", "-41, 11, 0")]
-        public Task LiteralReadOnlyArrayFields_Diagnostic_CS(string arrayType, string arrayInitializer)
+        [InlineData("sbyte", "ConstSByte, ConstSByte")]
+        [InlineData("sbyte", "")]
+        public Task ConstReadOnlyArrayFields_Diagnostic_CS(string arrayType, string arrayInitializer)
         {
             var test = new VerifyCS.Test
             {
@@ -29,7 +36,10 @@ namespace Microsoft.NetCore.Analyzers.Runtime.UnitTests
 using System;
 public class C
 {{
-    {{|#0:private static readonly {arrayType}[] _array = new {arrayType}[] {{ {arrayInitializer} }}|}};
+    private static readonly {arrayType}[] {{|#0:_array|}} = new {arrayType}[] {{ {arrayInitializer} }};
+    private const byte ConstByte = 7;
+    private const sbyte ConstSByte = -7;
+    private const bool ConstBool = true;
 }}",
                 ExpectedDiagnostics = { VerifyCS.Diagnostic(Rule).WithLocation(0) },
                 ReferenceAssemblies = ReferenceAssemblies.Net.Net50
@@ -38,14 +48,15 @@ public class C
         }
 
         [Theory]
-        [InlineData("byte b = a[2];")]
-        [InlineData("byte c = a[1] + a[2];")]
+        [InlineData("int n = a[2];")]
+        [InlineData("int n = a[1] + a[2];")]
+        [InlineData("int n = a.Length;")]
         [InlineData("a[0].ToString();")]
         [InlineData("ConsumeRos(a);")]
         [InlineData("ConsumeRos(a.AsSpan());")]
         [InlineData("ConsumeRos(a.AsSpan(3));")]
         [InlineData("ConsumeRos(a.AsSpan(1, 4));")]
-        public Task LiteralReadOnlyArrayField_LegalUses_Diagnostic_CS(string code)
+        public Task LegalUsage_Diagnostic_CS(string code)
         {
             var test = new VerifyCS.Test
             {
@@ -53,7 +64,7 @@ public class C
 using System;
 public class C
 {{
-    {{|#0:private static readonly byte[] a = new byte[] {{ 2, 4, 8, 16 }}|}};
+    private static readonly byte[] {{|#0:a|}} = new byte[] {{ 2, 4, 8, 16 }};
     public void ConsumeRos(ReadOnlySpan<byte> span) {{ }}
 
     public void M()
@@ -70,7 +81,10 @@ public class C
         [Theory]
         [InlineData("new byte[] { GetByte(), 7, 14 }")]
         [InlineData("GetBytes()")]
-        public Task NonLiteralInitializer_NoDiagnostic_CS(string initializer)
+        [InlineData("new byte[] { 6, 19, GetByte() }")]
+        [InlineData("new byte[] { 5, readOnlyByte, 5 }")]
+        [InlineData("new byte[] { 4, mutableByte, 4 }")]
+        public Task NonConstInitializer_NoDiagnostic_CS(string initializer)
         {
             var test = new VerifyCS.Test
             {
@@ -78,9 +92,12 @@ public class C
 using System;
 public class C
 {{
-    private static readonly byte[] a = {initializer};
     private static byte[] GetBytes() => null;
     private static byte GetByte() => 4;
+    private static readonly byte readOnlyByte = 7;
+    private static byte mutableByte = 6;
+
+    private static readonly byte[] a = {initializer};
 }}",
                 ReferenceAssemblies = ReferenceAssemblies.Net.Net50
             };
@@ -91,13 +108,25 @@ public class C
         [InlineData("ConsumeArray(a);")]
         [InlineData("ConsumeSpan(a);")]
         [InlineData("ConsumeEnumerable(a);")]
+        [InlineData("int n = a.Rank;")]
+        [InlineData("a.CopyTo(new byte[5], 0);")]
         [InlineData("a[0] = 12;")]
+        [InlineData("(a[0], a[1]) = t;")]
+        [InlineData("(a[1], b) = t;")]
+        [InlineData("(b, a[1]) = t;")]
         [InlineData("a[1] += 12;")]
         [InlineData("a[2] -= 12;")]
         [InlineData("a[3] *= 12;")]
         [InlineData("a[4] /= 12;")]
+        [InlineData("a[1]++;")]
+        [InlineData("++a[1];")]
+        [InlineData("a[1]--;")]
+        [InlineData("--a[1];")]
         [InlineData("ref byte r = ref a[1];")]
         [InlineData("ConsumeByteRef(ref a[3]);")]
+        [InlineData("ConsumeByteOut(out a[3]);")]
+        [InlineData("ConsumeImplicit(a);")]
+        [InlineData("ConsumeExplicit((Explicit)a);")]
         public Task IllegalFieldUsage_NoDiagnostic_CS(string code)
         {
             var test = new VerifyCS.Test
@@ -112,11 +141,65 @@ public class C
     private static void ConsumeEnumerable(IEnumerable<byte> bytes) {{ }}
     private static void ConsumeSpan(Span<byte> bytes) {{ }}
     private static void ConsumeByteRef(ref byte b) {{ }}
+    private static void ConsumeByteOut(out byte b) => b = default;
+    private static void ConsumeImplicit(Implicit i) {{ }}
+    private static void ConsumeExplicit(Explicit e) {{ }}
 
-    public void M()
+    public void M(byte b, (byte x, byte y) t)
     {{
         {code}
     }}
+}}
+
+public class Implicit
+{{
+    public static implicit operator Implicit(byte[] operand) => new Implicit();
+}}
+
+public class Explicit
+{{
+    public static explicit operator Explicit(byte[] operand) => new Explicit();
+}}",
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            return test.RunAsync();
+        }
+
+        [Fact]
+        public Task ElementReturnByRef_NoDiagnostic_CS()
+        {
+            var test = new VerifyCS.Test
+            {
+                TestCode = @"
+using System;
+public class C
+{
+    private static readonly byte[] a = new byte[] { 1, 2, 3 };
+
+    public ref byte M()
+    {
+        return ref a[1];
+    }
+}",
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            return test.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("private readonly byte[] a = new byte[] { 1 };")]
+        [InlineData("private static byte[] a = new byte[] { 1 };")]
+        [InlineData("private static readonly short[] a = new short[] { 1 };")]
+        [InlineData("private static readonly int[] a = new int[] { 1 };")]
+        [InlineData("private static readonly string[] a = new string[] { nameof(a) };")]
+        public Task IllegalDeclarations_NoDiagnostic_CS(string declaration)
+        {
+            var test = new VerifyCS.Test
+            {
+                TestCode = $@"
+public class C
+{{
+    {declaration}
 }}",
                 ReferenceAssemblies = ReferenceAssemblies.Net.Net50
             };

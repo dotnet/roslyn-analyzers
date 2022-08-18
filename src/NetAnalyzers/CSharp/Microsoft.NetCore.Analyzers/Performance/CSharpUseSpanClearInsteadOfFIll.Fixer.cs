9ec428acc1d78782b58c29ebc084b43f3a2d3621
Buyaa Namnan <buyankhishig.namnan@microsoft.com>
2022-08-18 11:02:52 -0700

    Update src/NetAnalyzers/CSharp/Microsoft.NetCore.Analyzers/Performance/CSharpUseSpanClearInsteadOfFIll.Fixer.cs

--- src/NetAnalyzers/CSharp/Microsoft.NetCore.Analyzers/Performance/CSharpUseSpanClearInsteadOfFIll.Fixer.cs
+++ src/NetAnalyzers/CSharp/Microsoft.NetCore.Analyzers/Performance/CSharpUseSpanClearInsteadOfFIll.Fixer.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
-using System.Composition;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;

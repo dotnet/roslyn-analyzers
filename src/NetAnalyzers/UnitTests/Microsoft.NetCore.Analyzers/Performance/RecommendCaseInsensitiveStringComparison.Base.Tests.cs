9516781040b8c2a764bc5871e4a2cdad357e3d95
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2023-05-30 22:40:09 -0700

    Base test class

---
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Performance/RecommendCaseInsensitiveStringComparison.Base.Tests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.NetCore.Analyzers.Performance.UnitTests
+{
+    public abstract class RecommendCaseInsensitiveStringComparison_Base_Tests
+    {
+        protected static readonly DiagnosticDescriptor StringComparisonRule = RecommendCaseInsensitiveStringComparisonAnalyzer.RecommendCaseInsensitiveStringComparisonRule;
+        protected static readonly DiagnosticDescriptor StringComparerRule = RecommendCaseInsensitiveStringComparisonAnalyzer.RecommendCaseInsensitiveStringComparerRule;
+
+        protected const string ContainsName = RecommendCaseInsensitiveStringComparisonAnalyzer.StringContainsMethodName;
+        protected const string IndexOfName = RecommendCaseInsensitiveStringComparisonAnalyzer.StringIndexOfMethodName;
+        protected const string StartsWithName = RecommendCaseInsensitiveStringComparisonAnalyzer.StringStartsWithMethodName;
+        protected const string CompareToName = RecommendCaseInsensitiveStringComparisonAnalyzer.StringCompareToMethodName;
+
+    }
+}

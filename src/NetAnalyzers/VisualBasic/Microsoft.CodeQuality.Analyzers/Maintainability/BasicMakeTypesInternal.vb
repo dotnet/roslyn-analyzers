ef03d124116b7b6576acc80b3a8f9b4ef44f75f2
Collin Alpert <collinalpert@gmail.com>
2023-07-31 13:22:29 +0200

    Add header comment.

--- src/NetAnalyzers/VisualBasic/Microsoft.CodeQuality.Analyzers/Maintainability/BasicMakeTypesInternal.vb
+++ src/NetAnalyzers/VisualBasic/Microsoft.CodeQuality.Analyzers/Maintainability/BasicMakeTypesInternal.vb
@@ -1,3 +1,5 @@
+' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
 Imports System.Collections.Immutable
 Imports Analyzer.Utilities.Extensions
 Imports Microsoft.CodeAnalysis

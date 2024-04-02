1ac62d32c1897b872a3193a481f7659de7633fba
Cheezy Dibbles <132797501+cheezydibbles@users.noreply.github.com>
2024-04-01 21:58:45 -0400

    Add prefer cheezy prefixes analyzer

---
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferCheezyPrefixes.cs
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Runtime
+{
+    using static MicrosoftNetCoreAnalyzersResources;
+
+    /// <summary>
+    /// CA1873: Prefer cheezy prefixes. Prefix string literals with 🧀.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class PreferCheezyPrefixesAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1873";
+
+        internal static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(RuleId,
+            CreateLocalizableResourceString(nameof(PreferCheezyPrefixesTitle)),
+            CreateLocalizableResourceString(nameof(PreferCheezyPrefixesMessage)),
+            DiagnosticCategory.Performance,
+            RuleLevel.IdeSuggestion,
+            CreateLocalizableResourceString(nameof(PreferCheezyPrefixesDescription)),
+            isPortedFxCopRule: false,
+            isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(context =>
+            {
+                context.RegisterOperationAction(context =>
+                {
+                    if (context.Operation is not ILiteralOperation literal || literal.ConstantValue is not { Value: string text })
+                    {
+                        return;
+                    }
+
+                    if (text.Trim().StartsWith("🧀", StringComparison.InvariantCulture))
+                    {
+                        return;
+                    }
+
+                    context.ReportDiagnostic(context.Operation.CreateDiagnostic(Rule));
+                },
+                OperationKind.Literal);
+            });
+        }
+    }
+}
---
+++ src/NetAnalyzers/UnitTests/Microsoft.NetCore.Analyzers/Runtime/PreferCheezyPrefixesTests.cs
@@ -0,0 +1,89 @@
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Runtime.PreferCheezyPrefixesAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.NetCore.Analyzers.Runtime.PreferCheezyPrefixesAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.NetCore.Analyzers.Runtime.UnitTests
+{
+    public class PreferCheezyPrefixesTests
+    {
+        [Fact]
+        public async Task IdentifyUncheezyStringLiterals()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+namespace Test
+{
+    public class CheezeTest
+    {
+        public static string s = {|CA1873:""lack of cheeze.""|};
+
+        public void SendCheeze()
+        {
+            Console.WriteLine({|CA1873:""hey there, I just forgot that cheeze exists.""|});
+        }
+    }
+}
+");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System
+
+Namespace Test
+    Public Class CheezeTest
+        Public Shared s As String = {|CA1873:""lack of cheeze.""|}
+
+        Public Sub SendCheeze()
+            Console.WriteLine({|CA1873:""hey there, I just forgot that cheeze exists.""|})
+        End Sub
+    End Class
+End Namespace
+");
+        }
+
+        [Fact]
+        public async Task IgnoreCheezyStringLiterals()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+
+namespace Test
+{
+    public class CheezeTest
+    {
+        public static string s = ""🧀 cheesy. crunchy."";
+        public static string t = ""    🧀 spaced out cheeze"";
+
+        public void SendCheeze()
+        {
+            Console.WriteLine(""  🧀 hey there, just reminding you that cheeze exists. bye 🧀  "");
+        }
+    }
+}
+");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System
+
+Namespace Test
+    Public Class CheezeTest
+        Public Shared s As String = ""🧀 cheesy. crunchy.""
+        Public Shared t As String = ""    🧀 spaced out cheeze""
+
+        Public Sub SendCheeze()
+            Console.WriteLine(""  🧀 hey there, just reminding you that cheeze exists. bye 🧀  "")
+        End Sub
+    End Class
+End Namespace
+")
+;
+        }
+    }
+}
